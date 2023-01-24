8be5dc824be7435b7cff42db5a447c8937cf529f
Ivan Chub <ivansergchub@gmail.com>
2023-01-24 12:24:08 -0800

    starting to work on the github sync

---
+++ apps/server/src/githubSync/githubSync.ts
@@ -0,0 +1,9 @@
+export async function githubSync(): Promise<void> {}
+
+const repositoryUrls = ["https://github.com/ethers-io/ethers.js/"];
+
+async function loadRepositories() {}
+
+async function loadRepositoryContributors(repoId: number) {}
+
+async function loadUserKeys(userId: number) {}
