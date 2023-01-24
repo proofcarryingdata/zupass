9b5e4bc8c587f08079e6f2b088f4cb6b51159f9a
Ivan Chub <ivansergchub@gmail.com>
2023-01-24 11:24:06 -0800

    fixed .env path in prod

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,8 +1,13 @@
 import * as dotenv from "dotenv";
 import * as path from "path";
 import { startAPI } from "./api/api";
+import { IS_PROD } from "./isProd";
 
-const dotEnvPath = path.join(process.cwd(), ".env");
+// in production, the .env file is placed at the root of the project,
+// rather than being under `apps/server`
+const dotEnvPath = IS_PROD
+  ? path.join(process.cwd(), ".env")
+  : path.join(process.cwd(), "../../.env");
 
 console.log(`Loading environment variables from: ${dotEnvPath} `);
 dotenv.config({ path: dotEnvPath });
