d14a53a43ce52b65d2e2eef94e1597515f177b5f
Ivan Chub <ivansergchub@gmail.com>
2023-01-24 05:52:22 -0800

    attempt port

--- apps/server/src/isProd.ts
+++ apps/server/src/isProd.ts
@@ -1,1 +1,1 @@
-export const IS_PROD = process.env.NODE_ENV === 'production';
+export const IS_PROD = process.env.NODE_ENV === "production";
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,15 +1,15 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import { IS_PROD } from "./isProd";
 
 console.log("starting server");
 
-const app = express()
-const port = IS_PROD ? 8080 : 3002;
+const app = express();
+const port = IS_PROD ? process.env.PORT : 3002;
 
-app.get('/', (req: Request, res:Response) => {
-  res.send('Hello World!')
-})
- 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
