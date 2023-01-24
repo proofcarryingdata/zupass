26f304b15c95793e3d7a5163e9d07348b0e6015e
Ivan Chub <ivansergchub@gmail.com>
2023-01-23 17:18:33 -0800

    turbo

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -9,7 +9,7 @@ const port = IS_PROD ? 80 : 3002;
 app.get('/', (req: Request, res:Response) => {
   res.send('Hello World!')
 })
-
+ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
