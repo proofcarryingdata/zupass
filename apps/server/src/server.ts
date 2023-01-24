67f7fa4ef4c280245f997e5040b90d343cfac1f1
Ivan Chub <ivansergchub@gmail.com>
2023-01-23 17:05:16 -0800

    added server

---
+++ apps/server/src/server.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from "express";
+import { IS_PROD } from "./isProd";
+
+console.log("starting server");
+
+const app = express()
+const port = IS_PROD ? 80 : 3002;
+
+app.get('/', (req: Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
