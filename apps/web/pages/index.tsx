00604eef4fbe9652520c79a85b20612b136cb876
Ivan Chub <ivansergchub@gmail.com>
2023-01-30 17:17:04 -0800

    more

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,22 @@
-import { Button } from "ui";
-
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>0xPARC Goerli Testnet Eth Faucet</h1>
+      <div>
+        check if your account is eligible
+        <input type="text" />
+        <button>check</button>
+      </div>
+      <div>
+        check if your public key is eligible
+        <input type="text" />
+        <button>check</button>
+      </div>
+      <div>
+        submit proof
+        <textarea />
+        <button>submit</button>
+      </div>
     </div>
   );
 }
