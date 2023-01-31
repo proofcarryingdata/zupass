312c7ee9972a7d7a6f1d8046cea246ec96e22909
Ivan Chub <ivansergchub@gmail.com>
2023-01-30 16:31:12 -0800

    sped up key download

--- apps/server/src/services/githubSync.ts
+++ apps/server/src/services/githubSync.ts
@@ -89,35 +89,41 @@ async function downloadPublicKeys(): Promise<PublicKey[]> {
     const contributions: Contribution[] = [];
     const allContributors: Contributor[] = [];
 
-    for (const repo of repos) {
-      const contributors = await loadRepositoryContributors(
-        repo,
-        octokit,
-        queue
-      );
-      contributors.forEach((c) =>
-        contributions.push({
-          contributor: c,
+    await Promise.all(
+      repos.map(async (repo) => {
+        const contributors = await loadRepositoryContributors(
           repo,
-        })
-      );
-      allContributors.push(...contributors);
-    }
+          octokit,
+          queue
+        );
+        contributors.forEach((c) =>
+          contributions.push({
+            contributor: c,
+            repo,
+          })
+        );
+        allContributors.push(...contributors);
+      })
+    );
 
     const uniqueContributors = _.uniqBy(allContributors, (c) => c.login);
     console.log(`[GITHUB] Loaded ${uniqueContributors.length} contributors`);
 
     const allKeys: PublicKey[] = [];
-    for (let i = 0; i < uniqueContributors.length; i++) {
-      console.log(`[GITHUB] Contributor ${i + 1}/${uniqueContributors.length}`);
-      const contributor = uniqueContributors[i];
+    await Promise.all(
+      uniqueContributors.map(async (contributor, i) => {
+        console.log(
+          `[GITHUB] Contributor ${i + 1}/${uniqueContributors.length}`
+        );
 
-      if (!contributor.id) {
-        continue; // this contributor was anonymous.
-      }
-      const keys = await loadUserKeys(contributor.id, octokit, queue);
-      allKeys.push(...keys);
-    }
+        if (!contributor.id) {
+          return; // this contributor was anonymous.
+        }
+
+        const keys = await loadUserKeys(contributor.id, octokit, queue);
+        allKeys.push(...keys);
+      })
+    );
 
     console.log(
       `[GITHUB] ${hardcodedRepositories.length} repositories
