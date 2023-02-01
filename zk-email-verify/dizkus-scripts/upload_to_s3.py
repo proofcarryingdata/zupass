eb7cde4930f601d64d85a6a0bd4ea1373e912d37
Ivan Chub <ivansergchub@gmail.com>
2023-02-01 14:37:26 -0800

    stealing zk email circuits

---
+++ zk-email-verify/dizkus-scripts/upload_to_s3.py
@@ -0,0 +1,47 @@
+import boto3
+import os
+import tarfile
+import time
+
+# Set up the client for the AWS S3 service
+s3 = boto3.client('s3')  # Ask Aayush for the access key and secret access key
+
+# Set the name of the remote directory and the AWS bucket
+source = '~/Documents/projects/zk-email-verify'
+source = '.'
+zkey_dir = source + '/chunked_build/email/'
+wasm_dir = source + '/chunked_build/email/email_js/'
+bucket_name = 'zkemail-zkey-chunks'  # us-east-1
+
+
+def upload_to_s3(filename, dir=""):
+    with open(dir + filename, 'rb') as file:
+        print("Starting upload...")
+        s3.upload_fileobj(file, bucket_name, filename, ExtraArgs={
+                          'ACL': 'public-read', 'ContentType': 'binary/octet-stream'})
+        print("Done uploading ", filename, "!")
+
+
+# Loop through the files in the remote directory
+for dir in [zkey_dir, wasm_dir]:
+    for file in os.listdir(dir):
+        # Check if the file matches the pattern
+        if file.startswith('email.zkey'):
+            upload_to_s3(file, dir)  # Uncompressed file
+
+            # Create a zip file for the file
+            tar_file_name = file + '.tar.gz'
+            with tarfile.open(tar_file_name, 'w:gz') as tar_file:
+                source_file_path = dir + file
+                print("Compressing: ", source_file_path)
+                tar_file.add(source_file_path,
+                             arcname=os.path.basename(source_file_path))
+
+            # Upload the zip file to the AWS bucket, overwriting any existing file with the same name
+            upload_to_s3(tar_file_name)
+
+            os.remove(tar_file_name)
+
+        if file.startswith('vkey.json') or file.startswith('email.wasm'):
+            # Upload the zip file to the AWS bucket, overwriting any existing file with the same name
+            upload_to_s3(file, dir)
---
+++ zk-email-verify/src/helpers/constants.ts
@@ -0,0 +1,22 @@
+// the numeric form of the payload1 passed into the primitive
+// corresponds to the openssh signature produced by the following command:
+// echo "E PLURIBUS UNUM; DO NOT SHARE" | ssh-keygen -Y sign -n double-blind.xyz -f ~/.ssh/id_rsa | pbcopy
+export const MAGIC_DOUBLE_BLIND_BASE_MESSAGE =
+  14447023197094784173331616578829287000074783130802912942914027114823662617007553911501158244718575362051758829289159984830457466395841150324770159971462582912755545324694933673046215187947905307019469n;
+// Length in bits
+export const MAGIC_DOUBLE_BLIND_BASE_MESSAGE_LEN = 672;
+
+export const CIRCOM_FIELD_MODULUS = 21888242871839275222246405745257275088548364400416034343698204186575808495617n;
+export const MAX_HEADER_PADDED_BYTES = 1024; // NOTE: this must be the same as the first arg in the email in main args circom
+export const MAX_BODY_PADDED_BYTES = 1536; // NOTE: this must be the same as the arg to sha the remainder number of bytes in the email in main args circom
+
+// circom constants from main.circom / https://zkrepl.dev/?gist=30d21c7a7285b1b14f608325f172417b
+// template RSAGroupSigVerify(n, k, levels) {
+// component main { public [ modulus ] } = RSAVerify(121, 17);
+// component main { public [ root, payload1 ] } = RSAGroupSigVerify(121, 17, 30);
+export const CIRCOM_BIGINT_N = 121;
+export const CIRCOM_BIGINT_K = 17;
+export const CIRCOM_LEVELS = 30;
+
+// This is the string that comes right before the target string in the email. Ideally as close to the end of the email as possible.
+export const STRING_PRESELECTOR = 'meant for @';
