c170bccc120076286471124bf080c2a1b01e51f1
Ivan Chub <ivansergchub@gmail.com>
2023-02-08 18:33:52 -0800

    further cleanup

--- zk-blind/scripts/shaHash.ts
+++ zk-blind/scripts/shaHash.ts
@@ -1,6 +1,4 @@
 import { createHash } from "crypto";
-// const { webcrypto, KeyObject } = await import('crypto');
-// const { subtle } = webcrypto;
 
 export async function shaHash(str: Uint8Array) {
   return createHash("sha256").update(str).digest();
