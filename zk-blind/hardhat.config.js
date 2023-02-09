5180c804e9ac64ce2c7256ef3d69b618245ac4b2
Ivan Chub <ivansergchub@gmail.com>
2023-02-09 10:27:42 -0800

    modifying test

--- zk-blind/hardhat.config.js
+++ zk-blind/hardhat.config.js
@@ -1,37 +1,13 @@
 require("hardhat-circom");
-let secret = require("./secret");
-// require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/hardhat-waffle");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.6.11",
-      },
-      {
-        version: "0.8.9",
-      },
-    ],
-  },
-  networks: {
-    goerli: {
-      url: secret.goerli,
-      accounts: [secret.key],
-      gas: 350000000000,
-      gasPrice: 80000000000,
-    },
-  },
-  allowUnlimitedContractSize: true,
   circom: {
     inputBasePath: "./circuits",
     ptau: "powersOfTau28_hez_final_22.ptau",
     circuits: [
       {
-        name: "jwt",
+        name: "main",
         protocol: "groth16",
       },
     ],
