0e18623a95001e12faba43e64e4113e702877ec3
Ivan Chub <ivansergchub@gmail.com>
2023-02-13 13:46:37 -0800

    moved zk-faucet to faucet

--- faucet/packages/circuits/hardhat.config.js
+++ faucet/packages/circuits/hardhat.config.js
@@ -4,16 +4,6 @@ require("hardhat-circom");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.6.11",
-      },
-      {
-        version: "0.8.9",
-      },
-    ],
-  },
   circom: {
     inputBasePath: "./circuits",
     ptau: "https://hermezptau.blob.core.windows.net/ptau/powersOfTau28_hez_final_15.ptau",
