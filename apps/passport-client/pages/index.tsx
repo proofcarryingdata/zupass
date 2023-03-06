5f26b01233bdb830aa1ca1f088427e8be4f09a1c
Ivan Chub <ivansergchub@gmail.com>
2023-03-06 12:50:58 -0800

    choosing ports

--- apps/passport-client/pages/index.tsx
+++ apps/passport-client/pages/index.tsx
@@ -1,9 +1,26 @@
-import { Button } from "../components/Button";
+import styled from "styled-components";
 
 export default function Web() {
   return (
-    <div>
-      <Button />
-    </div>
+    <Container>
+      <h1>ZUZALU PASSPORT</h1>
+      <img src="/passport.jpg" />
+      <br />
+      <button>Login</button>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+
+  button {
+    padding: 16px 64px;
+    font-size: 3em;
+    cursor: pointer;
+  }
+`;
