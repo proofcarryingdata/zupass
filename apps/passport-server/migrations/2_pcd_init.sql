a742ae8976b445441ae63c2717a5e836014e5793
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2023-03-08 00:18:17 +0530

    add sqltyper and stub PCD table and queries

---
+++ apps/passport-server/migrations/2_pcd_init.sql
@@ -0,0 +1,24 @@
+drop table contributions;
+drop table repositories;
+drop table public_keys;
+drop table redemptions;
+
+create table users (
+  id SERIAL PRIMARY KEY,
+  -- email
+  identifier VARCHAR NOT NULL,
+  -- REGULAR = 0, UNVERIFIED = 1 for now
+  status SMALLINT NOT NULL,
+  -- set once at row creation time
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  -- salt used for KDF
+  salt VARCHAR NOT NULL,
+  -- Encrypted PCD store
+  encrypted_blob TEXT NOT NULL,
+  -- Authentication for user
+  server_password VARCHAR NOT NULL,
+  -- the passport should update this every time it updates a row
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+create index user_identifier on users (identifier);
