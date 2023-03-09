c5e0b592483ff54b26d79e22287523873e73af1d
Ivan Chub <ivansergchub@gmail.com>
2023-03-08 16:47:29 -0800

    add to group and get group server routes work

--- apps/passport-server/src/routing/routes/zuzaluRoutes.ts
+++ apps/passport-server/src/routing/routes/zuzaluRoutes.ts
@@ -1,6 +1,12 @@
+import { Group } from "@semaphore-protocol/group";
 import express, { Request, Response } from "express";
+import { serializeSemaphoreGroup } from "passport-interface";
 import { ApplicationContext } from "../../types";
 
+const globalGroup = new Group("1", 16);
+
+// http://localhost:3002/zuzalu/new-participant?commitment=5457595841026900857541504228783465546811548969738060765965868301945253125
+// example identity: ["da4e5656b0892923d30c0a8fa9e68a2ea5b8095c09a4198d066219d5b4e30a","651e367c40d65f65f38ba60f723feb2abcafddd1aa24e6de35a0d9189bca58"]
 export function initZuzaluRoutes(
   app: express.Application,
   context: ApplicationContext
@@ -9,18 +15,30 @@ export function initZuzaluRoutes(
 
   app.get("/zuzalu/new-participant", async (req: Request, res: Response) => {
     const redir = req.query.redir;
+    // TODO: check this was signed by the server and contains 'email' and 'member'
+    // that match the rest of this request
     const token = req.query.token;
+    // TODO: check this is a valid email
     const email = req.query.email;
-    const member = req.query.member;
+    const commitment = req.query.commitment;
 
-    console.log(req.query);
+    if (typeof commitment !== "string") {
+      throw new Error("identity commitment missing");
+    }
+
+    globalGroup.addMember(BigInt(commitment));
+
+    console.log(globalGroup.members);
 
     res.send("new participant");
   });
 
   app.get("/semaphore/:id", async (req: Request, res: Response) => {
+    // TODO: check this group actually exists
     const semaphoreId = req.params.id;
-    console.log(req.params);
-    res.send("semaphore id");
+
+    res.json(
+      JSON.stringify(serializeSemaphoreGroup(globalGroup, "Zuzalu Residents"))
+    );
   });
 }
