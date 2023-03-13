15e641a5b7dd08185b270f3416d7c1ac3b6cb05d
Ivan Chub <ivansergchub@gmail.com>
2023-03-13 15:58:38 +0700

    added test for semaphore group pcd

---
+++ packages/semaphore-group-pcd/test/pcd.spec.ts
@@ -0,0 +1,36 @@
+import { Group } from "@semaphore-protocol/group";
+import { Identity } from "@semaphore-protocol/identity";
+import assert from "assert";
+import * as path from "path";
+import {
+  SemaphoreGroupPCDArgs,
+  SemaphoreGroupPCDPackage,
+} from "../src/SemaphoreGroupPCD";
+
+const zkeyFilePath = path.join(__dirname, "../artifacts/16.zkey");
+const wasmFilePath = path.join(__dirname, "../artifacts/16.wasm");
+
+describe("semaphore group identity should work", function () {
+  it("should be able to generate a proof that verifies", async function () {
+    const { prove, verify, serialize, deserialize } = SemaphoreGroupPCDPackage;
+
+    const identity = new Identity();
+    const group = new Group(1, 16);
+    group.addMember(identity.commitment);
+    const externalNullifier = group.root;
+    const signal = 1;
+
+    const args: SemaphoreGroupPCDArgs = {
+      externalNullifier: BigInt(externalNullifier),
+      signal: BigInt(signal),
+      group,
+      identity,
+      zkeyFilePath,
+      wasmFilePath,
+    };
+
+    const pcd = await prove(args);
+    const verified = await verify(pcd);
+    assert.equal(verified, true);
+  });
+});
