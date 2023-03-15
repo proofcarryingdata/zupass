377d0960e0f23922780a831636afa17054363af6
Ivan Chub <ivansergchub@gmail.com>
2023-03-15 16:30:04 +0700

    working on a generic prover

---
+++ apps/passport-client/components/core/Prover.ts
@@ -0,0 +1,7 @@
+import { PCDRequest } from "@pcd/passport-interface";
+import { PCDCollection } from "@pcd/pcd-collection";
+
+export function handleRequest(
+  pcdCollection: PCDCollection,
+  pcdRequest: PCDRequest
+) {}
