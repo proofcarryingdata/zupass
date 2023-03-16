133f6721c9cf0a4c4938486acfb662d0258fc1fd
Ivan Chub <ivansergchub@gmail.com>
2023-03-16 12:05:03 +0700

    added input test pcd

---
+++ packages/input-test-pcd/src/InputTestPCD.ts
@@ -0,0 +1,63 @@
+import { PCD, PCDPackage, SerializedPCD } from "@pcd/pcd-types";
+import JSONBig from "json-bigint";
+import { v4 as uuid } from "uuid";
+
+export const InputTestPCDTypeName = "input-test-pcd";
+
+export interface InputTestPCDArgs {}
+
+export interface InputTestPCDClaim {}
+
+export type InputTestPCDProof = undefined;
+
+export class InputTestPCD implements PCD<InputTestPCDClaim, InputTestPCDProof> {
+  type = InputTestPCDTypeName;
+  claim: InputTestPCDClaim;
+  proof: InputTestPCDProof;
+  id: string;
+
+  public constructor(id: string, claim: InputTestPCDClaim) {
+    this.claim = claim;
+    this.proof = undefined;
+    this.id = id;
+  }
+}
+
+export async function prove(args: InputTestPCDArgs): Promise<InputTestPCD> {
+  return new InputTestPCD(uuid(), {});
+}
+
+export async function verify(pcd: InputTestPCD): Promise<boolean> {
+  return true;
+}
+
+export async function serialize(pcd: InputTestPCD): Promise<SerializedPCD> {
+  return {
+    type: InputTestPCDTypeName,
+    pcd: JSONBig.stringify({
+      type: pcd.type,
+      id: pcd.id,
+    }),
+  };
+}
+
+export async function deserialize(serialized: string): Promise<InputTestPCD> {
+  const parsed = JSONBig.parse(serialized);
+  return new InputTestPCD(parsed.id, {});
+}
+
+/**
+ * PCD-conforming wrapper for the Semaphore zero-knowledge protocol. You can
+ * find documentation of Semaphore here: https://semaphore.appliedzkp.org/docs/introduction
+ */
+export const SemaphoreIdentityPCDPackage: PCDPackage<
+  InputTestPCDClaim,
+  InputTestPCDProof,
+  InputTestPCDArgs
+> = {
+  name: InputTestPCDTypeName,
+  prove,
+  verify,
+  serialize,
+  deserialize,
+};
