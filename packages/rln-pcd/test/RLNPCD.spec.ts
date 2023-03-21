e747dbdd39c0b50d1b7a20985b09d14418a02aa8
mhchia <kevin.mh.chia@gmail.com>
2023-03-21 18:38:01 +0800

    feat: add pcd for RLNjs

---
+++ packages/rln-pcd/test/RLNPCD.spec.ts
@@ -0,0 +1,104 @@
+import { ArgumentTypeName } from "@pcd/pcd-types";
+import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
+import { RLNPCDPackage, RLNPCDArgs } from "@pcd/rln-pcd";
+import { Group } from "@semaphore-protocol/group";
+import { Identity } from "@semaphore-protocol/identity";
+import assert from "assert";
+import * as path from "path";
+
+
+const TREE_DEPTH = 16
+
+const zkeyFilePath = path.join(__dirname, `../artifacts/${TREE_DEPTH}.zkey`);
+const wasmFilePath = path.join(__dirname, `../artifacts/${TREE_DEPTH}.wasm`);
+const verificationKeyPath = path.join(__dirname, `../artifacts/${TREE_DEPTH}.json`);
+
+
+describe("rln should work", function () {
+  this.timeout(1000 * 30);
+
+  this.beforeAll(async function () {
+    await RLNPCDPackage.init!({
+      zkeyFilePath,
+      wasmFilePath,
+      verificationKeyPath,
+    });
+  });
+
+  let args: RLNPCDArgs;
+
+  beforeEach(async function () {
+    const identity = new Identity();
+    const group = new Group(1, TREE_DEPTH);
+    group.addMember(identity.commitment);
+    const merkleProof = group.generateMerkleProof(0);
+    const signal = "hey hey";
+    const rlnIdentifier = BigInt(5566);
+    const epoch = BigInt(42);
+
+    const identityPCD = await SemaphoreIdentityPCDPackage.serialize(
+      await SemaphoreIdentityPCDPackage.prove({ identity })
+    );
+
+    // Arguments required for proving
+    args = {
+      rlnIdentifier: {
+        argumentType: ArgumentTypeName.BigInt,
+        value: String(rlnIdentifier),
+      },
+      identity: {
+        argumentType: ArgumentTypeName.PCD,
+        value: identityPCD,
+      },
+      merkleProof: {
+        argumentType: ArgumentTypeName.Object,
+        value: merkleProof,
+      },
+      signal: {
+        argumentType: ArgumentTypeName.String,
+        value: signal,
+      },
+      epoch: {
+        argumentType: ArgumentTypeName.BigInt,
+        value: String(epoch)
+      },
+    };
+  });
+
+  it("should be able to generate a proof that verifies", async function () {
+    const { prove, verify } = RLNPCDPackage;
+    const pcd = await prove(args);
+    const verified = await verify(pcd);
+    assert.equal(verified, true);
+  });
+
+  it("should not verify an incorrect proof", async function () {
+    const { prove, verify } = RLNPCDPackage;
+    const pcd = await prove(args);
+    // change merkle root to make it invalid
+    pcd.proof.proof.snarkProof.publicSignals.merkleRoot = "0";
+    const verified = await verify(pcd);
+    assert.equal(verified, false);
+  });
+
+  it("serializing and then deserializing a PCD should result in equal PCDs", async function () {
+    const { prove, serialize, deserialize } = RLNPCDPackage;
+    const pcd = await prove(args);
+
+    const serialized_pcd = await serialize(pcd);
+    const deserialized_pcd = await deserialize(serialized_pcd.pcd);
+
+    assert.deepEqual(deserialized_pcd, pcd);
+  });
+
+  it("verifying a deserialized PCD that is valid should result in a correct verification", async function () {
+    const { prove, verify, serialize, deserialize } = RLNPCDPackage;
+    const pcd = await prove(args);
+
+    const serialized_pcd = await serialize(pcd);
+    const deserialized_pcd = await deserialize(serialized_pcd.pcd);
+    const verified = await verify(deserialized_pcd);
+
+    assert.equal(verified, true);
+  });
+});
