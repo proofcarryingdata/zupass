e7bcb43d3060cb3cecec54d7eb1f195b2f8b1d5b
Ivan Chub <ivansergchub@gmail.com>
2023-03-21 10:36:45 +0700

    added a comment

--- apps/passport-client/components/screens/SyncExistingScreen.tsx
+++ apps/passport-client/components/screens/SyncExistingScreen.tsx
@@ -3,6 +3,12 @@ import React, { useCallback, useState } from "react";
 import { downloadEncryptedStorage } from "../../src/api/endToEndEncryptionApi";
 import { BigInput, Button, H2, Spacer, TextCenter } from "../core";
 
+/**
+ * Users can navigate to this page in order to download
+ * their end-to-end encrypted storage, given that they have
+ * already logged in before. Backups happen automatically
+ * on first login.
+ */
 export function SyncExistingScreen() {
   const [email, setEmail] = useState("");
   const [syncKey, setSyncKey] = useState("");
