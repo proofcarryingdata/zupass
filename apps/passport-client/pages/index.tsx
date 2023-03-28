66f95c71e9f8e448950ca4870fe0eab3091e0cd5
DC <dcposch@dcpos.ch>
2023-03-28 02:18:00 -0700

    Reinstate redirect

--- apps/passport-client/pages/index.tsx
+++ apps/passport-client/pages/index.tsx
@@ -114,9 +114,9 @@ async function loadInitialState(): Promise<ZuState> {
 }
 
 // Redirect old site visitors to the correct site
-// if (!["zupass.org", "localhost"].includes(window.location.hostname)) {
-//   window.location.replace("https://zupass.org/" + window.location.hash);
-// }
+if (!["zupass.org", "localhost"].includes(window.location.hostname)) {
+  window.location.replace("https://zupass.org/" + window.location.hash);
+}
 
 const root = createRoot(document.querySelector("#root"));
 root.render(
