734e39f2505cc505fcf27dcd851840b0c6bd3c9a
Ivan Chub <ivansergchub@gmail.com>
2023-03-29 13:52:59 +0700

    fixed the generic prove screen

--- apps/passport-client/components/screens/ProveScreen/ParameterizedProveScreen.tsx
+++ apps/passport-client/components/screens/ProveScreen/ParameterizedProveScreen.tsx
@@ -2,7 +2,6 @@ import { PCDGetRequest, PCDRequestType } from "@pcd/passport-interface";
 import { title } from "process";
 import * as React from "react";
 import { useCallback, useContext, useState } from "react";
-import { useLocation } from "react-router-dom";
 import { DispatchContext } from "../../../src/dispatch";
 import { err } from "../../../src/util";
 import { Button, H1, Spacer } from "../../core";
@@ -17,24 +16,19 @@ import { PCDArgs } from "../../shared/PCDArgs";
  * HTML input fields that users will fill in by hand. For arguments that
  * are objects, supports loading from a URL.
  */
-export function ParameterizedProveScreen() {
-  const location = useLocation();
-  const params = new URLSearchParams(location.search);
-
+export function ParameterizedProveScreen({ req }: { req: PCDGetRequest }) {
   const [state, dispatch] = useContext(DispatchContext);
-  const request = JSON.parse(params.get("request")) as PCDGetRequest;
-
-  const [args, setArgs] = useState(JSON.parse(JSON.stringify(request.args)));
-  const pcdPackage = state.pcds.getPackage(request.pcdType);
+  const [args, setArgs] = useState(JSON.parse(JSON.stringify(req.args)));
+  const pcdPackage = state.pcds.getPackage(req.pcdType);
   const onProveClick = useCallback(async () => {
     const pcd = await pcdPackage.prove(args);
     const serialized = await pcdPackage.serialize(pcd);
-    window.location.href = `${request.returnUrl}?proof=${JSON.stringify(
+    window.location.href = `${req.returnUrl}?proof=${JSON.stringify(
       serialized
     )}`;
-  }, [args, pcdPackage, request.returnUrl]);
+  }, [args, pcdPackage, req.returnUrl]);
 
-  if (request.type !== PCDRequestType.Get) {
+  if (req.type !== PCDRequestType.Get) {
     err(dispatch, "Unsupported request", `Expected a PCD GET request`);
     return null;
   }
@@ -46,7 +40,7 @@ export function ParameterizedProveScreen() {
       <Spacer h={24} />
       <H1>🔑 &nbsp; {title}</H1>
       <Spacer h={24} />
-      {request.pcdType}
+      {req.pcdType}
       <pre>{JSON.stringify(args, null, 2)}</pre>
       <PCDArgs args={args} setArgs={setArgs} pcdCollection={state.pcds} />
       <Spacer h={16} />
