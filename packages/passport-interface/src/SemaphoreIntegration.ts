33f01a3cc7ece10309e73836ac9113b3dfc3e07e
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2023-03-30 13:49:25 +0700

    shutup lint

--- packages/passport-interface/src/SemaphoreIntegration.ts
+++ packages/passport-interface/src/SemaphoreIntegration.ts
@@ -1,21 +1,16 @@
 import { ArgumentTypeName } from "@pcd/pcd-types";
 import {
-  SemaphoreGroupPCD,
   SemaphoreGroupPCDPackage,
-  SerializedSemaphoreGroup,
 } from "@pcd/semaphore-group-pcd";
-import { Group } from "@semaphore-protocol/group";
-import { useEffect, useState } from "react";
 import { constructPassportPcdGetRequestUrl } from "./PassportInterface";
-import { useProof } from "./PCDIntegration";
 
 /**  */
 export function requestSemaphoreUrl(
   urlToPassportWebsite: string,
   returnUrl: string,
   urlToSemaphoreGroup: string,
-  externalNullifier: string = "1",
-  signal: string = "1"
+  externalNullifier = "1",
+  signal = "1"
 ) {
   const url = constructPassportPcdGetRequestUrl<
     typeof SemaphoreGroupPCDPackage
