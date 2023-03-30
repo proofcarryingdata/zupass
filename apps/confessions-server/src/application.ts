0635629da853bf0fd928234f861e43d583f4a29d
Veronica Zheng <veronica.zhixing.zheng@gmail.com>
2023-03-30 18:17:02 +0700

    use prisma

--- apps/confessions-server/src/application.ts
+++ apps/confessions-server/src/application.ts
@@ -1,4 +1,3 @@
-import { getDB } from "./database/postgresPool";
 import { startServer } from "./routing/server";
 import { ServiceInitializer } from "./services/types";
 import { ApplicationContext } from "./types";
@@ -6,11 +5,7 @@ import { ApplicationContext } from "./types";
 const services: ServiceInitializer[] = [startServer];
 
 export async function startApplication() {
-  const dbPool = await getDB();
-
-  const context: ApplicationContext = {
-    dbPool,
-  };
+  const context: ApplicationContext = {};
 
   for (const service of services) {
     await service(context);
--- apps/confessions-server/src/database/models.ts
+++
@@ -1,6 +0,0 @@
-/** A confession from a semaphore group member */
-export interface Confessions {
-  body: string;
-  semaphoreGroupUrl: string;
-  proof: string;
-}
