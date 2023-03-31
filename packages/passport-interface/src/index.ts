b3210189ac4689f42c678e2a8c58477537ee8c39
Ivan Chub <ivansergchub@gmail.com>
2023-03-31 20:36:01 +0700

    fixed build

--- packages/passport-interface/src/index.ts
+++ packages/passport-interface/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./EncryptedStorage";
 export * from "./Participant";
 export * from "./PassportInterface";
+export * from "./PCDIntegration";
 export * from "./RequestTypes";
 export * from "./SemaphoreGroupIntegration";
 export * from "./SemaphoreSignatureIntegration";
