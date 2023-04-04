3b83ef977fbae541f6867ab3479ef3a290471a5d
Ivan Chub <ivansergchub@gmail.com>
2023-04-04 16:53:48 +0700

    updated signature pcd tests

--- packages/semaphore-signature-pcd/test/SemaphoreSignature.spec.ts
+++ packages/semaphore-signature-pcd/test/SemaphoreSignature.spec.ts
@@ -1,24 +1,18 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { ArgumentTypeName } from "@pcd/pcd-types";
 import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
-import { Group } from "@semaphore-protocol/group";
 import { Identity } from "@semaphore-protocol/identity";
-import { generateProof, verifyProof } from "@semaphore-protocol/proof";
 import assert from "assert";
 import * as path from "path";
 import {
-  generateMessageHash,
-  SemaphoreSignaturePCD,
   SemaphoreSignaturePCDArgs,
-  SemaphoreSignaturePCDClaim,
   SemaphoreSignaturePCDPackage,
-  SemaphoreSignaturePCDProof,
 } from "../src/SemaphoreSignaturePCD";
 
 const zkeyFilePath: string = path.join(__dirname, "../artifacts/16.zkey");
 const wasmFilePath: string = path.join(__dirname, "../artifacts/16.wasm");
 
-describe("semaphore signature should work", function () {
+describe("semaphore signature PCD should work", function () {
   this.timeout(1000 * 30);
 
   // sets up shared Semaphore args across test cases
@@ -47,109 +41,42 @@ describe("semaphore signature should work", function () {
     };
   });
 
-  it("should be able to generate a proof that verifies", async function () {
+  it("should be able to generate a PCD that verifies", async function () {
     const { prove, verify } = SemaphoreSignaturePCDPackage;
     const pcd = await prove(args);
     const verified = await verify(pcd);
     assert.equal(verified, true);
   });
 
-  it("should not verify an incorrect proof", async function () {
+  it("should not verify a PCD with an incorrect signed message", async function () {
     const { prove, verify } = SemaphoreSignaturePCDPackage;
     const pcd = await prove(args);
-    // make the pcd invalid by updating its claim
     pcd.claim.signedMessage += "1";
     const verified = await verify(pcd);
     assert.equal(verified, false);
   });
 
-  it("should not verify a valid semaphore proof over a non singleton group", async function () {
+  it("should not verify a PCD with an incorrect nullifier hash", async function () {
     const { prove, verify } = SemaphoreSignaturePCDPackage;
-
-    // generate a valid proof to get hashed version of signal
     const pcd = await prove(args);
-
-    // pull out original identity from parameters
-    const serializedIdentityPCD = args.identity.value?.pcd;
-    const identityPCD = await SemaphoreIdentityPCDPackage.deserialize(
-      serializedIdentityPCD!
-    );
-
-    // group with other members, non-singleton
-    const group = new Group(1, 16);
-    group.addMember(identityPCD.claim.identity.commitment);
-    group.addMember("1");
-    group.addMember("2");
-    const fullProof = await generateProof(
-      identityPCD.claim.identity,
-      group,
-      identityPCD.claim.identity.commitment,
-      generateMessageHash(pcd.claim.signedMessage),
-      {
-        zkeyFilePath: zkeyFilePath,
-        wasmFilePath: wasmFilePath,
-      }
-    );
-
-    // make PCD with this proof
-    const fakeClaim: SemaphoreSignaturePCDClaim = {
-      identityCommitment: identityPCD.claim.identity.commitment.toString(),
-      signedMessage: pcd.claim.signedMessage,
-      nullifierHash: pcd.claim.nullifierHash,
-    };
-
-    const fakeProof: SemaphoreSignaturePCDProof = fullProof.proof;
-
-    const fakePCD = new SemaphoreSignaturePCD("0", fakeClaim, fakeProof);
-
-    // verify just the proof
-    const validProof = await verifyProof(fullProof, 16);
-    assert.equal(validProof, true);
-
-    // attempt to verify the entire PCD
-    const verified = await verify(fakePCD);
+    pcd.claim.nullifierHash += "1";
+    const verified = await verify(pcd);
     assert.equal(verified, false);
   });
 
-  it("should not verify a PCD with a different message in claim from the signal in a (valid) semaphore proof", async function () {
-    const { verify } = SemaphoreSignaturePCDPackage;
+  it("should not verify a PCD with an incorrect identity commitment", async function () {
+    const { prove, verify } = SemaphoreSignaturePCDPackage;
+    const pcd = await prove(args);
+    pcd.claim.identityCommitment += "1";
+    const verified = await verify(pcd);
+    assert.equal(verified, false);
+  });
 
-    // pull out original identity from parameters
-    const serializedIdentityPCD = args.identity.value?.pcd;
-    const identityPCD = await SemaphoreIdentityPCDPackage.deserialize(
-      serializedIdentityPCD!
-    );
-
-    // singleton group, but a different signal
-    const group = new Group(1, 16);
-    group.addMember(identityPCD.claim.identity.commitment);
-    const fullProof = await generateProof(
-      identityPCD.claim.identity,
-      group,
-      identityPCD.claim.identity.commitment,
-      "1",
-      {
-        zkeyFilePath: zkeyFilePath,
-        wasmFilePath: wasmFilePath,
-      }
-    );
-
-    // make PCD with this proof, but the same message as the original args
-    const fakeClaim: SemaphoreSignaturePCDClaim = {
-      identityCommitment: identityPCD.claim.identity.commitment.toString(),
-      signedMessage: args.signedMessage.value!,
-      nullifierHash: fullProof.nullifierHash + "",
-    };
-    const fakeProof: SemaphoreSignaturePCDProof = fullProof.proof;
-
-    const fakePCD = new SemaphoreSignaturePCD("0", fakeClaim, fakeProof);
-
-    // verify just the proof
-    const validProof = await verifyProof(fullProof, 16);
-    assert.equal(validProof, true);
-
-    // attempt to verify the entire PCD
-    const verified = await verify(fakePCD);
+  it("should not verify a proof with an incorrect proof", async function () {
+    const { prove, verify } = SemaphoreSignaturePCDPackage;
+    const pcd = await prove(args);
+    pcd.proof[0] += "1";
+    const verified = await verify(pcd);
     assert.equal(verified, false);
   });
 
