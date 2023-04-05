44393ac8b4a469c70c8337a8a660d151756826df
Ivan Chub <ivansergchub@gmail.com>
2023-04-05 15:10:34 +0700

    ok, here we go

--- apps/passport-client/components/screens/AddScreen/JustAddScreen.tsx
+++ apps/passport-client/components/screens/AddScreen/JustAddScreen.tsx
@@ -1,13 +1,17 @@
 import { PCDAddRequest } from "@pcd/passport-interface";
-import React, { useCallback } from "react";
+import React, { useCallback, useContext } from "react";
+import { DispatchContext } from "../../../src/dispatch";
+
 import { Button, CenterColumn, H2, Spacer } from "../../core";
 import { AppContainer } from "../../shared/AppContainer";
 import { AppHeader } from "../../shared/AppHeader";
 
 export function JustAddScreen({ request }: { request: PCDAddRequest }) {
+  const [_, dispatch] = useContext(DispatchContext);
+
   const onAddClick = useCallback(() => {
-    console.log("add clicked");
-  }, []);
+    dispatch({ type: "add-pcd", pcd: request.pcd });
+  }, [dispatch, request.pcd]);
 
   return (
     <div>
@@ -15,9 +19,7 @@ export function JustAddScreen({ request }: { request: PCDAddRequest }) {
         <Spacer h={24} />
         <AppHeader />
         <Spacer h={24} />
-        <H2>{"ADD A PCD".toUpperCase()}</H2>
-        just add screen <br />
-        {JSON.stringify(request)}
+        <H2>{"ADD PCD".toUpperCase()}</H2>
         <CenterColumn w={280}>
           <Button onClick={onAddClick}>Add</Button>
         </CenterColumn>
