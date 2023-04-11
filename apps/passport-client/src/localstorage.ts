88889e11ac5d0a8d1a157cac793035d785bfd4f5
Pierre DM <pdaixmoreux@gmail.com>
2023-04-11 18:57:58 +0200

    correct formatter

--- apps/passport-client/src/localstorage.ts
+++ apps/passport-client/src/localstorage.ts
@@ -4,29 +4,29 @@ import { SemaphoreGroupPCDPackage } from "@pcd/semaphore-group-pcd";
 import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
 import { SemaphoreSignaturePCDPackage } from "@pcd/semaphore-signature-pcd";
 import { Identity } from "@semaphore-protocol/identity";
-import { config } from "./config";
 import { JubJubSignaturePCDPackage } from "jubjub-signature-pcd";
+import { config } from "./config";
 
-export async function savePCDs (pcds: PCDCollection) {
+export async function savePCDs(pcds: PCDCollection) {
   const serialized = await pcds.serializeAll();
   const stringified = JSON.stringify(serialized);
-  window.localStorage[ "pcds" ] = stringified;
+  window.localStorage["pcds"] = stringified;
 }
 
-export async function loadPCDs () {
-  const stringified = window.localStorage[ "pcds" ];
+export async function loadPCDs() {
+  const stringified = window.localStorage["pcds"];
   const serialized = JSON.parse(stringified ?? "[]");
 
   const SERVER_STATIC_URL = config.passportServer + "/static/";
 
   await SemaphoreGroupPCDPackage.init({
     wasmFilePath: SERVER_STATIC_URL + "/semaphore-artifacts/16.wasm",
-    zkeyFilePath: SERVER_STATIC_URL + "/semaphore-artifacts/16.zkey",
+    zkeyFilePath: SERVER_STATIC_URL + "/semaphore-artifacts/16.zkey"
   });
 
   await SemaphoreSignaturePCDPackage.init({
     wasmFilePath: SERVER_STATIC_URL + "/semaphore-artifacts/16.wasm",
-    zkeyFilePath: SERVER_STATIC_URL + "/semaphore-artifacts/16.zkey",
+    zkeyFilePath: SERVER_STATIC_URL + "/semaphore-artifacts/16.zkey"
   });
 
   return await PCDCollection.deserialize(
@@ -40,30 +40,30 @@ export async function loadPCDs () {
   );
 }
 
-export function saveEncryptionKey (key: string): void {
-  window.localStorage[ "encryption_key" ] = key;
+export function saveEncryptionKey(key: string): void {
+  window.localStorage["encryption_key"] = key;
 }
 
-export async function loadEncryptionKey (): Promise<string | undefined> {
-  return window.localStorage[ "encryption_key" ];
+export async function loadEncryptionKey(): Promise<string | undefined> {
+  return window.localStorage["encryption_key"];
 }
 
-export function loadSelf (): ZuParticipant | undefined {
-  const self = window.localStorage[ "self" ];
+export function loadSelf(): ZuParticipant | undefined {
+  const self = window.localStorage["self"];
   if (self != null && self !== "") {
     return JSON.parse(self);
   }
 }
 
-export function saveSelf (self: ZuParticipant): void {
-  window.localStorage[ "self" ] = JSON.stringify(self);
+export function saveSelf(self: ZuParticipant): void {
+  window.localStorage["self"] = JSON.stringify(self);
 }
 
-export function loadIdentity (): Identity | null {
-  const str = window.localStorage[ "identity" ];
+export function loadIdentity(): Identity | null {
+  const str = window.localStorage["identity"];
   return str ? new Identity(str) : null;
 }
 
-export function saveIdentity (identity: Identity): void {
-  window.localStorage[ "identity" ] = identity.toString();
+export function saveIdentity(identity: Identity): void {
+  window.localStorage["identity"] = identity.toString();
 }
