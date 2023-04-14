2df35598f8f3d57f230e992c4079ff1ea452cbbb
Ivan Chub <ivansergchub@gmail.com>
2023-04-14 14:14:58 -0700

    tap to reveal commitment

--- apps/passport-client/components/pcd/SemaphoreIdentityCardBody.tsx
+++ apps/passport-client/components/pcd/SemaphoreIdentityCardBody.tsx
@@ -1,10 +1,27 @@
 import { SemaphoreIdentityPCD } from "@pcd/semaphore-identity-pcd";
 import React from "react";
+import styled from "styled-components";
+import { HiddenText } from "../shared/HiddenDisplay";
 
 export function SemaphoreIdentityCardBody({
   pcd,
 }: {
   pcd: SemaphoreIdentityPCD;
 }) {
-  return <div>semaphore identity</div>;
+  return (
+    <Container>
+      <Header>commitment</Header>
+      <HiddenText text={pcd.claim.identity.commitment.toString()} />
+    </Container>
+  );
 }
+
+const Header = styled.div`
+  font-weight: bold;
+`;
+
+const Container = styled.div`
+  padding: 0px 16px 16px 16px;
+  overflow: hidden;
+  width: 100%;
+`;
---
+++ apps/passport-client/components/shared/HiddenDisplay.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback, useState } from "react";
+import styled from "styled-components";
+
+export function HiddenText({ text }: { text: string }) {
+  const [visible, setVisible] = useState(false);
+
+  const onRevealClick = useCallback(() => {
+    setVisible(true);
+  }, []);
+
+  if (visible) {
+    return <TextContainer>{text}</TextContainer>;
+  }
+
+  return (
+    <HiddenTextContainer onClick={onRevealClick}>
+      tap to reveal
+    </HiddenTextContainer>
+  );
+}
+
+export const TextContainer = styled.div`
+  border: 1px solid var(--accent-lite);
+  overflow: hidden;
+  padding: 4px 8px;
+  border-radius: 4px;
+`;
+
+export const HiddenTextContainer = styled(TextContainer)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+  cursor: pointer;
+`;
