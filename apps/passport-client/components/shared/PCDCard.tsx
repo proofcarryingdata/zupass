4eb6303edd69d29346f0c9e3a8a10d69c54b06a8
Ivan Chub <ivansergchub@gmail.com>
2023-04-17 16:52:55 +0200

    header upper case

--- apps/passport-client/components/shared/PCDCard.tsx
+++ apps/passport-client/components/shared/PCDCard.tsx
@@ -32,7 +32,7 @@ export function PCDCard({
   }, [pcd, pcdPackage]);
   const header = isZuzaluIdentity
     ? "VERIFIED ZUZALU PASSPORT"
-    : displayOptions?.header ?? "PCD";
+    : displayOptions?.header?.toUpperCase() ?? "PCD";
 
   if (expanded) {
     return (
