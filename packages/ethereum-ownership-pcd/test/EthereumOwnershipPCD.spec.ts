4ba673f7dfe4262f26dd42014d24c5b16467a10e
Ivan Chub <ivansergchub@gmail.com>
2023-04-19 14:07:43 +0200

    added a test that verifies that the signature must be valid

--- packages/ethereum-ownership-pcd/test/EthereumOwnershipPCD.spec.ts
+++ packages/ethereum-ownership-pcd/test/EthereumOwnershipPCD.spec.ts
@@ -5,6 +5,7 @@ import {
   SemaphoreIdentityPCDTypeName,
 } from "@pcd/semaphore-identity-pcd";
 import { Identity } from "@semaphore-protocol/identity";
+import assert from "assert";
 import { ethers } from "ethers";
 import * as path from "path";
 import { EthereumOwnershipPCDPackage } from "../src/EthereumOwnershipPCD";
@@ -55,5 +56,45 @@ describe("Ethereum ownership PCD", function () {
     });
 
     await EthereumOwnershipPCDPackage.verify(ethereumPCD);
+  });
+
+  it("should not be able create a PCD from an invalid signature", async function () {
+    const wallet = ethers.Wallet.createRandom(null);
+    const identity = await SemaphoreIdentityPCDPackage.prove({
+      identity: new Identity(),
+    });
+    const serializedIdentity = await SemaphoreIdentityPCDPackage.serialize(
+      identity
+    );
+    const signatureOfIdentityCommitment = await wallet.signMessage(
+      new TextEncoder().encode(identity.claim.identity.commitment.toString())
+    );
+    const mangledSignature =
+      signatureOfIdentityCommitment.substring(
+        0,
+        signatureOfIdentityCommitment.length - 1
+      ) + "0";
+
+    assert.rejects(async () => {
+      await EthereumOwnershipPCDPackage.prove({
+        ethereumAddress: {
+          argumentType: ArgumentTypeName.String,
+          value: wallet.address,
+        },
+        ethereumSignatureOfCommitment: {
+          argumentType: ArgumentTypeName.String,
+          value: mangledSignature,
+        },
+        identity: {
+          argumentType: ArgumentTypeName.PCD,
+          pcdType: SemaphoreIdentityPCDTypeName,
+          value: serializedIdentity,
+        },
+        identityCommitment: {
+          argumentType: ArgumentTypeName.String,
+          value: identity.claim.identity.commitment.toString(),
+        },
+      });
+    });
   });
 });
