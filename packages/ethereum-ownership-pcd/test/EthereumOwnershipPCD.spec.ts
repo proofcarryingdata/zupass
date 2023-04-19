2e13a5e0bb7a7550c74590c3898c102bfbfe3819
Ivan Chub <ivansergchub@gmail.com>
2023-04-19 13:30:39 +0200

    starting to implement the ethereum ownership pcd test

--- packages/ethereum-ownership-pcd/test/EthereumOwnershipPCD.spec.ts
+++ packages/ethereum-ownership-pcd/test/EthereumOwnershipPCD.spec.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { ethers } from "ethers";
 import * as path from "path";
 
 const zkeyFilePath: string = path.join(__dirname, "../artifacts/16.zkey");
 const wasmFilePath: string = path.join(__dirname, "../artifacts/16.wasm");
 
-describe("semaphore signature PCD should work", function () {});
+describe("semaphore signature PCD should work", function () {
+  it("should work", async function () {
+    const newWallet = ethers.Wallet.createRandom(null);
+    console.log(newWallet);
+  });
+});
