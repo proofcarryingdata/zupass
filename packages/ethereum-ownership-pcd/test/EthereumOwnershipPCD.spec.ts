2093807e29a80d74e668c6d58a268e7bee60b829
Ivan Chub <ivansergchub@gmail.com>
2023-04-19 13:04:26 +0200

    eth ownership pcd as dependency

---
+++ packages/ethereum-ownership-pcd/test/EthereumOwnershipPCD.spec.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import * as path from "path";
+
+const zkeyFilePath: string = path.join(__dirname, "../artifacts/16.zkey");
+const wasmFilePath: string = path.join(__dirname, "../artifacts/16.wasm");
+
+describe("semaphore signature PCD should work", function () {});
