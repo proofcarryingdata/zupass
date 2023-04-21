7b7f3788f6bbb1d8ba2af1a6caff8c0c1e60aa24
Ivan Chub <ivansergchub@gmail.com>
2023-04-21 14:36:59 +0200

    styling the invalid participant modal

--- apps/passport-client/components/modals/InvalidParticipantModal.tsx
+++ apps/passport-client/components/modals/InvalidParticipantModal.tsx
@@ -1,3 +1,24 @@
+import { Spacer } from "@pcd/passport-ui";
+import styled from "styled-components";
+import { Button, H1 } from "../core";
+
 export function InvalidParticipantModal() {
-  return <div>this participant is invalid</div>;
+  return (
+    <Container>
+      <Spacer h={24} />
+      <H1>Invalid Passport</H1>
+      <Spacer h={24} />
+      <p>
+        You've reset your passport on another device, invalidating this one.
+        Click the button below to log out. Then you'll be able to sync your
+        existing passport onto this device.
+      </p>
+      <Spacer h={24} />
+      <Button>Exit</Button>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  padding: 24px;
+`;
