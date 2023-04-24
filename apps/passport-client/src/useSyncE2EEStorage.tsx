7c68b61a71ff39effd12e6cd7194d90d87cf9611
Ivan Chub <ivansergchub@gmail.com>
2023-04-24 11:41:15 +0200

    remove old syncing logic

--- apps/passport-client/src/useSyncE2EEStorage.tsx
+++ apps/passport-client/src/useSyncE2EEStorage.tsx
@@ -4,15 +4,12 @@ import {
   passportEncrypt,
 } from "@pcd/passport-crypto";
 import { PCDCollection } from "@pcd/pcd-collection";
-import { useContext, useEffect } from "react";
 import {
   downloadEncryptedStorage,
   uploadEncryptedStorage,
 } from "./api/endToEndEncryptionApi";
-import { DispatchContext } from "./dispatch";
 import { loadEncryptionKey, loadPCDs, loadSelf } from "./localstorage";
 import { getPackages } from "./pcdLoader";
-import { ZuState } from "./state";
 
 /**
  * Uploads the state of this passport which is contained in localstorage
@@ -57,41 +54,6 @@ export async function downloadStorage() {
   await pcds.deserializeAllAndAdd(decrypted.pcds);
 }
 
-function trySync(state: ZuState) {
-  const lastSyncedIds = localStorage["last-synced-ids"];
-  let parsedLastSyncedIds: string[] = [];
-
-  try {
-    parsedLastSyncedIds = JSON.parse(lastSyncedIds) as string[];
-  } catch (_e) {
-    console.log(`[SYNC] failed to parse last synced ids - start from scratch`);
-  }
-
-  const lastSync = parsedLastSyncedIds.join(",");
-  const pcds = state.pcds.getAll();
-  const currentPcdIds = pcds.map((pcd) => pcd.id);
-  const currentSync = currentPcdIds.join(",");
-
-  if (lastSync !== currentSync) {
-    console.log("[SYNC] diff detected - uploading PCDs");
-    uploadStorage().then(() => {
-      localStorage["last-synced-ids"] = JSON.stringify(currentPcdIds);
-    });
-  } else {
-    console.log("[SYNC] no diff");
-  }
-}
-
-/**
- * Listens to changes on the state of the application. Whenever there is a change
- * that causes there to be a difference between the current set of PCDs (as identified
- * by the concatenation of their ids) and the set of PCDs that was last uploaded,
- * uploads the current set of PCDs, and saves the fact that we uploaded them.
- */
 export function useSyncE2EEStorage() {
-  const [state] = useContext(DispatchContext);
-
-  useEffect(() => {
-    trySync(state);
-  }, [state]);
+  return "ok";
 }
