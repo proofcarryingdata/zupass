593fae584e98101144bc340c502d38420d620e61
Ivan Chub <ivansergchub@gmail.com>
2023-04-29 16:40:17 +0200

    respond with cached content

--- apps/passport-client/src/service-worker.ts
+++ apps/passport-client/src/service-worker.ts
@@ -5,6 +5,18 @@ const addResourcesToCache = async (resources: string[]) => {
   await cache.addAll(resources);
 };
 
+const cacheFirst = async (request) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+  return fetch(request);
+};
+
+self.addEventListener("fetch", (event: any) => {
+  event.respondWith(cacheFirst(event.request));
+});
+
 self.addEventListener("install", (event: any) => {
   event.waitUntil(
     addResourcesToCache([
