643f346c65ca5df2ec3c8507723200249cffb9cd
Ivan Chub <ivansergchub@gmail.com>
2023-05-02 16:04:34 +0200

    trace migrations

--- apps/passport-server/src/database/postgresMigrations.ts
+++ apps/passport-server/src/database/postgresMigrations.ts
@@ -1,13 +1,16 @@
 import * as path from "path";
 import { PoolClient } from "pg";
 import { migrate } from "postgres-migrations";
+import { traced } from "../services/telemetry";
 
 const MIGRATIONS_PATH = path.join(process.cwd(), "migrations");
 
 export async function migrateDatabase(client: PoolClient): Promise<void> {
   console.log(`[INIT] Executing migrations from directory ${MIGRATIONS_PATH}`);
 
-  await migrate({ client }, MIGRATIONS_PATH);
+  await traced("DB", "migrate", async () => {
+    await migrate({ client }, MIGRATIONS_PATH);
+  });
 
   console.log(`[INIT] Migrations completed successfully`);
 }
