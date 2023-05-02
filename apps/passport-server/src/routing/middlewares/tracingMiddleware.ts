9c63d6df18e1f2d3a4d1120fd357e939176aff1e
Ivan Chub <ivansergchub@gmail.com>
2023-05-02 17:48:15 +0200

    added a comment

--- apps/passport-server/src/routing/middlewares/tracingMiddleware.ts
+++ apps/passport-server/src/routing/middlewares/tracingMiddleware.ts
@@ -1,6 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { traced } from "../../services/telemetry";
 
+/**
+ * Express middleware that traces all HTTP requests to HoneyComb. This
+ * makes it possible to inspect the performance characteristics of all
+ * HTTP requests.
+ */
 export function tracingMiddleware() {
   console.log("[TRACING] setting up middleware");
   return (req: Request, res: Response, next: NextFunction) => {
@@ -16,6 +21,7 @@ export function tracingMiddleware() {
 
         res.on("close", () => {
           span?.setAttribute("statusCode", res.statusCode);
+          // TODO: what to do tracing-wise if a request hangs?
           span?.end();
         });
 
