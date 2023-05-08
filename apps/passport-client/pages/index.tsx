47a31afb1a8b2d88a45bc1ff3bef7596ba12e7e9
Ivan Chub <ivansergchub@gmail.com>
2023-05-08 18:43:03 +0200

    removed components from passport-client - they live in the ui package now

--- apps/passport-client/pages/index.tsx
+++ apps/passport-client/pages/index.tsx
@@ -1,10 +1,5 @@
 import { App } from "@pcd/passport-client-ui/pages/index";
 import { createRoot } from "react-dom/client";
-import { RollbarProvider } from "../components/shared/RollbarProvider";
 
 const root = createRoot(document.querySelector("#root"));
-root.render(
-  <RollbarProvider>
-    <App />
-  </RollbarProvider>
-);
+root.render(<App />);
