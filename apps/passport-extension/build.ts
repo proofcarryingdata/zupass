0e7ed2b02eb4fbe88b09104ac168fecf3c626d24
Ivan Chub <ivansergchub@gmail.com>
2023-05-08 14:08:01 +0200

    build command works

--- apps/passport-extension/build.ts
+++ apps/passport-extension/build.ts
@@ -3,7 +3,6 @@ import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfil
 import * as dotenv from "dotenv";
 import { build, BuildOptions, context } from "esbuild";
 import fs from "fs";
-import { v4 as uuid } from "uuid";
 
 dotenv.config();
 
@@ -38,32 +37,6 @@ const passportAppOpts: BuildOptions = {
   define,
 };
 
-const serviceWorkerOpts: BuildOptions = {
-  tsconfig: "./service-worker-tsconfig.json",
-  bundle: true,
-  entryPoints: ["src/service-worker.ts"],
-  plugins: [
-    NodeModulesPolyfillPlugin(),
-    NodeGlobalsPolyfillPlugin({
-      process: true,
-      buffer: true,
-    }),
-  ],
-  // The output directory here needs to be `public/` rather than
-  // `public/js` in order for the service worker to be served from
-  // the root of the website, which is necessary because service
-  // workers are only able to be attached to the same scope as they
-  // themselves are served from.
-  outdir: "public/",
-  // Service workers are only updated when the binary contents of their
-  // files changes. The service worker for zupass.org uses this environment
-  // variable, which causes its contents to be changed every time `build.ts`
-  // is invoked, so that each new production deploy invalidates the previous
-  // service worker, which clears zupass.org application code (html, js, etc.),
-  // so that clients are not forever stuck on one version of the code.
-  define: { ...define, "process.env.SW_ID": JSON.stringify(uuid()) },
-};
-
 run(process.argv[2])
   .then(() => console.log("Built passport client"))
   .catch((err) => console.error(err));
@@ -80,16 +53,8 @@ async function run(command: string) {
         JSON.stringify(passportRes.metafile)
       );
 
-      const serviceWorkerRes = await build({
-        ...serviceWorkerOpts,
-        minify: true,
-      });
-      console.error("Built", serviceWorkerRes);
       break;
     case "dev":
-      const serviceWorkerCtx = await context(serviceWorkerOpts);
-      await serviceWorkerCtx.watch();
-
       const ctx = await context(passportAppOpts);
       await ctx.watch();
 
