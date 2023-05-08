bb59a55db60782c649d648e4fc5f4eaeb0a166f3
Ivan Chub <ivansergchub@gmail.com>
2023-05-08 14:15:22 +0200

    extension loads properly

--- apps/passport-extension/pages/index.tsx
+++ apps/passport-extension/pages/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 function App() {
-  return <div>App</div>;
+  return <div style={{ padding: "64px 32px" }}>Zupass</div>;
 }
 
 const root = createRoot(document.querySelector("#root"));
