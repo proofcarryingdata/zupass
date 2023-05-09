77dfa1641fa888d1a3c1fdcb76fb91a76a949d63
Ivan Chub <ivansergchub@gmail.com>
2023-05-09 19:35:53 +0200

    remove service worker build code from passport-client-ui

--- packages/passport-client-ui/build.ts
+++ packages/passport-client-ui/build.ts
@@ -1,23 +1,7 @@
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import * as dotenv from "dotenv";
 import { build, BuildOptions, context } from "esbuild";
 import fs from "fs";
-import { v4 as uuid } from "uuid";
-
-dotenv.config();
-
-const define = {
-  "process.env.PASSPORT_SERVER_URL": JSON.stringify(
-    process.env.PASSPORT_SERVER_URL || "http://localhost:3002"
-  ),
-  "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
-  ...(process.env.ROLLBAR_TOKEN !== undefined
-    ? {
-        "process.env.ROLLBAR_TOKEN": JSON.stringify(process.env.ROLLBAR_TOKEN),
-      }
-    : {}),
-};
 
 const passportAppOpts: BuildOptions = {
   sourcemap: true,
@@ -35,33 +19,6 @@ const passportAppOpts: BuildOptions = {
   },
   outdir: "public/js",
   metafile: true,
-  define,
-};
-
-const serviceWorkerOpts: BuildOptions = {
-  tsconfig: "./service-worker-tsconfig.json",
-  bundle: true,
-  entryPoints: ["src/service-worker.ts"],
-  plugins: [
-    NodeModulesPolyfillPlugin(),
-    NodeGlobalsPolyfillPlugin({
-      process: true,
-      buffer: true,
-    }),
-  ],
-  // The output directory here needs to be `public/` rather than
-  // `public/js` in order for the service worker to be served from
-  // the root of the website, which is necessary because service
-  // workers are only able to be attached to the same scope as they
-  // themselves are served from.
-  outdir: "public/",
-  // Service workers are only updated when the binary contents of their
-  // files changes. The service worker for zupass.org uses this environment
-  // variable, which causes its contents to be changed every time `build.ts`
-  // is invoked, so that each new production deploy invalidates the previous
-  // service worker, which clears zupass.org application code (html, js, etc.),
-  // so that clients are not forever stuck on one version of the code.
-  define: { ...define, "process.env.SW_ID": JSON.stringify(uuid()) },
 };
 
 run(process.argv[2])
@@ -80,16 +37,8 @@ async function run(command: string) {
         JSON.stringify(passportRes.metafile)
       );
 
-      const serviceWorkerRes = await build({
-        ...serviceWorkerOpts,
-        minify: true,
-      });
-      console.error("Built", serviceWorkerRes);
       break;
     case "dev":
-      const serviceWorkerCtx = await context(serviceWorkerOpts);
-      await serviceWorkerCtx.watch();
-
       const ctx = await context(passportAppOpts);
       await ctx.watch();
 
