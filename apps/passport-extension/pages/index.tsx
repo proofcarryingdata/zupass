5f3a609495b890db4630431c2f39c960b68c4c3a
Ivan Chub <ivansergchub@gmail.com>
2023-05-09 19:54:57 +0200

    made it possible to load the extension in chrome by fixing the icons

--- apps/passport-extension/pages/index.tsx
+++ apps/passport-extension/pages/index.tsx
@@ -1,3 +1,8 @@
 import { mountApplication } from "@pcd/passport-client-ui/pages/index";
 
-mountApplication(document.querySelector("#root"));
+mountApplication(document.querySelector("#root"), {
+  devMode: process.env.NODE_ENV !== "production",
+  passportServer: process.env.PASSPORT_SERVER_URL,
+  maxProofAge: 1000 * 60 * 60 * 4,
+  rollbarToken: process.env.ROLLBAR_TOKEN,
+});
