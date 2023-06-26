3dd91678ed18213f38e111aa86f531ce8feb083a
Ivan Chub <ivansergchub@gmail.com>
2023-06-26 13:00:31 +0100

    bifurcate generic vs zuzalu login experience

--- apps/passport-client/src/api/user.ts
+++ apps/passport-client/src/api/user.ts
@@ -6,6 +6,30 @@ export async function requestConfirmationEmail(
   identity: Identity,
   force: boolean
 ): Promise<Response> {
+  if (config.isZuzalu) {
+    return requestZuzaluConfirmationEmail(email, identity, force);
+  }
+
+  return requestGenericConfirmationEmail(email, identity, force);
+}
+
+export async function submitNewUser(
+  email: string,
+  token: string,
+  identity: Identity
+): Promise<Response> {
+  if (config.isZuzalu) {
+    return submitNewZuzaluUser(email, token, identity);
+  }
+
+  return submitNewGenericUser(email, token, identity);
+}
+
+export async function requestZuzaluConfirmationEmail(
+  email: string,
+  identity: Identity,
+  force: boolean
+): Promise<Response> {
   const params = new URLSearchParams({
     email,
     commitment: identity.commitment.toString(),
@@ -16,7 +40,41 @@ export async function requestConfirmationEmail(
   return res;
 }
 
-export async function submitNewUser(
+export async function submitNewZuzaluUser(
+  email: string,
+  token: string,
+  identity: Identity
+): Promise<Response> {
+  // Verify the token, save the participant to local storage, redirect to
+  // the home page.
+  const query = new URLSearchParams({
+    email,
+    token,
+    commitment: identity.commitment.toString(),
+  }).toString();
+  const loginUrl = `${config.passportServer}/zuzalu/new-participant?${query}`;
+
+  const res = await fetch(loginUrl);
+  if (!res.ok) throw new Error(await res.text());
+  return res;
+}
+
+export async function requestGenericConfirmationEmail(
+  email: string,
+  identity: Identity,
+  force: boolean
+): Promise<Response> {
+  const params = new URLSearchParams({
+    email,
+    commitment: identity.commitment.toString(),
+    force: force ? "true" : "false",
+  }).toString();
+  const url = `${config.passportServer}/zuzalu/send-login-email?${params}`;
+  const res = await fetch(url, { method: "POST" });
+  return res;
+}
+
+export async function submitNewGenericUser(
   email: string,
   token: string,
   identity: Identity
