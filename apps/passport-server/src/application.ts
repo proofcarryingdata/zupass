2931e781b98c245e3fca84ce3dd92fa70254accf
Ivan Chub <ivansergchub@gmail.com>
2023-06-27 09:30:37 +0100

    cleaning up application starting logic

--- apps/passport-server/src/application.ts
+++ apps/passport-server/src/application.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+import * as path from "path";
 import { getHoneycombAPI } from "./apis/honeycombAPI";
 import { getDB } from "./database/postgresPool";
 import { startServer } from "./routing/server";
@@ -10,8 +12,17 @@ import { startSemaphoreService } from "./services/semaphoreService";
 import { startTelemetry as startTelemetryService } from "./services/telemetryService";
 import { startUserService } from "./services/userService";
 import { ApplicationContext } from "./types";
+import { IS_PROD } from "./util/isProd";
 
 export async function startApplication() {
+  const dotEnvPath = IS_PROD
+    ? `/etc/secrets/.env`
+    : path.join(process.cwd(), ".env");
+
+  console.log(`[INIT] Loading environment variables from: ${dotEnvPath} `);
+  dotenv.config({ path: dotEnvPath });
+  console.log("[INIT] Starting application");
+
   const dbPool = await getDB();
   const honeyClient = getHoneycombAPI();
   const rollbar = getRollbar();
@@ -32,6 +43,9 @@ export async function startApplication() {
   const semaphoreService = startSemaphoreService(context);
   const userService = startUserService(context, semaphoreService);
   const e2eeService = startE2EEService(context);
+  const globalServices = { semaphoreService, userService, e2eeService };
 
-  startServer(context, { semaphoreService, userService, e2eeService });
+  startServer(context, globalServices);
+
+  return { context, globalServices };
 }
