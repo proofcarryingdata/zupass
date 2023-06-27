0623617f1253399d3f6699d5d54a11ef72d3e55d
Ivan Chub <ivansergchub@gmail.com>
2023-06-27 11:02:16 +0100

    testing semaphore service

---
+++ apps/passport-server/test/semaphore.spec.ts
@@ -0,0 +1,30 @@
+import { ZuParticipant } from "@pcd/passport-interface";
+import chai, { expect } from "chai";
+import spies from "chai-spies";
+import "mocha";
+import { step } from "mocha-steps";
+import { startApplication } from "../src/application";
+import { PCDPass } from "../src/types";
+import { login } from "./user/login";
+
+chai.use(spies);
+
+describe("semaphore service", function () {
+  let application: PCDPass;
+  let user: ZuParticipant;
+
+  this.beforeAll(async () => {
+    console.log("starting application");
+    application = await startApplication();
+  });
+
+  step("should be able to log in", async function () {
+    user = await login(application);
+  });
+
+  step("semaphore service should now be aware of the user", async function () {
+    const { semaphoreService } = application.globalServices;
+    const genericGroup = semaphoreService.groupGeneric();
+    expect(genericGroup.group.indexOf(user.commitment)).to.be.above(0);
+  });
+});
