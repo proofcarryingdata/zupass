652e88bb778bdf9ecb9e710428b228beb3c08e51
Ivan Chub <ivansergchub@gmail.com>
2023-06-28 16:42:53 +0100

    fix height problem

--- apps/passport-client/components/shared/AppContainer.tsx
+++ apps/passport-client/components/shared/AppContainer.tsx
@@ -32,7 +32,7 @@ export function AppContainer({
 const Background = styled.div<{ color: string }>`
   background-color: ${(p) => p.color};
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   min-height: 100vh;
 `;
 
@@ -41,7 +41,7 @@ const Container = styled.div`
   justify-content: flex-start;
   align-items: center;
   flex-direction: column;
-  height: 100%;
+  min-height: 100%;
   max-width: 420px;
   margin: 0 auto;
   position: relative;
