2562b21f511bf6af825861751b8147553654c61f
Ivan Chub <ivansergchub@gmail.com>
2023-06-28 18:52:06 +0100

    nit

--- apps/passport-server/test/globalhooks.spec.ts
+++ apps/passport-server/test/globalhooks.spec.ts
@@ -4,6 +4,9 @@ import chaiHttp from "chai-http";
 import spies from "chai-spies";
 import { overrideEnvironment, zuzaluTestingEnv } from "./util/env";
 
+/**
+ * This is executed once prior to the rest of the test suite.
+ */
 before(() => {
   chai.use(chaiAsPromised);
   chai.use(spies);
