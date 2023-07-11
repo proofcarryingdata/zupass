514ff1523a9e16bc9b4a090d50a02f49f244aaac
Ivan Chub <ivansergchub@gmail.com>
2023-07-11 14:48:25 +0100

    adding a function to the client that requests issued pcds

---
+++ apps/passport-client/src/api/issuedPCDs.ts
@@ -0,0 +1,22 @@
+import { IssuedPCDsRequest, IssuedPCDsResponse } from "@pcd/passport-interface";
+import { appConfig } from "../appConfig";
+
+export async function requestIssuedPCDs(
+  request: IssuedPCDsRequest
+): Promise<IssuedPCDsResponse | undefined> {
+  try {
+    const url = `${appConfig.passportServer}/pcds/prove`;
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const pendingPCD = (await response.json()) as IssuedPCDsResponse;
+    return pendingPCD;
+  } catch (e) {
+    return undefined;
+  }
+}
