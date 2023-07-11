07d29c1e13638b735934447f2b04ca14d900f486
Ivan Chub <ivansergchub@gmail.com>
2023-07-11 14:29:40 +0100

    added serde test

--- packages/rsa-pcd/test/RSAPCD.spec.ts
+++ packages/rsa-pcd/test/RSAPCD.spec.ts
@@ -2,16 +2,18 @@ import { ArgumentTypeName } from "@pcd/pcd-types";
 import { expect } from "chai";
 import "mocha";
 import NodeRSA from "node-rsa";
-import { RSAPCDPackage } from "../src";
+import { RSAPCD, RSAPCDPackage } from "../src";
 
 describe("RSA signature PCD should work", function () {
   this.timeout(1000 * 30);
 
-  it("should verify with a valid signature", async function () {
-    const key = new NodeRSA({ b: 2048 });
-    const exportedKey = key.exportKey("private");
-    const message = "message to sign";
-    const pcd = await RSAPCDPackage.prove({
+  const key = new NodeRSA({ b: 2048 });
+  const exportedKey = key.exportKey("private");
+  const message = "message to sign";
+  let pcd: RSAPCD;
+
+  this.beforeAll(async () => {
+    pcd = await RSAPCDPackage.prove({
       privateKey: {
         argumentType: ArgumentTypeName.String,
         value: exportedKey,
@@ -21,7 +23,18 @@ describe("RSA signature PCD should work", function () {
         value: message,
       },
     });
+  });
+
+  it("should be able to prove and verify with valid arguments", async function () {
     const isValid = await RSAPCDPackage.verify(pcd);
     expect(isValid).to.eq(true);
+  });
+
+  it("should be able to serialize and deserialize", async function () {
+    const serialized = await RSAPCDPackage.serialize(pcd);
+    const deserialized = await RSAPCDPackage.deserialize(serialized.pcd);
+    const deserializedValid = await RSAPCDPackage.verify(deserialized);
+    expect(deserializedValid).to.eq(true);
+    expect(pcd).to.deep.eq(deserialized);
   });
 });
