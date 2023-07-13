e2d9f9b8d59bf76cfdf70ae31db888573f0075e5
Ivan Chub <ivansergchub@gmail.com>
2023-07-13 16:26:49 +0100

    finished extracting qr code

--- apps/passport-client/components/shared/IdentityQR.tsx
+++ apps/passport-client/components/shared/IdentityQR.tsx
@@ -1,23 +1,12 @@
+import { QRDisplayWithRegenerateAndStorage } from "@pcd/passport-ui";
 import { SemaphoreSignaturePCDPackage } from "@pcd/semaphore-signature-pcd";
-import React, {
-  useCallback,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import { useCallback, useContext } from "react";
 import styled from "styled-components";
 import { appConfig } from "../../src/appConfig";
 import { createQRProof } from "../../src/createQRProof";
 import { DispatchContext } from "../../src/dispatch";
 import { encodeQRPayload, makeEncodedVerifyLink } from "../../src/qr";
 import { icons } from "../icons";
-import { QR } from "./QR";
-
-interface SavedQRState {
-  timestamp: number;
-  payload: string;
-}
 
 const [qrBg, qrFg] = (() => {
   try {
@@ -59,113 +48,6 @@ export function IdentityQR() {
   );
 }
 
-export function QRDisplayWithRegenerateAndStorage({
-  generateQRPayload,
-  maxAgeMs,
-  uniqueId,
-  loadingLogo,
-  loadedLogo,
-  fgColor,
-  bgColor,
-}: {
-  generateQRPayload: () => Promise<string>;
-  maxAgeMs: number;
-  uniqueId: string;
-  loadingLogo: React.ReactNode;
-  loadedLogo: React.ReactNode;
-  fgColor?: string;
-  bgColor?: string;
-}) {
-  /**
-   * Generate a fresh identity-revealing proof every n ms. We regenerate before
-   * the proof expires to allow for a few minutes of clock skew between prover
-   * and verifier.
-   */
-  const regenerateAfterMs = (maxAgeMs * 2) / 3;
-
-  const [savedState, setSavedState] = useState<SavedQRState | undefined>(() => {
-    const savedState = JSON.parse(
-      localStorage[uniqueId] || "{}"
-    ) as Partial<SavedQRState>;
-    console.log(
-      `[QR] loaded saved state for ${uniqueId}: ${JSON.stringify(savedState)}`
-    );
-
-    const { timestamp, payload } = savedState;
-
-    if (
-      timestamp != null &&
-      Date.now() - timestamp < maxAgeMs &&
-      payload !== undefined
-    ) {
-      console.log(`[QR] from localStorage, timestamp ${timestamp}`);
-      return { timestamp, payload: payload };
-    }
-
-    return undefined;
-  });
-
-  const maybeGenerateQR = useCallback(async () => {
-    const timestamp = Date.now();
-    if (savedState && timestamp - savedState.timestamp < regenerateAfterMs) {
-      console.log(`[QR] not regenerating, timestamp ${timestamp}`);
-      return;
-    }
-    console.log(`[QR] regenerating data ${timestamp}`);
-    const newData = await generateQRPayload();
-    const newSavedState: SavedQRState = { timestamp, payload: newData };
-    localStorage[uniqueId] = JSON.stringify(newSavedState);
-    setSavedState(newSavedState);
-  }, [generateQRPayload, savedState, uniqueId]);
-
-  useEffect(() => {
-    maybeGenerateQR();
-    const interval = setInterval(maybeGenerateQR, maxAgeMs / 10);
-    return () => clearInterval(interval);
-  }, [maxAgeMs, maybeGenerateQR]);
-
-  const logoOverlay = useMemo(() => {
-    return savedState ? loadedLogo : loadingLogo;
-  }, [loadedLogo, loadingLogo, savedState]);
-
-  console.log(`[QR] rendering ${savedState?.payload}`);
-
-  return (
-    <QRDisplay
-      logoOverlay={logoOverlay}
-      value={savedState?.payload}
-      fgColor={fgColor}
-      bgColor={bgColor}
-    />
-  );
-}
-
-export function QRDisplay({
-  value,
-  logoOverlay,
-  fgColor,
-  bgColor,
-}: {
-  value?: string;
-  logoOverlay?: React.ReactNode;
-  fgColor?: string;
-  bgColor?: string;
-}) {
-  console.log(`[QR] VALUE`, value);
-  return (
-    <QRWrap>
-      {value !== undefined && (
-        <QR
-          value={value}
-          bgColor={bgColor ?? "#ffffff"}
-          fgColor={fgColor ?? "#000000"}
-        />
-      )}
-      {logoOverlay}
-    </QRWrap>
-  );
-}
-
 function QRLogoLoading() {
   return <QRLogo width="48" height="48" src={icons.qrCenterLoading} />;
 }
@@ -179,13 +61,4 @@ const QRLogo = styled.img`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-`;
-
-// Style constants
-const qrSize = "300px";
-const QRWrap = styled.div`
-  position: relative;
-  width: ${qrSize};
-  height: ${qrSize};
-  margin: 0 auto;
 `;
