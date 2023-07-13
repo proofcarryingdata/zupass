4573d80208d2cc8b597e1833103b5db2d8409118
Richard Liu <richard.y.liu@berkeley.edu>
2023-07-13 09:10:27 +0100

    updates

--- apps/passport-server/migrations/16_devconnect_tickets.sql
+++ apps/passport-server/migrations/16_devconnect_tickets.sql
@@ -1,5 +1,6 @@
 -- Tickets with active items, aggregated by (email, event, organizer)
 create table devconnect_pretix_tickets (
+  id SERIAL PRIMARY KEY,
    -- email address
   email VARCHAR NOT NULL,
   -- full name, eg "Vitalik Buterin"
@@ -9,45 +10,54 @@ create table devconnect_pretix_tickets (
   -- URL of the Pretix organizer
   organizer_url VARCHAR NOT NULL,
   -- active item IDs
-  item_ids BIGINT[] NOT NULL,
-  -- rows of Table will b unique on (email, event_id, organizer_url)
-   primary key(email, event_id, organizer_url)
+  item_ids VARCHAR[] NOT NULL,
+  -- each ticket will be unique on (email, event_id, organizer_url)
+   UNIQUE (email, event_id, organizer_url)
 );
 
 -- Configuration tables - for hosts to manually edit
 create table pretix_organizers_config (
+  id SERIAL PRIMARY KEY,
    -- URL of the Pretix organizer
-  organizer_url VARCHAR NOT NULL PRIMARY KEY,
+  organizer_url VARCHAR NOT NULL UNIQUE,
   -- Pretix API token
   token VARCHAR NOT NULL
 );
 create table pretix_events_config (
-  organizer_url VARCHAR NOT NULL REFERENCES pretix_organizers_config(organizer_url),
-  -- Event ID
-  event_id VARCHAR NOT NULL PRIMARY KEY,
-  -- Relevant item IDs that correspond to ticket products
-  active_item_ids BIGINT[] NOT NULL
+  id SERIAL PRIMARY KEY,
+  -- FK into `pretix_organizers_config_id` table
+  pretix_organizers_config_id SERIAL NOT NULL REFERENCES pretix_organizers_config(id),
+  -- Event ID from Pretix API
+  event_id VARCHAR NOT NULL UNIQUE,
+  -- Relevant item IDs that correspond to ticket products.
+  -- We skip processing of all item IDs that are not in this list.
+  active_item_ids VARCHAR[] NOT NULL
 );
 
 -- Tables for us to keep relevant data from the Pretix API synced
 -- References rows from config tables
 create table devconnect_pretix_events_info (
-  event_id VARCHAR NOT NULL PRIMARY KEY,
-  organizer_url VARCHAR REFERENCES pretix_organizers_config(organizer_url),
+  id SERIAL PRIMARY KEY,
+  -- One-to-one FK with `pretix_events_config` table
+  pretix_events_config_id SERIAL NOT NULL UNIQUE REFERENCES pretix_events_config(id),
+  -- Event name from Pretix API
   event_name VARCHAR NOT NULL
 );
 create table devconnect_pretix_items_info (
-  item_id BIGINT NOT NULL PRIMARY KEY,
-  event_id VARCHAR NOT NULL REFERENCES pretix_events_config(event_id),
+  id SERIAL PRIMARY KEY,
+  -- Item ID from Pretix API
+  item_id VARCHAR NOT NULL UNIQUE,
+  -- FK into `devconnect_pretix_events_info` table
+  devconnect_pretix_events_info_id SERIAL NOT NULL REFERENCES devconnect_pretix_events_info(id),
+  -- Item name from Pretix API
   item_name VARCHAR NOT NULL
 );
 
 -- Privileged users that can check status of ticketholders
 create table devconnect_superusers (
+  id SERIAL PRIMARY KEY,
    -- email address
-  email VARCHAR NOT NULL PRIMARY KEY,
-  -- Pretix event ID
-  event_id VARCHAR NOT NULL,
-  -- URL of the Pretix organizer
-  organizer_url VARCHAR NOT NULL
+  email VARCHAR NOT NULL,
+  -- One-to-one FK with `pretix_events_config` table
+  pretix_events_config_id SERIAL NOT NULL UNIQUE REFERENCES pretix_events_config(id)
 );
