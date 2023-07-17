72d13d51f8ffa5853881f77684e44d01942991c2
Ivan Chub <ivansergchub@gmail.com>
2023-07-17 11:40:01 +0100

    implement server 404

--- apps/passport-server/src/routing/server.ts
+++ apps/passport-server/src/routing/server.ts
@@ -33,15 +33,14 @@ export async function startServer(
       app.use(express.json());
       app.use(cors());
       app.use(tracingMiddleware());
-
-      initAllRoutes(app, context, globalServices);
-
       app.use(
         cors({
           origin: "*",
           methods: ["GET", "POST", "PUT", "DELETE"],
         })
       );
+
+      initAllRoutes(app, context, globalServices);
 
       app.use(
         (
@@ -52,15 +51,10 @@ export async function startServer(
         ) => {
           logger(`[ERROR] ${req.method} ${req.url}`);
           logger(err.stack);
+          globalServices.rollbarService?.reportError(err);
           res.status(500).send(err.message);
         }
       );
-
-      globalServices.rollbarService?.initExpressMiddleware(app);
-
-      app.use((_req, res, _next) => {
-        res.status(404).send("Not a valid API route, refer to documentation.");
-      });
 
       const server = app.listen(port, () => {
         logger(`[INIT] HTTP server listening on port ${port}`);
