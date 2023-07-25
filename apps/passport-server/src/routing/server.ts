08a01a5cc46995938102551e69d9e10d916694dc
Ivan Chub <ichub@users.noreply.github.com>
2023-07-25 14:55:20 +0100

    upgrade sync storage size (#363)

--- apps/passport-server/src/routing/server.ts
+++ apps/passport-server/src/routing/server.ts
@@ -19,7 +19,7 @@ import { initZuzaluRoutes } from "./routes/zuzaluRoutes";
 
 export async function startServer(
   context: ApplicationContext,
-  globalServices: GlobalServices
+  globalServices: GlobalServices,
 ): Promise<{ app: Application; server: http.Server }> {
   return new Promise<{ app: Application; server: http.Server }>(
     (resolve, reject) => {
@@ -30,14 +30,18 @@ export async function startServer(
         app.use(morgan("tiny"));
       }
 
-      app.use(express.json());
+      app.use(
+        express.json({
+          limit: "5mb",
+        }),
+      );
       app.use(cors());
       app.use(tracingMiddleware());
       app.use(
         cors({
           origin: "*",
           methods: ["GET", "POST", "PUT", "DELETE"],
-        })
+        }),
       );
 
       initAllRoutes(app, context, globalServices);
@@ -47,13 +51,13 @@ export async function startServer(
           err: Error,
           req: express.Request,
           res: express.Response,
-          _next: NextFunction
+          _next: NextFunction,
         ) => {
           logger(`[ERROR] ${req.method} ${req.url}`);
           logger(err.stack);
           globalServices.rollbarService?.reportError(err);
           res.status(500).send(err.message);
-        }
+        },
       );
 
       const server = app.listen(port, () => {
@@ -65,14 +69,14 @@ export async function startServer(
       server.on("error", (e: Error) => {
         reject(e);
       });
-    }
+    },
   );
 }
 
 function initAllRoutes(
   app: express.Application,
   context: ApplicationContext,
-  globalServices: GlobalServices
+  globalServices: GlobalServices,
 ): void {
   initStatusRoutes(app, context, globalServices);
   initHealthcheckRoutes(app, context);
