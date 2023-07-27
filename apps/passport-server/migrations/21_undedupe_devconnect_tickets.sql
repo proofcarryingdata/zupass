a588a72546c2d0de62bfa715906ae2f5692baaad
Ivan Chub <ivansergchub@gmail.com>
2023-07-27 12:03:34 +0100

    add constraint for unique position ids

---
+++ apps/passport-server/migrations/21_undedupe_devconnect_tickets.sql
@@ -0,0 +1,6 @@
+alter table devconnect_pretix_tickets
+drop constraint devconnect_pretix_tickets_email_devconnect_pretix_items_inf_key;
+
+alter table devconnect_pretix_tickets
+add column position_id VARCHAR NOT NULL UNIQUE;
+
