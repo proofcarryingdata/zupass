5e90922bf297f7217f2ff5fdd1d61e9e552f34d1
Ivan Chub <ivansergchub@gmail.com>
2023-08-08 14:31:18 +0100

    implement an rsa image pcd

---
+++ packages/rsa-image-pcd/src/utils.ts
@@ -0,0 +1,18 @@
+import NodeRSA from "node-rsa";
+import { RSAImagePCD } from "./RSAImagePCD";
+
+export function getPublicKey(pcd?: RSAImagePCD): NodeRSA | undefined {
+  const encodedPublicKey = pcd?.proof?.rsaPCD?.proof?.publicKey;
+  if (!encodedPublicKey) {
+    return undefined;
+  }
+
+  try {
+    const key = new NodeRSA(encodedPublicKey, "public");
+    return key;
+  } catch (e) {
+    console.log("failed to deserialize key");
+  }
+
+  return undefined;
+}
