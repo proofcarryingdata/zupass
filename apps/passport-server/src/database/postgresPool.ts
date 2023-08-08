59a835ff6979e18ffdddd780b04c4aa9fb85afc2
Rob Knight <rob.knight@legra.com>
2023-08-08 12:21:53 +0100

    Use pg native bindings

--- apps/passport-server/src/database/postgresPool.ts
+++ apps/passport-server/src/database/postgresPool.ts
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+import { native, Pool } from "pg";
 import { logger } from "../util/logger";
 import { getDatabaseConfiguration } from "./postgresConfiguration";
 import { migrateDatabase } from "./postgresMigrations";
@@ -6,7 +6,12 @@ import { migrateDatabase } from "./postgresMigrations";
 export async function getDB(): Promise<Pool> {
   logger("[INIT] Initializing Postgres client");
 
-  const pool = new Pool(getDatabaseConfiguration());
+  if (!native) {
+    throw new Error("Postgres native bindings not available");
+  }
+
+  const pool = new native.Pool(getDatabaseConfiguration());
+
   const client = await pool.connect();
   await migrateDatabase(client);
   client.release();
