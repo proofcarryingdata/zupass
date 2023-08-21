f96a050b7d4d63a9d08be56c01094f173d1881a8
Rob Knight <mail@robknight.org.uk>
2023-08-21 19:09:41 +0200

    Move <reference> above import

--- packages/eddsa-pcd/src/EdDSAPCD.ts
+++ packages/eddsa-pcd/src/EdDSAPCD.ts
@@ -6,12 +6,11 @@ import {
   StringArgument,
   StringArrayArgument
 } from "@pcd/pcd-types";
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="util/declarations/circomlibjs.d.ts" />
 import { buildEddsa, buildPoseidon } from "circomlibjs";
 import { v4 as uuid } from "uuid";
 import { EdDSACardBody } from "./CardBody";
-
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="util/declarations/circomlibjs.d.ts" />
 
 export const EdDSAPCDTypeName = "eddsa-pcd";
 
