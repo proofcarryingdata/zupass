5e6564ed5b9e34fdc09b8e124bccf15c583b6fb4
Ivan Chub <ivansergchub@gmail.com>
2023-08-22 20:18:09 -0700

    new line

--- packages/eddsa-pcd/src/util/util.ts
+++ packages/eddsa-pcd/src/util/util.ts
@@ -1,4 +1,4 @@
-import * as ed from 'noble-ed25519';
+import * as ed from "noble-ed25519";
 
 export function newEdDSAPrivateKey(): string {
   return Buffer.from(ed.utils.randomPrivateKey()).toString("hex");
