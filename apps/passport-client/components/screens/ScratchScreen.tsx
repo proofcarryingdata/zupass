3b49d3c7b7509bacefbee4acc0494275c58f1d07
Ivan Chub <ivansergchub@gmail.com>
2023-08-24 12:39:35 -0700

    working on a file system ui

--- apps/passport-client/components/screens/ScratchScreen.tsx
+++ apps/passport-client/components/screens/ScratchScreen.tsx
@@ -1,59 +1,136 @@
+import { EdDSAPCDPackage, newEdDSAPrivateKey } from "@pcd/eddsa-pcd";
 import { PCDDisk, PCDPackages } from "@pcd/pcd-collection";
+import { DeserializedDirectory } from "@pcd/pcd-collection/src/util";
 import { ArgumentTypeName } from "@pcd/pcd-types";
-import { RSAPCDPackage } from "@pcd/rsa-pcd";
-import NodeRSA from "node-rsa";
-import { useEffect, useState } from "react";
+import { DependencyList, useEffect, useState } from "react";
+import styled from "styled-components";
 import { v4 as uuid } from "uuid";
+
+function useAsync<T>(
+  func: () => Promise<T>,
+  deps: DependencyList
+): { value: T; err: Error } {
+  const [value, setValue] = useState<T | undefined>();
+  const [err, setErr] = useState<Error | undefined>();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setValue(await func());
+      } catch (e) {
+        setErr(e);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+
+  return { value, err };
+}
 
 async function newPCD(id?: string) {
   id = id ?? uuid();
-  const pkey = new NodeRSA({ b: 512 });
 
-  const pcd = await RSAPCDPackage.prove({
+  const pcd = await EdDSAPCDPackage.prove({
     id: {
       argumentType: ArgumentTypeName.String,
       value: id
     },
     privateKey: {
       argumentType: ArgumentTypeName.String,
-      value: pkey.exportKey("private")
+      value: newEdDSAPrivateKey()
     },
-    signedMessage: {
-      argumentType: ArgumentTypeName.String,
-      value: "signed message"
+    message: {
+      argumentType: ArgumentTypeName.StringArray,
+      value: ["5"]
     }
   });
 
   return pcd;
 }
 
-function useTestDisk(): PCDDisk | undefined {
-  const [disk, setDisk] = useState<PCDDisk | undefined>();
+function useTestDisk() {
+  return useAsync<PCDDisk | undefined>(async () => {
+    const disk = new PCDDisk(new PCDPackages([EdDSAPCDPackage]));
 
-  useEffect(() => {
-    (async () => {
-      const disk = new PCDDisk(new PCDPackages([RSAPCDPackage]));
+    disk.mkdirp("/foo");
+    disk.mkdirp("/bar");
+    disk.mkdirp("/foo/baz");
 
-      disk.mkdirp("/foo");
-      disk.mkdirp("/bar");
-      disk.mkdirp("/foo/baz");
+    const pcd = await newPCD();
 
-      const pcd = await newPCD();
+    await disk.insertPCD(pcd, "/");
+    await disk.insertPCD(pcd, "/foo");
+    await disk.insertPCD(pcd, "/bar");
+    await disk.insertPCD(pcd, "/foo/baz");
 
-      await disk.insertPCD(pcd, "/");
-      await disk.insertPCD(pcd, "/foo");
-      await disk.insertPCD(pcd, "/bar");
-      await disk.insertPCD(pcd, "/foo/baz");
+    console.log("created disk", disk);
 
-      setDisk(disk);
-    })();
+    return disk;
   }, []);
+}
 
-  return disk;
+function useDiskSnapshot(disk: PCDDisk | undefined): {
+  value: DeserializedDirectory | undefined;
+  err: Error | undefined;
+} {
+  return useAsync<DeserializedDirectory | undefined>(async () => {
+    if (!disk) {
+      return undefined;
+    }
+
+    return disk.getSnapshot();
+  }, [disk]);
+}
+
+export function RenderDirectory({
+  dir
+}: {
+  dir: DeserializedDirectory | undefined;
+}) {
+  if (!dir) {
+    return undefined;
+  }
+
+  return (
+    <DirectoryContainer>
+      path: {dir.path} <br />
+      child directories: <br />
+      {dir.childDirectories.map((dir) => (
+        <div>{dir.path}</div>
+      ))}
+      child pcds: <br />
+      {dir.pcds.map((pcd) => (
+        <div>pcd: {pcd.id}</div>
+      ))}
+    </DirectoryContainer>
+  );
 }
 
 export function ScratchScreen() {
-  const testDisk = useTestDisk();
+  const { value: testDisk, err } = useTestDisk();
+  const { value: snapshot, err: err2 } = useDiskSnapshot(testDisk);
 
-  return <div>Scratch Screen</div>;
+  console.log(err ?? err2);
+
+  return (
+    <Container>
+      Scratch Screen <br />
+      <RenderDirectory dir={snapshot} />
+    </Container>
+  );
 }
+
+const DirectoryContainer = styled.div`
+  display: inline-block;
+  width: 400px;
+  border: 1px solid black;
+  padding: 8px;
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+  color: black;
+  padding: 64px;
+`;
