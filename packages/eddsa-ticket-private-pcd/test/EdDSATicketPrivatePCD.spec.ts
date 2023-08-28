9dbec2558eb4ffc1fd81d1801cdb2476b77868ee
gubsheep <briangu33@gmail.com>
2023-08-27 21:42:08 -0700

    clean up and add some tests

--- packages/eddsa-ticket-private-pcd/test/EdDSATicketPrivatePCD.spec.ts
+++ packages/eddsa-ticket-private-pcd/test/EdDSATicketPrivatePCD.spec.ts
@@ -6,16 +6,17 @@ import {
   SemaphoreIdentityPCDTypeName
 } from "@pcd/semaphore-identity-pcd";
 import { Identity } from "@semaphore-protocol/identity";
-import { Eddsa, PoseidonFn, buildEddsa, buildPoseidon } from "circomlibjs";
+import { expect } from "chai";
 import "mocha";
 import * as path from "path";
 import {
   EdDSATicketFieldsRequest,
   EdDSATicketPrivatePCD,
+  EdDSATicketPrivatePCDArgs,
   EdDSATicketPrivatePCDPackage,
-  EdDSATicketPrivatePCDTypeName
+  EdDSATicketPrivatePCDTypeName,
+  STATIC_TICKET_PCD_NULLIFIER
 } from "../src";
-import { fromHexString } from "../src/utils/utils";
 
 const zkeyFilePath = path.join(__dirname, `../artifacts/circuit_final.zkey`);
 const wasmFilePath = path.join(
@@ -23,13 +24,61 @@ const wasmFilePath = path.join(
   `../artifacts/eddsaTicket_js/eddsaTicket.wasm`
 );
 
-let eddsa: Eddsa;
-let poseidon: PoseidonFn;
+let toArgs: (
+  ticketData: ITicketData,
+  fieldsRequested: EdDSATicketFieldsRequest,
+  withNullifier: boolean
+) => Promise<EdDSATicketPrivatePCDArgs>;
+
+const identity = new Identity(
+  '["329061722381819402313027227353491409557029289040211387019699013780657641967", "99353161014976810914716773124042455250852206298527174581112949561812190422"]'
+);
+
+// Key borrowed from https://github.com/iden3/circomlibjs/blob/4f094c5be05c1f0210924a3ab204d8fd8da69f49/test/eddsa.js#L103
+const prvKey =
+  "0001020304050607080900010203040506070809000102030405060708090001";
+
+const WATERMARK = BigInt(6);
+const EXTERNAL_NULLIFIER = BigInt(42);
 
 describe("EdDSA partial ticket should work", function () {
+  console.log("begin");
   this.timeout(1000 * 30);
 
-  let resp: EdDSATicketPrivatePCD;
+  let pcd1: EdDSATicketPrivatePCD;
+
+  const ticketData1: ITicketData = {
+    // the fields below are not signed and are used for display purposes
+
+    attendeeName: "test name",
+    attendeeEmail: "user@test.com",
+    eventName: "event",
+    ticketName: "ticket",
+    checkerEmail: "checker@test.com",
+
+    // the fields below are signed using the server's private eddsa key
+
+    ticketId: "b38501b0-cf66-4416-a4ef-631a9a9b32c4",
+    eventId: "4332f16c-8444-4261-94b9-a0ba8ca917e2",
+    productId: "c94002fb-2c41-480b-842d-fa826fb517e5",
+    timestampConsumed: 0,
+    timestampSigned: 1693028498280,
+    attendeeSemaphoreId: identity.getCommitment().toString(),
+    isConsumed: false,
+    isRevoked: false
+  };
+
+  const fieldsRequested1: EdDSATicketFieldsRequest = {
+    revealTicketId: false,
+    revealEventId: true,
+    revealProductId: true,
+    revealTimestampConsumed: false,
+    revealTimestampSigned: false,
+    revealAttendeeSemaphoreId: false,
+    revealIsConsumed: false,
+    revealIsRevoked: false,
+    revealNullifierHash: true
+  };
 
   this.beforeAll(async function () {
     await EdDSAPCDPackage.init!({});
@@ -40,129 +89,106 @@ describe("EdDSA partial ticket should work", function () {
       wasmFilePath
     });
 
-    eddsa = await buildEddsa();
-    poseidon = await buildPoseidon();
+    toArgs = async (
+      ticketData: ITicketData,
+      fieldsRequested: EdDSATicketFieldsRequest,
+      withNullifier: boolean
+    ) => {
+      let ticketPCD = await EdDSATicketPCDPackage.prove({
+        ticket: {
+          value: ticketData,
+          argumentType: ArgumentTypeName.Object
+        },
+        privateKey: {
+          value: prvKey,
+          argumentType: ArgumentTypeName.String
+        },
+        id: {
+          value: undefined,
+          argumentType: ArgumentTypeName.String
+        }
+      });
+
+      let serializedTicketPCD =
+        await EdDSATicketPCDPackage.serialize(ticketPCD);
+
+      let identityPCD = await SemaphoreIdentityPCDPackage.prove({
+        identity
+      });
+
+      let serializedIdentityPCD =
+        await SemaphoreIdentityPCDPackage.serialize(identityPCD);
+
+      const ret: EdDSATicketPrivatePCDArgs = {
+        ticket: {
+          value: serializedTicketPCD,
+          argumentType: ArgumentTypeName.PCD,
+          pcdType: EdDSATicketPrivatePCDTypeName
+        },
+        identity: {
+          value: serializedIdentityPCD,
+          argumentType: ArgumentTypeName.PCD,
+          pcdType: SemaphoreIdentityPCDTypeName
+        },
+        fieldsRequested: {
+          value: fieldsRequested,
+          argumentType: ArgumentTypeName.Object
+        },
+        watermark: {
+          value: WATERMARK.toString(),
+          argumentType: ArgumentTypeName.BigInt
+        }
+      };
+
+      if (withNullifier) {
+        ret.externalNullifier = {
+          value: EXTERNAL_NULLIFIER.toString(),
+          argumentType: ArgumentTypeName.BigInt
+        };
+      }
+
+      return ret;
+    };
   });
 
-  it("should generate a valid proof", async function () {
-    const identity = new Identity(
-      '["329061722381819402313027227353491409557029289040211387019699013780657641967", "99353161014976810914716773124042455250852206298527174581112949561812190422"]'
+  it("should be able to generate and verify a valid proof", async function () {
+    const pcdArgs = await toArgs(ticketData1, fieldsRequested1, true);
+    pcd1 = await EdDSATicketPrivatePCDPackage.prove(pcdArgs);
+
+    const claim = pcd1.claim;
+    expect(claim.partialTicket.ticketId).to.be.equal(undefined);
+    expect(claim.partialTicket.productId).to.not.be.equal(undefined);
+    expect(claim.externalNullifier).to.be.not.equal(undefined);
+
+    let verificationRes = await EdDSATicketPrivatePCDPackage.verify(pcd1);
+    expect(verificationRes).to.be.true;
+  });
+
+  it("should not verify an invalid proof", async function () {
+    const pcdArgs = await toArgs(ticketData1, fieldsRequested1, true);
+    const invalidPCD = await EdDSATicketPrivatePCDPackage.prove(pcdArgs);
+
+    invalidPCD.claim.watermark = "111";
+
+    let verificationRes = await EdDSATicketPrivatePCDPackage.verify(pcd1);
+    expect(verificationRes).to.be.false;
+  });
+
+  it("should default to static pcd watermark", async function () {
+    const pcdArgs = await toArgs(ticketData1, fieldsRequested1, false);
+    const pcd2 = await EdDSATicketPrivatePCDPackage.prove(pcdArgs);
+
+    expect(pcd2.claim.externalNullifier).to.equal(STATIC_TICKET_PCD_NULLIFIER);
+  });
+
+  it("should be able to serialize and deserialize a PCD", async function () {
+    const serialized = await EdDSATicketPrivatePCDPackage.serialize(pcd1);
+    const deserialized = await EdDSATicketPrivatePCDPackage.deserialize(
+      serialized.pcd
     );
-
-    // Key borrowed from https://github.com/iden3/circomlibjs/blob/4f094c5be05c1f0210924a3ab204d8fd8da69f49/test/eddsa.js#L103
-    const prvKey =
-      "0001020304050607080900010203040506070809000102030405060708090001";
-
-    const ticketData: ITicketData = {
-      // the fields below are not signed and are used for display purposes
-
-      attendeeName: "test name",
-      attendeeEmail: "user@test.com",
-      eventName: "event",
-      ticketName: "ticket",
-      checkerEmail: "checker@test.com",
-
-      // the fields below are signed using the server's private eddsa key
-
-      ticketId: "b38501b0-cf66-4416-a4ef-631a9a9b32c4",
-      eventId: "4332f16c-8444-4261-94b9-a0ba8ca917e2",
-      productId: "c94002fb-2c41-480b-842d-fa826fb517e5",
-      timestampConsumed: 0,
-      timestampSigned: 1693028498280,
-      attendeeSemaphoreId: identity.getCommitment().toString(),
-      isConsumed: false,
-      isRevoked: false
-    };
-
-    const fieldsRequested: EdDSATicketFieldsRequest = {
-      revealTicketId: false,
-      revealEventId: true,
-      revealProductId: true,
-      revealTimestampConsumed: false,
-      revealTimestampSigned: false,
-      revealAttendeeSemaphoreId: false,
-      revealIsConsumed: false,
-      revealIsRevoked: false,
-      revealNullifierHash: true
-    };
-
-    const externalNullifier = BigInt(5);
-    const watermark = BigInt(6);
-
-    let ticketPCD = await EdDSATicketPCDPackage.prove({
-      ticket: {
-        value: ticketData,
-        argumentType: ArgumentTypeName.Object
-      },
-      privateKey: {
-        value: prvKey,
-        argumentType: ArgumentTypeName.String
-      },
-      id: {
-        value: undefined,
-        argumentType: ArgumentTypeName.String
-      }
-    });
-
-    const signatureRaw = eddsa.unpackSignature(
-      fromHexString(ticketPCD.proof.eddsaPCD.proof.signature)
-    );
-
-    const signature = {
-      R8x: (poseidon as any).F.toObject(signatureRaw.R8[0]),
-      R8y: (poseidon as any).F.toObject(signatureRaw.R8[1]),
-      S: signatureRaw.S
-    };
-
-    let serializedTicketPCD = await EdDSATicketPCDPackage.serialize(ticketPCD);
-
-    let identityPCD = await SemaphoreIdentityPCDPackage.prove({
-      identity
-    });
-
-    let serializedIdentityPCD =
-      await SemaphoreIdentityPCDPackage.serialize(identityPCD);
-
-    let eddsaRespPCD = await EdDSATicketPrivatePCDPackage.prove({
-      ticket: {
-        value: serializedTicketPCD,
-        argumentType: ArgumentTypeName.PCD,
-        pcdType: EdDSATicketPrivatePCDTypeName
-      },
-      identity: {
-        value: serializedIdentityPCD,
-        argumentType: ArgumentTypeName.PCD,
-        pcdType: SemaphoreIdentityPCDTypeName
-      },
-      fieldsRequested: {
-        value: fieldsRequested,
-        argumentType: ArgumentTypeName.Object
-      },
-      watermark: {
-        value: watermark.toString(),
-        argumentType: ArgumentTypeName.BigInt
-      },
-      externalNullifier: {
-        value: externalNullifier.toString(),
-        argumentType: ArgumentTypeName.BigInt
-      }
-    });
-
-    let verificationRes =
-      await EdDSATicketPrivatePCDPackage.verify(eddsaRespPCD);
-
-    /*
-    const signatureRaw = eddsa.unpackSignature(
-      fromHexString(ticket.proof.eddsaPCD.proof.signature)
-    );
-
-    const signature = {
-      R8x: (poseidon as any).F.toObject(signatureRaw.R8[0]),
-      R8y: (poseidon as any).F.toObject(signatureRaw.R8[1]),
-      S: signatureRaw.S
-    };
-    console.log("signature");
-    console.log(signature);
-    */
+    const deserializedValid =
+      await EdDSATicketPrivatePCDPackage.verify(deserialized);
+    expect(deserializedValid).to.eq(true);
+    expect(pcd1).to.deep.eq(deserialized);
   });
 });
