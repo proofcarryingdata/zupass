3252fb054a4c67a90e2c4cd511438fb65e980eea
Rob Knight <mail@robknight.org.uk>
2023-08-29 17:02:57 +0200

    Test deleting a verification

--- apps/passport-server/test/telegram.spec.ts
+++ apps/passport-server/test/telegram.spec.ts
@@ -3,6 +3,7 @@ import "mocha";
 import { step } from "mocha-steps";
 import { Pool } from "postgres-pool";
 import { getDB } from "../src/database/postgresPool";
+import { deleteTelegramVerification } from "../src/database/queries/telegram/deleteTelegramVerification";
 import { fetchTelegramVerificationStatus } from "../src/database/queries/telegram/fetchTelegramConversation";
 import { insertTelegramVerification } from "../src/database/queries/telegram/insertTelegramConversation";
 import { overrideEnvironment, pcdpassTestingEnv } from "./util/env";
@@ -23,10 +24,23 @@ describe("telegram bot functionality", function () {
     expect(db).to.not.eq(null);
   });
 
+  const dummyUserId = 12345;
+  const dummyChatId = 54321;
+
   step("should be able to record a verified user", async function () {
     // Insert a dummy Telegram user and chat as verified
-    expect(await insertTelegramVerification(db, 12345, 54321)).to.eq(1);
+    expect(
+      await insertTelegramVerification(db, dummyUserId, dummyChatId)
+    ).to.eq(1);
     // Check that the user is verified for access to the chat
-    expect(await fetchTelegramVerificationStatus(db, 12345, 54321)).to.be.true;
+    expect(await fetchTelegramVerificationStatus(db, dummyUserId, dummyChatId))
+      .to.be.true;
+  });
+
+  step("should be able to delete a verification", async function () {
+    await deleteTelegramVerification(db, dummyUserId, dummyChatId);
+    // Check that the user is no longer verified for access to the chat
+    expect(await fetchTelegramVerificationStatus(db, dummyUserId, dummyChatId))
+      .to.be.false;
   });
 });
