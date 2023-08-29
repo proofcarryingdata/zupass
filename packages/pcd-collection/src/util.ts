2f0276c00e0c40f7cbd4228d3a7ad2c457be768f
Ivan Chub <ivansergchub@gmail.com>
2023-08-28 22:17:05 -0700

    add a bunch of comments

--- packages/pcd-collection/src/util.ts
+++ packages/pcd-collection/src/util.ts
@@ -1,5 +1,16 @@
 import _ from "lodash";
 
+/**
+ * Gets the list of folders that are direct descendants of a given
+ * path, given a full list of directories that are present. For example,
+ * if the full list of paths is something like:
+ *
+ * a/b/c/d
+ * a/b/q/r
+ *
+ * ... then this function would say that the list of child folders of
+ * a/b is [c, q], even though a/b/c and a/b/q are not explicitly listed.
+ */
 export function getFoldersInFolder(
   folderPath: string,
   allPaths: string[]
@@ -20,6 +31,10 @@ export function getFoldersInFolder(
   return directDescendants;
 }
 
+/**
+ * For a path like a/b/c/d, returns a/b/c, a/b, a, and the root
+ * denoted by "".
+ */
 export function getAllAncestors(path: string): string[] {
   const parts = splitPath(path);
   const result = [];
@@ -32,6 +47,10 @@ export function getAllAncestors(path: string): string[] {
   return result;
 }
 
+/**
+ * Checks that a particular child path is a direct child of a
+ * particular parent.
+ */
 export function isChild(parent: string, child: string): boolean {
   const normalizedPath = normalizePath(parent);
   const descendantParts = splitPath(child);
@@ -44,6 +63,10 @@ export function isChild(parent: string, child: string): boolean {
   return false;
 }
 
+/**
+ * Checks if a particular path has {@link folderPath} as a descendant.
+ * eg. a/b/c/d is a descendant of a/b, but not of a/q.
+ */
 export function isFolderAncestor(path: string, folderPath: string): boolean {
   const pathParts = splitPath(path);
   const folderParts = splitPath(folderPath);
@@ -61,24 +84,41 @@ export function isFolderAncestor(path: string, folderPath: string): boolean {
   return true;
 }
 
+/**
+ * Splits a path via the path separator.
+ */
 export function splitPath(path: string): string[] {
   return path.split("/").filter((p) => p !== "");
 }
 
+/**
+ * Removes unnecessary path separators from a path.
+ */
 export function normalizePath(path: string): string {
   return splitPath(path).join("/");
 }
 
+/**
+ * For a path like a/b/c, returns a/b. for the root returns the root.
+ */
 export function getParentFolder(folderPath: string): string {
   const parts = splitPath(folderPath);
   parts.pop();
   return parts.join("/");
 }
 
+/**
+ * Returns whether or not this path is the root. Root canonically
+ * represented by an empty string, but also normalizes from /, //, ///, etc.
+ */
 export function isRootFolder(folderPath: string): boolean {
   return normalizePath(folderPath) === "";
 }
 
+/**
+ * Gets the last path segment of a path. Eg. a/b/c would return c.
+ * For root, returns root.
+ */
 export function getNameFromPath(path: string): string {
   const parts = splitPath(path);
 
