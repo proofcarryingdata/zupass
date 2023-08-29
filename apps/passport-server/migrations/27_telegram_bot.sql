974ce78fd4cde0b49d8f61ca1e3ed3914cbcb4b5
Rob Knight <mail@robknight.org.uk>
2023-08-29 20:12:17 +0200

    Fix unique constraints

--- apps/passport-server/migrations/27_telegram_bot.sql
+++ apps/passport-server/migrations/27_telegram_bot.sql
@@ -2,12 +2,14 @@
 create table telegram_bot_conversations (
   id SERIAL PRIMARY KEY,
   -- user_id corresponding the Telegram user
-  telegram_user_id BIGINT NOT NULL UNIQUE,
+  telegram_user_id BIGINT NOT NULL,
   -- chat_id between the bot and the Telegram user
-  telegram_chat_id BIGINT NOT NULL UNIQUE,
+  telegram_chat_id BIGINT NOT NULL,
   -- whether the user has been verified via PCDPass
   verified BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+CREATE UNIQUE INDEX user_chat ON telegram_bot_conversations (telegram_user_id, telegram_chat_id); 
 
 create table telegram_bot_events (
   -- the event ID matching the ticketData from the PCD
