bc24f4c96b6d1594052f99b7b8f05f4d413cf4dd
Rob Knight <mail@robknight.org.uk>
2023-08-31 13:54:49 +0200

    Updated eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,7 +4,7 @@ module.exports = {
     "turbo",
     "prettier",
     "turbo",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "react", "react-hooks"],
@@ -13,17 +13,18 @@ module.exports = {
     "no-case-declarations": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
-      "warn", // or "error"
+      "error",
       {
         argsIgnorePattern: "^_",
         varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
+        caughtErrorsIgnorePattern: "^_"
+      }
     ],
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "error",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
     "@typescript-eslint/no-empty-interface": "off",
-    "react/no-unescaped-entities": "off",
-  },
+    "react/no-unescaped-entities": "off"
+  }
 };
