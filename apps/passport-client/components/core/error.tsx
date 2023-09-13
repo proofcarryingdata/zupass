afc64e80ceee6aac77f188d2859eea77fee894b7
Richard Liu <richard.y.liu@berkeley.edu>
2023-09-13 16:54:35 -0700

    Password strength indicator (#538)
    Fixes #500 
    
    
    https://github.com/proofcarryingdata/zupass/assets/36896271/fb8f84b5-c4c2-45ba-82d2-83183d08bb21

---
+++ apps/passport-client/components/core/error.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ErrorMessage = styled.div`
+  color: var(--danger-bright);
+  text-align: center;
+  font-size: 14px;
+`;
---
+++ apps/passport-client/src/password.ts
@@ -0,0 +1,9 @@
+import zxcvbn from "zxcvbn";
+
+// Must not be "too guessable", "very guessable", or "somewhat guessable"
+export const MINIMUM_PASSWORD_STRENGTH = 2;
+
+export const checkPasswordStrength = (password: string): boolean => {
+  const { score } = zxcvbn(password);
+  return score >= MINIMUM_PASSWORD_STRENGTH;
+};
