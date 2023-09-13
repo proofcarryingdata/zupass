5496af34481c12189d05703cdc6e2e0e5c228e1f
Ivan Chub <ivansergchub@gmail.com>
2023-09-13 15:16:18 -0700

    autoformat

--- apps/load-testing/src/load-test.ts
+++ apps/load-testing/src/load-test.ts
@@ -8,16 +8,23 @@ import { uploadEncryptedStorage } from "./api/endToEndEncryptionApi";
 import { requestIssuedPCDs } from "./api/issuedPCDs";
 import { requestLoginCode, submitNewUser } from "./api/user";
 
-
 async function testSingleUser() {
-  const email = 'ivan' + Math.random() + '@0xparc.org';
+  const email = "ivan" + Math.random() + "@0xparc.org";
   const identity = new Identity();
   const crypto = await PCDCrypto.newInstance();
   const encryptionKey = await crypto.generateRandomKey();
-  const code: string | undefined = await requestLoginCode(email, identity.commitment.toString(), true);
-  const newUserResponse = await submitNewUser(email, code, identity.commitment.toString());
+  const code: string | undefined = await requestLoginCode(
+    email,
+    identity.commitment.toString(),
+    true
+  );
+  const newUserResponse = await submitNewUser(
+    email,
+    code,
+    identity.commitment.toString()
+  );
   const user = await newUserResponse.json();
-  console.log('logged in as user', user);
+  console.log("logged in as user", user);
   console.log("proving that I am a user");
   const request: IssuedPCDsRequest = {
     userProof: await SemaphoreSignaturePCDPackage.serialize(
@@ -40,9 +47,9 @@ async function testSingleUser() {
 
   const userLoop = async () => {
     console.log("proved that I am a user");
-    console.log("getting issued pcds")
-    const issuedPCDs = await requestIssuedPCDs(request)
-    console.log("get issued pcds", issuedPCDs)
+    console.log("getting issued pcds");
+    const issuedPCDs = await requestIssuedPCDs(request);
+    console.log("get issued pcds", issuedPCDs);
     console.log("saving e2ee data");
     const encryptedStorage = await passportEncrypt(
       JSON.stringify({
@@ -50,10 +57,10 @@ async function testSingleUser() {
       }),
       encryptionKey
     );
-    
+
     await uploadEncryptedStorage(encryptionKey, encryptedStorage);
     console.log("saved e2ee data");
-  }
+  };
 
   setInterval(() => {
     userLoop();
@@ -62,21 +69,26 @@ async function testSingleUser() {
 
 async function runLoadTest() {
   await SemaphoreSignaturePCDPackage.init({
-    wasmFilePath: "../passport-server/public/semaphore-artifacts/16.wasm", 
+    wasmFilePath: "../passport-server/public/semaphore-artifacts/16.wasm",
     zkeyFilePath: "../passport-server/public/semaphore-artifacts/16.zkey"
-  })
-  
+  });
+
   for (let i = 0; i < 25; i++) {
-    setTimeout(() => {
-      testSingleUser();
-    }, Math.random() * 1000 * 5)
+    setTimeout(
+      () => {
+        testSingleUser();
+      },
+      Math.random() * 1000 * 5
+    );
   }
 }
 
-runLoadTest().then(() => {
-  console.log("finished starting load test");
-  // process.exit(0);
-}).catch(e => {
-  console.log("error running load test");
-  console.log(e);
-});
+runLoadTest()
+  .then(() => {
+    console.log("finished starting load test");
+    // process.exit(0);
+  })
+  .catch((e) => {
+    console.log("error running load test");
+    console.log(e);
+  });
