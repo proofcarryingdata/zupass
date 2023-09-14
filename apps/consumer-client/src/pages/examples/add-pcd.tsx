proofcarryingdata/zupass

commit a0c2ce30b6ab9d572d54d8c01c884eabb08bb443

Generic issuance (#516)
Implements feeds, allowing third parties to make PCDs available to `passport-client`.

In order to resolve build issues, `consumer-client` has been converted to a regular React SPA, away from a NextJS app.

apps/consumer-client/src/pages/examples/add-pcd.tsx
@@ -1,0 +1,439 @@
+import { EdDSAPCDPackage } from "@pcd/eddsa-pcd";
+import {
+  EthereumGroupPCDPackage,
+  getRawPubKeyBuffer,
+  GroupType
+} from "@pcd/ethereum-group-pcd";
+import { EthereumOwnershipPCDPackage } from "@pcd/ethereum-ownership-pcd";
+import {
+  constructPassportPcdAddRequestUrl,
+  constructPassportPcdProveAndAddRequestUrl,
+  openSignedZuzaluSignInPopup,
+  usePassportPopupMessages
+} from "@pcd/passport-interface";
+import { ArgumentTypeName, SerializedPCD } from "@pcd/pcd-types";
+import { SemaphoreGroupPCDPackage } from "@pcd/semaphore-group-pcd";
+import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
+import { SemaphoreSignaturePCDPackage } from "@pcd/semaphore-signature-pcd";
+import { WebAuthnPCDPackage } from "@pcd/webauthn-pcd";
+import { Poseidon, Tree } from "@personaelabs/spartan-ecdsa";
+import { Identity } from "@semaphore-protocol/identity";
+import { startRegistration } from "@simplewebauthn/browser";
+import {
+  generateRegistrationOptions,
+  verifyRegistrationResponse
+} from "@simplewebauthn/server";
+import { ethers } from "ethers";
+import JSONBig from "json-bigint";
+import { useEffect, useState } from "react";
+import { v4 as uuid } from "uuid";
+import { HomeLink } from "../../components/Core";
+import { ExampleContainer } from "../../components/ExamplePage";
+import { ZUPASS_URL, ZUZALU_SEMAPHORE_GROUP_URL } from "../../constants";
+import { sendPassportRequest } from "../../util";
+
+/**
+ * Example 3rd party application page which shows how to add
+ * PCDs into the passport.
+ */
+export default function Page() {
+  return (
+    <div>
+      <HomeLink />
+      <h2>Prove and Add</h2>
+      <p>
+        This page contains several examples of how to add PCDs to the passport.
+        You can add a PCD to the passport in one of two ways:
+      </p>
+      <ul>
+        <li>
+          Add a PCD (which can be kind of dangerous if the user then expects
+          that PCD to be private, as is the case for adding a raw Semaphore
+          Identity).
+        </li>
+        <li>
+          Prove, and <i>then</i> add the PCD to the passport. The application
+          that initiates this does not get a copy of the PCD back, it just adds
+          it to the passport.
+        </li>
+      </ul>
+      <ExampleContainer>
+        <button onClick={addGroupMembershipProofPCD}>
+          prove and add a group membership proof
+        </button>
+        <br />
+        <br />
+        <button onClick={addSignatureProofPCD}>
+          prove and add a signature proof
+        </button>
+        <br />
+        <br />
+        <button onClick={addIdentityPCD}>
+          add a new semaphore identity to the passport
+        </button>
+        <br />
+        <br />
+        <button onClick={addWebAuthnPCD}>
+          add a new webauthn credential to the passport
+        </button>
+        <br />
+        <br />
+        <AddEthAddrPCDButton />
+        <br />
+        <br />
+        <AddEthGroupPCDButton />
+        <br />
+        <br />
+        <button onClick={addEdDSAPCD}>add a new EdDSA signature proof</button>
+      </ExampleContainer>
+    </div>
+  );
+}
+
+function AddEthAddrPCDButton() {
+  const [pcdStr] = usePassportPopupMessages();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (!pcdStr) return;
+    if (!isActive) return;
+
+    const parsed = JSON.parse(pcdStr) as SerializedPCD;
+
+    const ethereum = (window as any).ethereum;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    if (!ethereum) {
+      alert("Please install MetaMask to use this dApp!");
+    }
+
+    (async function () {
+      await ethereum.request({ method: "eth_requestAccounts" });
+      const pcd = await SemaphoreSignaturePCDPackage.deserialize(parsed.pcd);
+      const signature = await provider
+        .getSigner()
+        .signMessage(pcd.claim.identityCommitment);
+
+      const popupUrl = window.location.origin + "/popup";
+
+      const proofUrl = constructPassportPcdProveAndAddRequestUrl<
+        typeof EthereumOwnershipPCDPackage
+      >(ZUPASS_URL, popupUrl, EthereumOwnershipPCDPackage.name, {
+        identity: {
+          argumentType: ArgumentTypeName.PCD,
+          pcdType: SemaphoreIdentityPCDPackage.name,
+          value: undefined,
+          userProvided: true,
+          description:
+            "The Semaphore Identity which you are proving owns the given Ethereum address."
+        },
+        ethereumAddress: {
+          argumentType: ArgumentTypeName.String,
+          value: await provider.getSigner().getAddress()
+        },
+        ethereumSignatureOfCommitment: {
+          argumentType: ArgumentTypeName.String,
+          value: signature
+        }
+      });
+
+      sendPassportRequest(proofUrl);
+    })();
+
+    setIsActive(false);
+  }, [pcdStr, isActive]);
+
+  return (
+    <button
+      onClick={() => {
+        setIsActive(true);
+        zupassSignIn("eth-pcd");
+      }}
+    >
+      add a new Ethereum address to the passport
+    </button>
+  );
+}
+
+async function zupassSignIn(originalSiteName: string) {
+  openSignedZuzaluSignInPopup(
+    ZUPASS_URL,
+    window.location.origin + "/popup",
+    originalSiteName
+  );
+}
+
+function AddEthGroupPCDButton() {
+  const [pcdStr] = usePassportPopupMessages();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (!pcdStr) return;
+    if (!isActive) return;
+
+    const parsed = JSON.parse(pcdStr) as SerializedPCD;
+
+    const ethereum = (window as any).ethereum;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    if (!ethereum) {
+      alert("Please install MetaMask to use this dApp!");
+    }
+
+    (async function () {
+      await ethereum.request({ method: "eth_requestAccounts" });
+      const pcd = await SemaphoreSignaturePCDPackage.deserialize(parsed.pcd);
+
+      const msgHash = Buffer.from(
+        ethers.utils.hashMessage(pcd.claim.identityCommitment).slice(2),
+        "hex"
+      );
+      const signatureOfIdentityCommitment = await provider
+        .getSigner()
+        .signMessage(pcd.claim.identityCommitment);
+
+      const poseidon = new Poseidon();
+      await poseidon.initWasm();
+      const treeDepth = 20; // Provided circuits have tree depth = 20
+      const pubKeyTree = new Tree(treeDepth, poseidon);
+
+      // Add some public keys to the tree
+      for (const member of [
+        "0x04b4d5188949bf70c4db5e965a9ea67b80407e8ee7fa3a260ccf86e9c0395fe82cba155fdff55829b3c862322aba402d00b563861b603879ee8ae211c34257d4ad",
+        "0x042d21e6aa2021a991a82d08591fa0528d0bebe4ac9a34d851a74507327d930dec217380bd602fe48a143bb21106ab274d6a51aff396f0e4f7e1e3a8a673d46d83"
+      ]) {
+        pubKeyTree.insert(poseidon.hashPubKey(getRawPubKeyBuffer(member)));
+      }
+      // Add the prover's public key to the tree
+      const proverPubkeyBuffer: Buffer = getRawPubKeyBuffer(
+        ethers.utils.recoverPublicKey(msgHash, signatureOfIdentityCommitment)
+      );
+      pubKeyTree.insert(poseidon.hashPubKey(proverPubkeyBuffer));
+      const pubKeyIndex = pubKeyTree.indexOf(
+        poseidon.hashPubKey(proverPubkeyBuffer)
+      ); // == 2 in this test
+
+      // Prove membership of the prover's public key in the tree
+      const merkleProof = pubKeyTree.createProof(pubKeyIndex);
+
+      const popupUrl = window.location.origin + "/popup";
+      const proofUrl = constructPassportPcdProveAndAddRequestUrl<
+        typeof EthereumGroupPCDPackage
+      >(ZUPASS_URL, popupUrl, EthereumGroupPCDPackage.name, {
+        identity: {
+          argumentType: ArgumentTypeName.PCD,
+          pcdType: SemaphoreIdentityPCDPackage.name,
+          value: undefined,
+          userProvided: true,
+          description:
+            "The Semaphore Identity which you are signing the message."
+        },
+        groupType: {
+          argumentType: ArgumentTypeName.String,
+          value: GroupType.PUBLICKEY
+        },
+        signatureOfIdentityCommitment: {
+          argumentType: ArgumentTypeName.String,
+          value: signatureOfIdentityCommitment
+        },
+        merkleProof: {
+          argumentType: ArgumentTypeName.String,
+          value: JSONBig({ useNativeBigInt: true }).stringify(merkleProof)
+        }
+      });
+
+      sendPassportRequest(proofUrl);
+    })();
+
+    setIsActive(false);
+  }, [pcdStr, isActive]);
+
+  return (
+    <button
+      onClick={() => {
+        setIsActive(true);
+        zupassSignIn("eth-group-pcd");
+      }}
+    >
+      add a new Ethereum Group Membership to the passport
+    </button>
+  );
+}
+
+async function addGroupMembershipProofPCD() {
+  const url = constructPassportPcdProveAndAddRequestUrl<
+    typeof SemaphoreGroupPCDPackage
+  >(
+    ZUPASS_URL,
+    window.location.origin + "/popup",
+    SemaphoreGroupPCDPackage.name,
+    {
+      externalNullifier: {
+        argumentType: ArgumentTypeName.BigInt,
+        userProvided: true,
+        value: "1",
+        description:
+          "You can choose a nullifier to prevent this signed message from being used across domains."
+      },
+      group: {
+        argumentType: ArgumentTypeName.Object,
+        userProvided: false,
+        remoteUrl: ZUZALU_SEMAPHORE_GROUP_URL,
+        description: "The Semaphore group which you are proving you belong to."
+      },
+      identity: {
+        argumentType: ArgumentTypeName.PCD,
+        pcdType: SemaphoreIdentityPCDPackage.name,
+        value: undefined,
+        userProvided: true,
+        description:
+          "The Semaphore Identity which you are signing the message on behalf of."
+      },
+      signal: {
+        argumentType: ArgumentTypeName.BigInt,
+        userProvided: true,
+        value: "1",
+        description: "The message you are signing with your Semaphore identity."
+      }
+    },
+    {
+      genericProveScreen: true,
+      description:
+        "Generate a group membership proof using your passport's Semaphore Identity.",
+      title: "Group Membership Proof"
+    }
+  );
+
+  sendPassportRequest(url);
+}
+
+async function addEdDSAPCD() {
+  const proofUrl = constructPassportPcdProveAndAddRequestUrl<
+    typeof EdDSAPCDPackage
+  >(
+    ZUPASS_URL,
+    window.location.origin + "/popup",
+    EdDSAPCDPackage.name,
+    {
+      message: {
+        argumentType: ArgumentTypeName.StringArray,
+        value: ["0x12345", "0x54321"],
+        userProvided: true
+      },
+      privateKey: {
+        argumentType: ArgumentTypeName.String,
+        userProvided: false,
+        // Key borrowed from https://github.com/iden3/circomlibjs/blob/4f094c5be05c1f0210924a3ab204d8fd8da69f49/test/eddsa.js#L103
+        value:
+          "0001020304050607080900010203040506070809000102030405060708090001"
+      },
+      id: {
+        argumentType: ArgumentTypeName.String,
+        value: uuid(),
+        userProvided: false
+      }
+    },
+    { title: "EdDSA Signature Proof" }
+  );
+
+  sendPassportRequest(proofUrl);
+}
+
+async function addSignatureProofPCD() {
+  const proofUrl = constructPassportPcdProveAndAddRequestUrl<
+    typeof SemaphoreSignaturePCDPackage
+  >(
+    ZUPASS_URL,
+    window.location.origin + "/popup",
+    SemaphoreSignaturePCDPackage.name,
+    {
+      identity: {
+        argumentType: ArgumentTypeName.PCD,
+        pcdType: SemaphoreIdentityPCDPackage.name,
+        value: undefined,
+        userProvided: true
+      },
+      signedMessage: {
+        argumentType: ArgumentTypeName.String,
+        value: "1",
+        userProvided: false
+      }
+    },
+    {
+      title: "Semaphore Signature Proof"
+    }
+  );
+
+  sendPassportRequest(proofUrl);
+}
+
+async function addIdentityPCD() {
+  const newIdentity = await SemaphoreIdentityPCDPackage.prove({
+    identity: new Identity()
+  });
+
+  const serializedNewIdentity =
+    await SemaphoreIdentityPCDPackage.serialize(newIdentity);
+
+  const url = constructPassportPcdAddRequestUrl(
+    ZUPASS_URL,
+    window.location.origin + "/popup",
+    serializedNewIdentity
+  );
+
+  sendPassportRequest(url);
+}
+
+async function addWebAuthnPCD() {
+  // Register a new WebAuthn credential for testing.
+  const generatedRegistrationOptions = await generateRegistrationOptions({
+    rpName: "consumer-client",
+    rpID: window.location.hostname,
+    userID: "user-id",
+    userName: "user",
+    attestationType: "direct",
+    challenge: "challenge",
+    supportedAlgorithmIDs: [-7]
+  });
+  const startRegistrationResponse = await startRegistration(
+    generatedRegistrationOptions
+  );
+  const verificationResponse = await verifyRegistrationResponse({
+    response: startRegistrationResponse,
+    expectedOrigin: window.location.origin,
+    expectedChallenge: generatedRegistrationOptions.challenge,
+    supportedAlgorithmIDs: [-7] // support ES256 signing algorithm
+  });
+
+  if (!verificationResponse.registrationInfo) {
+    throw new Error("Registration failed the return correct response.");
+  }
+
+  // Get relevant credential arguments from registration response.
+  const { credentialID, credentialPublicKey, counter } =
+    verificationResponse.registrationInfo;
+
+  // Create new WebAuthn PCD. This process initiates the WebAuth
+  // authentication ceremony, prompting a authorization gesture like
+  // a fingerprint or Face ID scan, depending on the device.
+  const newCredential = await WebAuthnPCDPackage.prove({
+    rpID: window.location.hostname,
+    authenticator: {
+      credentialID,
+      credentialPublicKey,
+      counter
+    },
+    challenge: "1", // arbitrary challenge to be signed
+    origin: window.location.origin
+  });
+
+  const serializedNewCredential =
+    await WebAuthnPCDPackage.serialize(newCredential);
+
+  // Add new WebAuthn PCD to Passport.
+  const url = constructPassportPcdAddRequestUrl(
+    ZUPASS_URL,
+    window.location.origin + "/popup",
+    serializedNewCredential
+  );
+
+  sendPassportRequest(url);
+}

apps/consumer-client/src/pages/examples/get-without-proving.tsx
@@ -1,0 +1,52 @@
+import {
+  getWithoutProvingUrl,
+  usePassportPopupMessages
+} from "@pcd/passport-interface";
+import { SemaphoreSignaturePCDPackage } from "@pcd/semaphore-signature-pcd";
+import { useMemo } from "react";
+import { CollapsableCode, HomeLink } from "../../components/Core";
+import { ExampleContainer } from "../../components/ExamplePage";
+import { ZUPASS_URL } from "../../constants";
+import { sendPassportRequest } from "../../util";
+
+/**
+ * Example page which shows how to get a PCD from the passport without
+ * proving it first.
+ */
+export default function Page() {
+  const [passportPCDStr] = usePassportPopupMessages();
+
+  const formatted = useMemo(() => {
+    try {
+      return JSON.stringify(JSON.parse(passportPCDStr), null, 2);
+    } catch (e) {
+      return null;
+    }
+  }, [passportPCDStr]);
+
+  return (
+    <>
+      <HomeLink />
+      <h2>Get Without Proving</h2>
+      <p>
+        This page shows a working example of how to request a PCD from the
+        passport without proving it first.
+      </p>
+      <ExampleContainer>
+        <button onClick={getProofWithoutProving}>
+          get pcd without proving
+        </button>
+        {formatted && <CollapsableCode code={formatted} />}
+      </ExampleContainer>
+    </>
+  );
+}
+
+function getProofWithoutProving() {
+  const url = getWithoutProvingUrl(
+    ZUPASS_URL,
+    window.location.origin + "/popup",
+    SemaphoreSignaturePCDPackage.name
+  );
+  sendPassportRequest(url);
+}

commit cec79c79c3a4e36c42ae2f04c82265a7a5837d66

fix consumer client popup (#585)
bug introduced in https://github.com/proofcarryingdata/zupass/pull/516

apps/consumer-client/src/pages/examples/add-pcd.tsx
@@ -113,7 +113,7 @@ function AddEthAddrPCDButton() {
         .getSigner()
         .signMessage(pcd.claim.identityCommitment);
 
-      const popupUrl = window.location.origin + "/popup";
+      const popupUrl = window.location.origin + "#/popup";
 
       const proofUrl = constructPassportPcdProveAndAddRequestUrl<
         typeof EthereumOwnershipPCDPackage
@@ -157,7 +157,7 @@ function AddEthAddrPCDButton() {
 async function zupassSignIn(originalSiteName: string) {
   openSignedZuzaluSignInPopup(
     ZUPASS_URL,
-    window.location.origin + "/popup",
+    window.location.origin + "#/popup",
     originalSiteName
   );
 }
@@ -214,7 +214,7 @@ function AddEthGroupPCDButton() {
       // Prove membership of the prover's public key in the tree
       const merkleProof = pubKeyTree.createProof(pubKeyIndex);
 
-      const popupUrl = window.location.origin + "/popup";
+      const popupUrl = window.location.origin + "#/popup";
       const proofUrl = constructPassportPcdProveAndAddRequestUrl<
         typeof EthereumGroupPCDPackage
       >(ZUPASS_URL, popupUrl, EthereumGroupPCDPackage.name, {
@@ -263,7 +263,7 @@ async function addGroupMembershipProofPCD() {
     typeof SemaphoreGroupPCDPackage
   >(
     ZUPASS_URL,
-    window.location.origin + "/popup",
+    window.location.origin + "#/popup",
     SemaphoreGroupPCDPackage.name,
     {
       externalNullifier: {
@@ -310,7 +310,7 @@ async function addEdDSAPCD() {
     typeof EdDSAPCDPackage
   >(
     ZUPASS_URL,
-    window.location.origin + "/popup",
+    window.location.origin + "#/popup",
     EdDSAPCDPackage.name,
     {
       message: {
@@ -342,7 +342,7 @@ async function addSignatureProofPCD() {
     typeof SemaphoreSignaturePCDPackage
   >(
     ZUPASS_URL,
-    window.location.origin + "/popup",
+    window.location.origin + "#/popup",
     SemaphoreSignaturePCDPackage.name,
     {
       identity: {
@@ -375,7 +375,7 @@ async function addIdentityPCD() {
 
   const url = constructPassportPcdAddRequestUrl(
     ZUPASS_URL,
-    window.location.origin + "/popup",
+    window.location.origin + "#/popup",
     serializedNewIdentity
   );
 
@@ -431,7 +431,7 @@ async function addWebAuthnPCD() {
   // Add new WebAuthn PCD to Passport.
   const url = constructPassportPcdAddRequestUrl(
     ZUPASS_URL,
-    window.location.origin + "/popup",
+    window.location.origin + "#/popup",
     serializedNewCredential
   );
 

apps/consumer-client/src/pages/examples/get-without-proving.tsx
@@ -45,7 +45,7 @@ export default function Page() {
 function getProofWithoutProving() {
   const url = getWithoutProvingUrl(
     ZUPASS_URL,
-    window.location.origin + "/popup",
+    window.location.origin + "#/popup",
     SemaphoreSignaturePCDPackage.name
   );
   sendPassportRequest(url);

apps/consumer-client/src/pages/examples/group-proof.tsx
@@ -4,12 +4,12 @@ import {
   usePassportPopupMessages,
   usePCDMultiplexer,
   usePendingPCD,
-  useSemaphoreGroupProof,
+  useSemaphoreGroupProof
 } from "@pcd/passport-interface";
 import { ArgumentTypeName } from "@pcd/pcd-types";
 import {
   generateMessageHash,
-  SemaphoreGroupPCDPackage,
+  SemaphoreGroupPCDPackage
 } from "@pcd/semaphore-group-pcd";
 import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
 import { useState } from "react";
@@ -19,7 +19,7 @@ import { PendingPCDStatusDisplay } from "../../components/PendingPCDStatusDispla
 import {
   ZUPASS_SERVER_URL,
   ZUPASS_URL,
-  ZUZALU_SEMAPHORE_GROUP_URL,
+  ZUZALU_SEMAPHORE_GROUP_URL
 } from "../../constants";
 
 /**
@@ -138,7 +138,7 @@ function requestMembershipProof(
   proveOnServer: boolean,
   originalSiteName: string
 ) {
-  const popupUrl = window.location.origin + "/popup";
+  const popupUrl = window.location.origin + "#/popup";
   const proofUrl = constructPassportPcdGetRequestUrl<
     typeof SemaphoreGroupPCDPackage
   >(
@@ -151,13 +151,13 @@ function requestMembershipProof(
         userProvided: true,
         value: generateMessageHash(originalSiteName).toString(),
         description:
-          "You can choose a nullifier to prevent this signed message from being used across domains.",
+          "You can choose a nullifier to prevent this signed message from being used across domains."
       },
       group: {
         argumentType: ArgumentTypeName.Object,
         userProvided: false,
         remoteUrl: ZUZALU_SEMAPHORE_GROUP_URL,
-        description: "The Semaphore group which you are proving you belong to.",
+        description: "The Semaphore group which you are proving you belong to."
       },
       identity: {
         argumentType: ArgumentTypeName.PCD,
@@ -165,15 +165,14 @@ function requestMembershipProof(
         value: undefined,
         userProvided: true,
         description:
-          "The Semaphore Identity which you are signing the message on behalf of.",
+          "The Semaphore Identity which you are signing the message on behalf of."
       },
       signal: {
         argumentType: ArgumentTypeName.BigInt,
         userProvided: true,
         value: "1",
-        description:
-          "The message you are signing with your Semaphore identity.",
-      },
+        description: "The message you are signing with your Semaphore identity."
+      }
     },
     {
       genericProveScreen: true,
@@ -181,7 +180,7 @@ function requestMembershipProof(
         "Generate a group membership proof using your passport's Semaphore Identity.",
       title: "Group Membership Proof",
       debug: debug,
-      proveOnServer: proveOnServer,
+      proveOnServer: proveOnServer
     }
   );
 

apps/consumer-client/src/pages/examples/signature-proof.tsx
@@ -101,7 +101,7 @@ export default function Page() {
 }
 
 function requestSemaphoreSignature(proveOnServer: boolean) {
-  const popupUrl = window.location.origin + "/popup";
+  const popupUrl = window.location.origin + "#/popup";
   const proofUrl = constructPassportPcdGetRequestUrl<
     typeof SemaphoreSignaturePCDPackage
   >(

apps/consumer-client/src/pages/pcdpass-examples/group-proof.tsx
@@ -58,7 +58,7 @@ export default function Page() {
           onClick={() =>
             openGroupMembershipPopup(
               PCDPASS_URL,
-              window.location.origin + "/popup",
+              window.location.origin + "#/popup",
               PCDPASS_SEMAPHORE_GROUP_URL,
               "consumer-client"
             )

apps/consumer-client/src/pages/pcdpass-examples/signature-proof.tsx
@@ -62,7 +62,7 @@ export default function Page() {
             () =>
               openSemaphoreSignaturePopup(
                 PCDPASS_URL,
-                window.location.origin + "/popup",
+                window.location.origin + "#/popup",
                 messageToSign,
                 serverProving
               ),

apps/consumer-client/src/pages/pcdpass-examples/zkeddsa-proof.tsx
@@ -9,10 +9,10 @@ import { ArgumentTypeName } from "@pcd/pcd-types";
 import { SemaphoreIdentityPCDPackage } from "@pcd/semaphore-identity-pcd";
 import {
   EdDSATicketFieldsToReveal,
+  generateMessageHash,
   ZKEdDSATicketPCD,
   ZKEdDSATicketPCDArgs,
-  ZKEdDSATicketPCDPackage,
-  generateMessageHash
+  ZKEdDSATicketPCDPackage
 } from "@pcd/zk-eddsa-ticket-pcd";
 import path from "path";
 import { useEffect, useMemo, useState } from "react";
@@ -108,7 +108,7 @@ export default function Page() {
           onClick={() =>
             openZKEdDSATicketPopup(
               PCDPASS_URL,
-              window.location.origin + "/popup",
+              window.location.origin + "#/popup",
               fieldsToReveal,
               watermark,
               externalNullifier

apps/consumer-client/src/pages/zuzalu-examples/group-proof.tsx
@@ -62,7 +62,7 @@ export default function Page() {
           onClick={() =>
             openGroupMembershipPopup(
               ZUPASS_URL,
-              window.location.origin + "/popup",
+              window.location.origin + "#/popup",
               ZUZALU_SEMAPHORE_GROUP_URL,
               "consumer-client"
             )

apps/consumer-client/src/pages/zuzalu-examples/sign-in.tsx
@@ -89,7 +89,7 @@ export default function Page() {
           onClick={() =>
             openSignedZuzaluSignInPopup(
               ZUPASS_URL,
-              window.location.origin + "/popup",
+              window.location.origin + "#/popup",
               "consumer-client"
             )
           }

apps/consumer-client/src/pages/zuzalu-examples/signature-proof.tsx
@@ -66,7 +66,7 @@ export default function Page() {
             () =>
               openSemaphoreSignaturePopup(
                 ZUPASS_URL,
-                window.location.origin + "/popup",
+                window.location.origin + "#/popup",
                 messageToSign,
                 serverProving
               ),

apps/consumer-client/src/pages/zuzalu-examples/uuid-proof.tsx
@@ -1,8 +1,8 @@
 import {
-  openSignedZuzaluUUIDPopup,
-  useFetchUser,
-  usePassportPopupMessages,
-  useSemaphoreSignatureProof
+    openSignedZuzaluUUIDPopup,
+    useFetchUser,
+    usePassportPopupMessages,
+    useSemaphoreSignatureProof
 } from "@pcd/passport-interface";
 import { useEffect, useState } from "react";
 import { CollapsableCode, HomeLink } from "../../components/Core";
@@ -60,7 +60,7 @@ export default function Page() {
           onClick={() =>
             openSignedZuzaluUUIDPopup(
               ZUPASS_URL,
-              window.location.origin + "/popup",
+              window.location.origin + "#/popup",
               "consumer-client"
             )
           }