0ce6d8d5c97040e605512b232f694e2b435e9b23
Ivan Chub <ichub@users.noreply.github.com>
2023-09-15 07:48:43 -0700

    follow up to #585 (#590)

--- apps/consumer-client/src/util.ts
+++ apps/consumer-client/src/util.ts
@@ -3,6 +3,6 @@
  * Open the popup window under the current domain, let it redirect there.
  */
 export function sendPassportRequest(proofUrl: string) {
-  const popupUrl = `/popup?proofUrl=${encodeURIComponent(proofUrl)}`;
+  const popupUrl = `#/popup?proofUrl=${encodeURIComponent(proofUrl)}`;
   window.open(popupUrl, "_blank", "width=360,height=480,top=100,popup");
 }
