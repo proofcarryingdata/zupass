proofcarryingdata/zupass

commit 8e516f2fa51744b8d025c861284e774b9154990f

fix flickering background color (#603)
When loading PCDPass app, we always get the `--bg-dark-primary` bg color
from the static css file first. However, many screens use `gray` bg
colors and it creates a odd experience when loading a path directly. For
example, opening the /prove link from Telegram, one would see a
flickering background color.

If we simply remove the background color in the stylesheet, the
background color ended up to be a flash of white on first paint because
AppContainer is not yet loaded. Indeed, this is a common problem
especially for websites with dark mode:
https://stackoverflow.com/questions/37126401/modify-bodys-classes-before-first-paint

This PR introduces an eagerly executed js blob that pattern matches on
location hash. This ensure the bg color is correctly set on first paint.
It also refactor the AppContainer bg to switch on a centralized list of
routes config. We still want to keep the AppContainer injection because
the hand written js might not always give the correct result as the
HashRouter.

Tested locally with both `yarn dev` and `yarn build` to ensure build can
access the config file.

See the comparison between this branch and production build



https://github.com/proofcarryingdata/zupass/assets/4317392/6d952d0b-27a7-4b64-a53f-2029f19c187a

relates to https://github.com/proofcarryingdata/zupass/issues/597

---------

Co-authored-by: Forest Fang <4317392+saurfang@users.noreply.github.com>

apps/passport-client/src/appConfig.ts
@@ -12,6 +12,8 @@ interface AppConfig {
   rollbarToken: string | undefined;
   // the environment to which the client uploads errors in rollbar
   rollbarEnvName: string | undefined;
+  // path without leading slash for routes that use gray background color
+  grayBackgroundRoutes: string[];
 }
 
 export const appConfig: AppConfig = {
@@ -20,7 +22,15 @@ export const appConfig: AppConfig = {
   maxIdentityProofAgeMs: 1000 * 60 * 60 * 4,
   isZuzalu: process.env.IS_ZUZALU === "true" ? true : false,
   rollbarToken: process.env.ROLLBAR_TOKEN,
-  rollbarEnvName: process.env.ROLLBAR_ENV_NAME
+  rollbarEnvName: process.env.ROLLBAR_ENV_NAME,
+  grayBackgroundRoutes: [
+    "", // home
+    "get-without-proving",
+    "halo",
+    "add",
+    "prove",
+    "scan"
+  ]
 };
 
 console.log("App Config: " + JSON.stringify(appConfig));

apps/passport-client/build.ts
@@ -6,6 +6,7 @@ import fs from "fs";
 import Handlebars from "handlebars";
 import * as path from "path";
 import { v4 as uuid } from "uuid";
+import { appConfig } from "./src/appConfig";
 
 dotenv.config();
 
@@ -129,7 +130,8 @@ function compileHtml() {
 
   const html = template({
     title: IS_ZUZALU ? "Zuzalu Passport" : "PCDpass",
-    cssPath: IS_ZUZALU ? "/global-zupass.css" : "/global-pcdpass.css"
+    cssPath: IS_ZUZALU ? "/global-zupass.css" : "/global-pcdpass.css",
+    grayBgPaths: appConfig.grayBackgroundRoutes.join("|")
   });
 
   fs.writeFileSync(path.join("public", "index.html"), html);

apps/passport-client/components/screens/AddScreen/AddScreen.tsx
@@ -56,7 +56,7 @@ export function AddScreen() {
 
   if (screen == null) {
     // Need AppContainer to display error
-    return <AppContainer bg="gray" />;
+    return <AppContainer />;
   }
   return screen;
 }

apps/passport-client/components/screens/AddScreen/JustAddScreen.tsx
@@ -51,7 +51,7 @@ export function JustAddScreen({ request }: { request: PCDAddRequest }) {
   }
 
   return (
-    <AppContainer bg="gray">
+    <AppContainer>
       <MaybeModal fullScreen />
       <Container>
         <Spacer h={16} />

apps/passport-client/components/screens/AddScreen/ProveAndAddScreen.tsx
@@ -64,7 +64,7 @@ export function ProveAndAddScreen({
   }
 
   return (
-    <AppContainer bg="gray">
+    <AppContainer>
       <MaybeModal fullScreen />
       <Container>
         <Spacer h={24} />

apps/passport-client/components/screens/AlreadyRegisteredScreen.tsx
@@ -132,7 +132,7 @@ export function AlreadyRegisteredScreen() {
   return (
     <>
       <MaybeModal />
-      <AppContainer bg="primary">
+      <AppContainer>
         <BackgroundGlow
           y={224}
           from="var(--bg-lite-primary)"

apps/passport-client/components/screens/CreatePasswordScreen.tsx
@@ -90,7 +90,7 @@ export function CreatePasswordScreen() {
   };
 
   return (
-    <AppContainer bg="primary">
+    <AppContainer>
       <BackgroundGlow
         y={224}
         from="var(--bg-lite-primary)"

apps/passport-client/components/screens/DevconnectCheckinScreen.tsx
@@ -151,7 +151,7 @@ function TicketError({
   }
 
   return (
-    <AppContainer bg={"primary"}>
+    <AppContainer>
       <Container>
         {showTicket && <TicketInfoSection ticketData={ticketData} />}
         <ErrorContainer>{errorContent}</ErrorContainer>
@@ -198,7 +198,7 @@ function UserReadyForCheckin({
   ticket: EdDSATicketPCD;
 }) {
   return (
-    <AppContainer bg={"primary"}>
+    <AppContainer>
       <Container>
         <TicketInfoSection ticketData={ticketData} />
         <CheckInSection ticket={ticket} />

commit 5891af277f1d6650c61d3828eba1d0e1de87f03b

fix conflict caused by #603

apps/passport-client/components/screens/AddSubscriptionScreen.tsx
@@ -6,9 +6,9 @@ import {
   Subscription
 } from "@pcd/passport-interface";
 import {
-  PCDPermission,
   isAppendToFolderPermission,
-  isReplaceInFolderPermission
+  isReplaceInFolderPermission,
+  PCDPermission
 } from "@pcd/pcd-collection";
 import {
   ArgumentTypeName,
@@ -82,7 +82,7 @@ export function AddSubscriptionScreen() {
   const alreadyFetched = fetchedProviderUrl === providerUrl;
 
   return (
-    <AppContainer bg="gray">
+    <AppContainer>
       <SubscriptionNavigation to="/subscriptions"></SubscriptionNavigation>
       <SubscriptionsScreenContainer>
         <Spacer h={16} />

apps/passport-client/components/screens/SubscriptionsScreen.tsx
@@ -16,7 +16,7 @@ export function SubscriptionsScreen() {
   }, []);
 
   return (
-    <AppContainer bg="gray">
+    <AppContainer>
       <MaybeModal />
       <SubscriptionNavigation to="/"></SubscriptionNavigation>
       <Container>

apps/passport-client/src/appConfig.ts
@@ -29,7 +29,9 @@ export const appConfig: AppConfig = {
     "halo",
     "add",
     "prove",
-    "scan"
+    "scan",
+    "add-subscription",
+    "subscriptions"
   ]
 };
 