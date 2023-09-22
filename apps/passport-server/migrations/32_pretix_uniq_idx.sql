01d87a44bf4cee9a2ca9099047d3adf82112e0cb
cha0sg0d <42984734+cha0sg0d@users.noreply.github.com>
2023-09-22 11:19:43 -0400

    Remove public from table IDs
    Co-authored-by: Richard Liu <richard.y.liu@berkeley.edu>

--- apps/passport-server/migrations/32_pretix_uniq_idx.sql
+++ apps/passport-server/migrations/32_pretix_uniq_idx.sql
@@ -1,14 +1,14 @@
 ALTER TABLE
-    "public"."pretix_events_config"
+    pretix_events_config
 ADD
     UNIQUE ("event_id");
 
 ALTER TABLE
-    "public"."devconnect_pretix_events_info"
+    devconnect_pretix_events_info
 ADD
     UNIQUE ("pretix_events_config_id");
 
 ALTER TABLE
-    "public"."devconnect_pretix_items_info"
+    devconnect_pretix_items_info
 ADD
     UNIQUE ("item_id", "devconnect_pretix_events_info_id");
