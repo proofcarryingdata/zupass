ae68b9ef005ddffa4bdb355dff065b9caf000ff5
Richard Liu <richard.y.liu@berkeley.edu>
2023-09-22 13:44:19 -0700

    remove migrations

--- apps/passport-server/migrations/33_unique_telegram_bot_event.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE
-    telegram_bot_events
-ADD
-    UNIQUE ("ticket_event_id", "telegram_chat_id");
-
-ALTER TABLE telegram_bot_events
-ADD CONSTRAINT fk_telegram_bot_events_ticket_event_id
-FOREIGN KEY (ticket_event_id)
-REFERENCES pretix_events_config(id);
