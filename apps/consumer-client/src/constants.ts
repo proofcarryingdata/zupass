1e6a49b36e3f526d9935a2b253cd4da5fde72dcb
cedoor <me@cedoor.dev>
2023-09-23 12:37:04 +0100

    chore: add staging env to consumer app

--- apps/consumer-client/src/constants.ts
+++ apps/consumer-client/src/constants.ts
@@ -1,13 +1,18 @@
 export const PCD_GITHUB_URL = "https://github.com/proofcarryingdata/pcd";
 
 export const IS_PROD = process.env.NODE_ENV === "production";
+export const IS_STAGING = process.env.NODE_ENV === "staging";
 
 export const ZUPASS_URL = IS_PROD
   ? "https://zupass.org/"
+  : IS_STAGING
+  ? "https://staging.zupass.org/"
   : "http://localhost:3000/";
 
 export const PCDPASS_URL = IS_PROD
   ? "https://pcdpass.xyz/"
+  : IS_STAGING
+  ? "https://staging.pcdpass.xyz/"
   : "http://localhost:3000/";
 
 export const ZUPASS_SERVER_URL = IS_PROD
