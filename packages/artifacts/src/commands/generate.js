97c52fbcc6cbfb22eeb34bffcea6efc41def63f3
Richard Liu <richard.y.liu@berkeley.edu>
2023-09-24 16:08:26 -0700

    Revert "postbuild fixes"
    This reverts commit 4b3625507d4c00dfc3e3e16f6cf146f8bcd3af4d.

--- packages/artifacts/src/commands/generate.js
+++ packages/artifacts/src/commands/generate.js
@@ -24,127 +24,113 @@ program
     "-o, --output <output>",
     "Path to the directory where the output will be written [default: artifacts]."
   )
-  .option(
-    "-f, --force",
-    "If present, forcibly overwrites the old artifacts directory rather than asking for confirmation."
-  )
-  .action(
-    async ({
-      ptauPower = 13,
-      output: outputPath = "artifacts",
-      force: forceOverwrite = false
-    }) => {
-      const ptauFilePath = `${tmpdir()}/${
-        pkg.name
-      }/powersOfTau28_hez_final_${ptauPower}.ptau`;
+  .action(async ({ ptauPower = 13, output: outputPath = "artifacts" }) => {
+    const ptauFilePath = `${tmpdir()}/${
+      pkg.name
+    }/powersOfTau28_hez_final_${ptauPower}.ptau`;
 
-      // Download the ptau file if it does not exist.
-      if (!existsSync(ptauFilePath)) {
-        const spinner = new Spinner(`Downloading ptau file from Hermez...`);
+    // Download the ptau file if it does not exist.
+    if (!existsSync(ptauFilePath)) {
+      const spinner = new Spinner(`Downloading ptau file from Hermez...`);
 
-        spinner.start();
-
-        try {
-          const ptauUrl = `https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_${ptauPower}.ptau`;
-
-          await download(ptauUrl, `${tmpdir()}/${pkg.name}`);
-
-          spinner.stop();
-
-          console.info(
-            `${logSymbols.success}`,
-            "Ptau file has been downloaded"
-          );
-        } catch (error) {
-          spinner.stop();
-
-          console.info(`${logSymbols.error}`, `${error}`);
-
-          process.exit(1);
-        }
-      }
+      spinner.start();
 
       try {
-        if (existsSync(outputPath)) {
-          if (
-            forceOverwrite ||
-            (await confirm(
-              `The '${outputPath}' directory already exists. Do you want to remove it?`
-            ))
-          ) {
-            // Remove old artifacts.
-            await executeCommand(`rm -fr ${outputPath}`);
-          } else {
-            console.info(
-              `${logSymbols.info}`,
-              "You need to remove the old directory if you want to generate new artifacts"
-            );
+        const ptauUrl = `https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_${ptauPower}.ptau`;
 
-            process.exit(0);
-          }
-        }
+        await download(ptauUrl, `${tmpdir()}/${pkg.name}`);
 
-        // Create the artifacts folder.
-        await executeCommand(`mkdir ${outputPath}`);
+        spinner.stop();
 
-        // Compile circuit.
-        await executeCommand(
-          `circom circuits/index.circom --r1cs --wasm -o ${outputPath}`
-        );
-
-        // Generate the first zkey file.
-        await zKey.newZKey(
-          `${outputPath}/index.r1cs`,
-          ptauFilePath,
-          `${outputPath}/index_0.zkey`,
-          logger
-        );
-
-        // Dummy contribution.
-        await zKey.contribute(
-          `${outputPath}/index_0.zkey`,
-          `${outputPath}/index_1.zkey`,
-          "Unsafe devmode contribution",
-          "Random text"
-        );
-
-        // Create the final zkey file.
-        await zKey.beacon(
-          `${outputPath}/index_1.zkey`,
-          `${outputPath}/circuit.zkey`,
-          "Final beacon",
-          "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
-          10,
-          logger
-        );
-
-        // Export the verification key.
-        const verificationKey = await zKey.exportVerificationKey(
-          `${outputPath}/circuit.zkey`,
-          logger
-        );
-
-        writeFileSync(
-          `${outputPath}/circuit.json`,
-          JSON.stringify(verificationKey, null, 4)
-        );
-
-        // Remove unnecessary files.
-        await executeCommand(
-          `mv ${outputPath}/index_js/index.wasm ${outputPath}/circuit.wasm`
-        );
-        await executeCommand(`rm -fr ${outputPath}/index*`);
-
-        console.info(
-          `\n${logSymbols.success}`,
-          "Dummy zero-knowledge artifacts have been generated"
-        );
-
-        process.exit(0);
+        console.info(`${logSymbols.success}`, "Ptau file has been downloaded");
       } catch (error) {
+        spinner.stop();
+
         console.info(`${logSymbols.error}`, `${error}`);
 
         process.exit(1);
       }
     }
-  );
+
+    try {
+      if (existsSync(outputPath)) {
+        if (
+          await confirm(
+            `The '${outputPath}' directory already exists. Do you want to remove it?`
+          )
+        ) {
+          // Remove old artifacts.
+          await executeCommand(`rm -fr ${outputPath}`);
+        } else {
+          console.info(
+            `${logSymbols.info}`,
+            "You need to remove the old directory if you want to generate new artifacts"
+          );
+
+          process.exit(0);
+        }
+      }
+
+      // Create the artifacts folder.
+      await executeCommand(`mkdir ${outputPath}`);
+
+      // Compile circuit.
+      await executeCommand(
+        `circom circuits/index.circom --r1cs --wasm -o ${outputPath}`
+      );
+
+      // Generate the first zkey file.
+      await zKey.newZKey(
+        `${outputPath}/index.r1cs`,
+        ptauFilePath,
+        `${outputPath}/index_0.zkey`,
+        logger
+      );
+
+      // Dummy contribution.
+      await zKey.contribute(
+        `${outputPath}/index_0.zkey`,
+        `${outputPath}/index_1.zkey`,
+        "Unsafe devmode contribution",
+        "Random text"
+      );
+
+      // Create the final zkey file.
+      await zKey.beacon(
+        `${outputPath}/index_1.zkey`,
+        `${outputPath}/circuit.zkey`,
+        "Final beacon",
+        "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+        10,
+        logger
+      );
+
+      // Export the verification key.
+      const verificationKey = await zKey.exportVerificationKey(
+        `${outputPath}/circuit.zkey`,
+        logger
+      );
+
+      writeFileSync(
+        `${outputPath}/circuit.json`,
+        JSON.stringify(verificationKey, null, 4)
+      );
+
+      // Remove unnecessary files.
+      await executeCommand(
+        `mv ${outputPath}/index_js/index.wasm ${outputPath}/circuit.wasm`
+      );
+      await executeCommand(`rm -fr ${outputPath}/index*`);
+
+      console.info(
+        `\n${logSymbols.success}`,
+        "Dummy zero-knowledge artifacts have been generated"
+      );
+
+      process.exit(0);
+    } catch (error) {
+      console.info(`${logSymbols.error}`, `${error}`);
+
+      process.exit(1);
+    }
+  });
