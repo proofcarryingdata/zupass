be16fd1e63b81cb2b388bd42d50e20d72b72b483
Ivan Chub <ivansergchub@gmail.com>
2023-09-25 16:33:33 -0700

    enable issuance service loading for zupass

--- apps/passport-client/components/shared/MainIdentityCard.tsx
+++ apps/passport-client/components/shared/MainIdentityCard.tsx
@@ -1,8 +1,8 @@
-import { DateRange, User, ZuzaluUserRole } from "@pcd/passport-interface";
+import { User } from "@pcd/passport-interface";
 import Avatar from "boring-avatars";
 import styled from "styled-components";
 import { useSelf } from "../../src/appHooks";
-import { H3, InfoLine, Spacer, TextCenter } from "../core";
+import { Spacer, TextCenter } from "../core";
 
 export function MainIdentityCard({ user }: { user?: User }) {
   const self = useSelf();
@@ -12,7 +12,6 @@ export function MainIdentityCard({ user }: { user?: User }) {
     <CardBody>
       <Spacer h={24} />
       <TextCenter>
-        <H3 col="var(--primary-dark)">{actualUser.name}</H3>
         <Avatar
           size={300}
           variant="bauhaus"
@@ -20,37 +19,9 @@ export function MainIdentityCard({ user }: { user?: User }) {
           square={false}
           colors={["#FFAD08", "#EDD75A", "#73B06F", "#0C8F8F", "#405059"]}
         />
-        <VisitorDateSection user={actualUser} />
       </TextCenter>
+      <Spacer h={16} />
     </CardBody>
-  );
-}
-
-function VisitorDateSection({ user }: { user?: User }) {
-  if (!user) return null;
-  if (user.role !== ZuzaluUserRole.Visitor) return null;
-  if (!user.visitor_date_ranges) return null;
-
-  return (
-    <>
-      <InfoLine>
-        <b>Visitor Dates:</b>
-      </InfoLine>
-      {user.visitor_date_ranges.map((range, i) => (
-        <InfoLine key={i}>
-          <DateRangeText range={range} />
-        </InfoLine>
-      ))}
-    </>
-  );
-}
-
-function DateRangeText({ range }: { range: DateRange }) {
-  return (
-    <span>
-      {new Date(range.date_from).toDateString()} -{" "}
-      {new Date(range.date_to).toDateString()}
-    </span>
   );
 }
 
@@ -59,24 +30,3 @@ const CardBody = styled.div`
   color: var(--primary-dark);
   border-radius: 0 0 12px 12px;
 `;
-
-const Footer = styled.div<{ role: string; notCurrent: boolean }>`
-  font-size: 20px;
-  letter-spacing: 1px;
-  background: ${(p) => {
-    if (p.notCurrent) {
-      return "var(--danger)";
-    }
-
-    return highlight(p.role) ? "var(--accent-lite)" : "var(--primary-dark)";
-  }};
-  color: ${(p) => (highlight(p.role) ? "var(--primary-dark)" : "var(--white)")};
-  /* Must be slightly lower than the card's border-radius to nest correctly. */
-  border-radius: 0 0 10px 10px;
-  padding: 8px;
-  text-align: center;
-`;
-
-function highlight(role: string) {
-  return role === "resident" || role === "organizer";
-}
