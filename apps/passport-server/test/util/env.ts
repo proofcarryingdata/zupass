3464f5f0d305c3475e2e835436739e27a9837ec8
Ivan Chub <ivansergchub@gmail.com>
2023-09-25 17:25:40 -0700

    remove explicit pretix config from 'pcdpassTestingEnv' to prevent confusion

--- apps/passport-server/test/util/env.ts
+++ apps/passport-server/test/util/env.ts
@@ -31,11 +31,7 @@ export const zuzaluTestingEnv: EnvironmentVariables = Object.freeze({
 
 export const pcdpassTestingEnv: EnvironmentVariables = Object.freeze({
   ...zuzaluTestingEnv,
-  IS_ZUZALU: "false",
-  PRETIX_ORG_URL: "pretix_org_url",
-  PRETIX_TOKEN: "pretix_token",
-  PRETIX_VISITOR_EVENT_ID: "visitor_event_id",
-  PRETIX_ZU_EVENT_ID: "zu_event_id"
+  IS_ZUZALU: "false"
 });
 
 export async function overrideEnvironment(
