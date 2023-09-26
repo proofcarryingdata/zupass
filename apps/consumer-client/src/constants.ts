6f475443cd59e02ade56b8d64b89c94b58d6db26
Cedoor <me@cedoor.dev>
2023-09-26 16:56:07 +0100

    Staging environment in consumer app (#679)
    Closes #661

--- apps/consumer-client/src/constants.ts
+++ apps/consumer-client/src/constants.ts
@@ -1,27 +1,40 @@
 export const PCD_GITHUB_URL = "https://github.com/proofcarryingdata/pcd";
 
 export const IS_PROD = process.env.NODE_ENV === "production";
+export const IS_STAGING = process.env.NODE_ENV === "staging";
 
 export const ZUPASS_URL = IS_PROD
   ? "https://zupass.org/"
+  : IS_STAGING
+  ? "https://staging.zupass.org/"
   : "http://localhost:3000/";
 
 export const PCDPASS_URL = IS_PROD
   ? "https://pcdpass.xyz/"
+  : IS_STAGING
+  ? "https://staging.pcdpass.xyz/"
   : "http://localhost:3000/";
 
 export const ZUPASS_SERVER_URL = IS_PROD
   ? "https://api.zupass.org/"
+  : IS_STAGING
+  ? "https://api-staging.zupass.org/"
   : "http://localhost:3002/";
 
 export const PCDPASS_SERVER_URL = IS_PROD
   ? "https://api.pcdpass.xyz/"
+  : IS_STAGING
+  ? "https://api-staging.pcdpass.xyz/"
   : "http://localhost:3002/";
 
 export const ZUZALU_SEMAPHORE_GROUP_URL = IS_PROD
   ? "https://api.zupass.org/semaphore/1"
+  : IS_STAGING
+  ? "https://api-staging.zupass.org/semaphore/1"
   : "http://localhost:3002/semaphore/1";
 
 export const PCDPASS_SEMAPHORE_GROUP_URL = IS_PROD
   ? "https://api.pcdpass.xyz/semaphore/5"
+  : IS_STAGING
+  ? "https://api-staging.pcdpass.xyz/semaphore/5"
   : "http://localhost:3002/semaphore/5";
