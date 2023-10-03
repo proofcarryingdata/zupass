43a96933bdaa24ed62fa1d4266df2a42b46c63bc
Cedoor <me@cedoor.dev>
2023-10-03 19:34:34 +0200

    docs: update eddsa util comments

--- packages/eddsa-pcd/src/util/util.ts
+++ packages/eddsa-pcd/src/util/util.ts
@@ -1,9 +1,7 @@
 import { getRandomValues, toHexString } from "@pcd/util";
 
 /**
- * Create a new EdDSA private key.
- * 
- * @returns an EdDSA private key as hexadecimal string.
+ * Creates a new EdDSA private key generating a cryptographically strong random 32-byte value. 
  */
 export function newEdDSAPrivateKey(): string {
   return toHexString(getRandomValues(32));
