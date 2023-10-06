aa96be963dc4bca79228e353a51d3565016263f6
Richard Liu <richard.y.liu@berkeley.edu>
2023-10-06 16:55:42 -0700

    add password also forces "Session Invalid" modal

--- apps/passport-client/src/dispatch.ts
+++ apps/passport-client/src/dispatch.ts
@@ -378,10 +378,8 @@ async function setSelf(self: User, state: AppState, update: ZuUpdate) {
   let userMismatched = false;
   let hasChangedPassword = false;
 
-  if (state.self?.salt != null && self.salt != state.self.salt) {
+  if (state.self && self.salt != state.self.salt) {
     // If the password has been changed on a different device, the salts will mismatch
-    // However, skip if the salt was undefined previously, as that means we just added
-    // a new password to an acount that didn't have a password previously.
     console.log("User salt mismatch");
     hasChangedPassword = true;
     requestLogToServer(
