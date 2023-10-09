f2993d007ff6add93a33ed9f1248cb76df031531
lermchair <13699109+lermchair@users.noreply.github.com>
2023-10-09 16:26:59 -0700

    loading states

--- apps/anon-message-client/src/app/page.tsx
+++ apps/anon-message-client/src/app/page.tsx
@@ -12,7 +12,7 @@ import sha256 from "js-sha256";
 import { useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 
-const MAX_HEADER_SIZE = 8192; // max header size (8kb) for nodejs
+const MAX_HEADER_SIZE = 280; // max tweet size
 
 function getMessageWatermark(message: string): bigint {
   const hashed = sha256.sha256(message).substring(0, 16);
@@ -50,7 +50,7 @@ function constructZupassPcdGetRequestUrl<T extends PCDPackage>(
   return `${zupassClientUrl}#/prove?request=${encReq}`;
 }
 
-function requestProof(
+async function requestProof(
   message: string,
   topicId: string,
   validEventIds: string[]
@@ -100,7 +100,7 @@ function requestProof(
     process.env.NEXT_PUBLIC_PASSPORT_SERVER_URL
   }/telegram/message?message=${encodeURIComponent(message)}&topicId=${topicId}`;
 
-  const proofUrl = constructZupassPcdGetRequestUrl<
+  const proofUrl = await constructZupassPcdGetRequestUrl<
     typeof ZKEdDSAEventTicketPCDPackage
   >(passportOrigin, returnUrl, ZKEdDSAEventTicketPCDPackage.name, args, {
     genericProveScreen: true,
@@ -116,6 +116,7 @@ export default function () {
   const [message, setMessage] = useState("");
   const [messageInvalid, setMessageInvalid] = useState(false);
   const [topicData, setTopicData] = useState<TopicData | undefined>();
+  const [loadingProofUrl, setLoadingProofUrl] = useState(false);
   const searchParams = useSearchParams();
   const topicDataRaw = searchParams.get("tgWebAppStartParam");
 
@@ -127,49 +128,76 @@ export default function () {
   }, [topicDataRaw]);
 
   useEffect(() => {
-    const headerSize = Buffer.byteLength(message);
-    if (headerSize > MAX_HEADER_SIZE) {
+    if (message.length > MAX_HEADER_SIZE) {
       setMessageInvalid(true);
     } else if (messageInvalid) {
       setMessageInvalid(false);
     }
   }, [message]);
 
-  const onClick = useCallback(() => {
+  const onClick = useCallback(async () => {
+    setLoadingProofUrl(true);
     if (!topicData || !topicData.topicId || !topicData.validEventIds) return;
-    requestProof(message, topicData.topicId, topicData.validEventIds);
+    await requestProof(message, topicData.topicId, topicData.validEventIds);
+    setLoadingProofUrl(false);
   }, [message]);
 
-  return (
-    <div className="w-screen h-screen flex flex-col items-center bg-[#037EE5] p-4">
-      <span className="text-white font-bold my-4">
-        {topicData ? `Post to #${topicData.topicName}` : "Post with zk-TG "}
-      </span>
-      <div className="flex flex-col gap-2 bg-[#50ACF9] rounded-lg w-full p-2">
-        <textarea
-          placeholder="Type your anonymous message here"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className={`border-2 ${
-            messageInvalid ? "border-red-500" : ""
-          } text-2xl rounded-lg text-black resize-none p-2 h-[30vh]`}
-        />
-      </div>
-      <div className="mt-8 text-center flex flex-col w-full">
-        {messageInvalid && (
-          <div className="p-2 bg-red-500 rounded-md mb-2">
-            <span className="text-white pb-2">Message is too long!</span>
-          </div>
-        )}
-        <span className="text-white pb-2">🔒 Anonymous posting</span>
-        <button
-          onClick={onClick}
-          className="w-full bg-white text-[#037ee5] text-xl font-bold px-4 rounded-full focus:outline-none focus:shadow-outline py-4 disabled:opacity-40"
-          disabled={messageInvalid}
+  if (!topicData) {
+    return (
+      <div className="w-screen h-screen flex flex-col items-center bg-[#037EE5] p-4">
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
         >
-          Send to channel
-        </button>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="w-screen h-screen flex flex-col items-center bg-[#037EE5] p-4">
+        <span className="text-white font-bold my-4">
+          {`Post to #${topicData.topicName}`}
+        </span>
+        <div className="flex flex-col gap-2 bg-[#50ACF9] rounded-lg w-full p-2">
+          <textarea
+            placeholder="Type your anonymous message here"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className={`border-2 ${
+              messageInvalid ? "border-red-500" : ""
+            } text-2xl rounded-lg text-black resize-none p-2 h-[30vh]`}
+          />
+        </div>
+        <div className="mt-8 text-center flex flex-col w-full">
+          {messageInvalid && (
+            <div className="p-2 bg-red-500 rounded-md mb-2">
+              <span className="text-white pb-2">Message is too long!</span>
+            </div>
+          )}
+          <span className="text-white pb-2">🔒 Anonymous posting</span>
+          <button
+            onClick={onClick}
+            className="w-full bg-white text-[#037ee5] text-xl font-bold px-4 rounded-full focus:outline-none focus:shadow-outline py-4 disabled:opacity-40"
+            disabled={messageInvalid}
+          >
+            {loadingProofUrl ? (
+              <div
+                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status"
+              >
+                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                  Loading...
+                </span>
+              </div>
+            ) : (
+              `Send to ${topicData.topicName}`
+            )}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
