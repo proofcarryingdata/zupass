91292b10f6b612b3947241fe70fa75cc9f029c7d
Richard Liu <richard.y.liu@berkeley.edu>
2023-10-09 15:49:19 -0700

    test localstorage

--- apps/device-client/src/app/page.tsx
+++ apps/device-client/src/app/page.tsx
@@ -1,23 +1,39 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [message, setMessage] = useState("");
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const onClick = () => alert(message);
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      console.log(e.code);
+    };
+    window.addEventListener("keydown", listener, { capture: true });
+    return () =>
+      window.removeEventListener("keydown", listener, { capture: true });
+  }, []);
+
+  const onSave = () => {
+    localStorage.setItem("message", message);
+  };
 
   return (
-    <div className="w-screen h-screen flex flex-col items-center bg-[#19473f] p-4">
+    <div
+      onKeyDown={(e) => {
+        console.log("hey", e);
+        setMessage((m) => m + e.key);
+      }}
+      className="w-screen h-screen flex flex-col items-center bg-[#19473f] p-4"
+    >
       <span className="text-white font-bold my-4">Device Scanner</span>
       <div className="flex flex-col gap-2 bg-[#206b5e] rounded-lg w-full p-2">
         <textarea
           ref={textareaRef}
           placeholder="Enter QR code output here"
           autoFocus
-          onBlur={() => textareaRef.current?.focus()}
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           className="border-2 text-2xl rounded-lg text-black resize-none p-2 h-[30vh]"
@@ -26,10 +42,19 @@ export default function Home() {
       <div className="mt-8 text-center flex flex-col w-full">
         <button
           disabled={!message}
-          onClick={onClick}
+          onClick={() => alert(localStorage.getItem("message"))}
           className="w-full bg-white text-[#19473f] text-xl font-bold px-4 rounded-full focus:outline-none focus:shadow-outline py-4"
         >
-          Scan
+          Get
+        </button>
+      </div>
+      <div className="mt-8 text-center flex flex-col w-full">
+        <button
+          disabled={!message}
+          onClick={onSave}
+          className="w-full bg-white text-[#19473f] text-xl font-bold px-4 rounded-full focus:outline-none focus:shadow-outline py-4"
+        >
+          Save
         </button>
       </div>
     </div>
