b4867b9f41ea28c3fb6273244d27dac1b75992d3
cha0sg0d <0xcha0sg0d@gmail.com>
2023-10-09 11:39:00 -0700

    feat: handle migration to staging

--- apps/passport-server/migrations/40_multiple_anon_topics.sql
+++ apps/passport-server/migrations/40_multiple_anon_topics.sql
@@ -3,6 +3,11 @@ create table telegram_chats (
 	primary key (telegram_chat_id)
 );
 
+INSERT INTO telegram_chats (telegram_chat_id)
+SELECT DISTINCT telegram_chat_id FROM telegram_bot_events
+UNION
+SELECT DISTINCT telegram_chat_id FROM telegram_bot_conversations;
+
 alter table telegram_bot_events 
 add foreign key (telegram_chat_id) references telegram_chats(telegram_chat_id),
 drop column anon_chat_id;
