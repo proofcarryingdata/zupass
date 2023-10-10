909484f1960b91ee4dd806dade71f108a3007c05
cha0sg0d <0xcha0sg0d@gmail.com>
2023-10-10 14:35:24 -0700

    chore: default true on migration

--- apps/passport-server/migrations/41_chat_topics.sql
+++ apps/passport-server/migrations/41_chat_topics.sql
@@ -4,4 +4,4 @@ ALTER TABLE telegram_chat_anon_topics RENAME TO telegram_chat_topics;
 -- Rename the columns
 ALTER TABLE telegram_chat_topics RENAME COLUMN anon_topic_id TO topic_id;
 ALTER TABLE telegram_chat_topics RENAME COLUMN anon_topic_name TO topic_name;
-ALTER TABLE telegram_chat_topics ADD COLUMN is_anon_topic BOOLEAN;
+ALTER TABLE telegram_chat_topics ADD COLUMN is_anon_topic BOOLEAN DEFAULT TRUE;
