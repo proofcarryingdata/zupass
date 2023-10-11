814a096ef76c4d4aee22c491ee0f2771885d903b
Rob Knight <mail@robknight.org.uk>
2023-10-11 10:18:22 -0700

    Make QR code redirection more robust

--- apps/passport-client/src/qr.ts
+++ apps/passport-client/src/qr.ts
@@ -1,5 +1,5 @@
 export function makeEncodedVerifyLink(encodedPCD: string): string {
-  const link = `${window.location.origin}#/verify?pcd=${encodeURIComponent(
+  const link = `${window.location.origin}/#/verify?pcd=${encodeURIComponent(
     encodedPCD
   )}`;
   return link;
