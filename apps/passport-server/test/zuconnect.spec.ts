9e36ef5309a5721a2073702ccf1c5065499bf82c
Rob Knight <mail@robknight.org.uk>
2023-10-12 19:21:41 -0700

    More tests

--- apps/passport-server/test/zuconnect.spec.ts
+++ apps/passport-server/test/zuconnect.spec.ts
@@ -15,7 +15,10 @@ import "mocha";
 import MockDate from "mockdate";
 import { SetupServer } from "msw/lib/node";
 import { Pool } from "postgres-pool";
-import { ZuconnectTripshaAPI } from "../src/apis/zuconnect/zuconnectTripshaAPI";
+import {
+  ZuconnectTicket,
+  ZuconnectTripshaAPI
+} from "../src/apis/zuconnect/zuconnectTripshaAPI";
 import { stopApplication } from "../src/application";
 import { getDB } from "../src/database/postgresPool";
 import { fetchAllZuconnectTickets } from "../src/database/queries/zuconnect/fetchZuconnectTickets";
@@ -262,5 +265,63 @@ describe("zuconnect functionality", function () {
 
     expect(response?.success).to.be.true;
     expect(response?.value?.verified).to.be.true;
+  });
+
+  let userWithTwoTicketsRow;
+  it("should allow a user to have more than one ticket", async () => {
+    // Note that this is not the email used for the previous user
+    const result = await testLogin(application, goodResponse.tickets[4].email, {
+      expectEmailIncorrect: false,
+      expectUserAlreadyLoggedIn: false,
+      force: false,
+      skipSetupPassword: false
+    });
+
+    if (!result) {
+      throw new Error("failed to log in");
+    }
+
+    expect(result).to.not.be.undefined;
+
+    userWithTwoTicketsRow = result;
+
+    const extraTicket: ZuconnectTicket = {
+      ...goodResponse.tickets[4],
+      type: "ZuConnect Organizer Pass",
+      id: randomUUID()
+    };
+
+    server.use(
+      makeHandler({
+        tickets: goodResponse.tickets.concat([extraTicket])
+      })
+    );
+    await zuconnectTripshaSyncService.sync();
+    const tickets = await fetchAllZuconnectTickets(db);
+    expect(tickets.length).to.eq(6);
+
+    MockDate.set(new Date());
+    const payload = JSON.stringify(createFeedCredentialPayload());
+    const response = await pollFeed(
+      application.expressContext.localEndpoint,
+      userWithTwoTicketsRow.identity,
+      payload,
+      ZupassFeedIds.Zuconnect_23
+    );
+    MockDate.reset();
+
+    expect(response.success).to.be.true;
+
+    expect(response.value?.actions.length).to.eq(2);
+    const populateAction = response.value?.actions[1] as ReplaceInFolderAction;
+    expect(populateAction.type).to.eq(PCDActionType.ReplaceInFolder);
+    expect(populateAction.folder).to.eq("Zuconnect");
+    expect(populateAction.pcds.length).to.eq(2);
+    for (const pcd of populateAction.pcds) {
+      expect(
+        (await EdDSATicketPCDPackage.deserialize(pcd.pcd)).claim.ticket
+          .attendeeEmail
+      ).to.eq(extraTicket.email);
+    }
   });
 });
