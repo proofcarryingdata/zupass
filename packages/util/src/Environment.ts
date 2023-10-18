667d8efbf51a0789d9bf7c9cf79696fd0ea93b86
Cedoor <me@cedoor.dev>
2023-10-19 00:36:29 +0100

    New `@pcd/util` function to check if WASM is supported (#1000)
    #956

--- packages/util/src/Environment.ts
+++ packages/util/src/Environment.ts
@@ -12,9 +12,43 @@ export function isNode(): boolean {
 }
 
 /**
- * Secure method check if the environment is a browser.
+ * Secure method to check if the environment is a browser.
  * @returns true if the environment is a browser, false otherwise.
  */
 export function isBrowser(): boolean {
   return new Function("try {return this===window}catch(e){ return false}")();
+}
+
+/**
+ * Secure method to check if Web Assembly is supported.
+ * It does the following:
+ *   Check whether WebAssembly is accessible in the current scope.
+ *   See whether it has the '.instantiate' function.
+ *   Try to synchronously compile the smallest possible module
+ *   See if we get a WebAssembly.Module out of it.
+ *   Finally, try to synchronously instantiate that module, and check that it's a WebAssembly.Instance.
+ * More info on: https://stackoverflow.com/questions/47879864/how-can-i-check-if-a-browser-supports-webassembly.
+ * @returns true if WASM is supported, false otherwise.
+ */
+export function isWebAssemblySupported() {
+  try {
+    if (
+      typeof WebAssembly === "object" &&
+      typeof WebAssembly.instantiate === "function"
+    ) {
+      const module = new WebAssembly.Module(
+        Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
+      );
+
+      if (module instanceof WebAssembly.Module) {
+        return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
+      }
+
+      return false;
+    }
+
+    return false;
+  } catch (e) {
+    return false;
+  }
 }
