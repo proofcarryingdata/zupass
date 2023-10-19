b9be680e5d7ed38dc6feb9e5126e73626487d44d
Ivan Chub <ivansergchub@gmail.com>
2023-10-19 12:47:12 -0700

    starting by adding some empty tests

---
+++ apps/passport-server/test/offline-checkin.spec.ts
@@ -0,0 +1,7 @@
+import { logger } from "../src/util/logger";
+
+describe.only("offline checkin for zuconnect and devconnect", function () {
+  it("should work", function () {
+    logger("test");
+  });
+});
---
+++ apps/passport-server/test/offline-checkindb.spec.ts
@@ -0,0 +1,7 @@
+import { logger } from "../src/util/logger";
+
+describe.only("offline checkin database queries should work", function () {
+  it("should work", function () {
+    logger("test");
+  });
+});
