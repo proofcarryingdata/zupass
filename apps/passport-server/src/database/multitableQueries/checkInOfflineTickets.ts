3a0ad3477d3d654ca04e0ba3b72edcb4553129d7
Ivan Chub <ivansergchub@gmail.com>
2023-10-20 17:56:01 -0700

    add comment to checkin offline tickets

--- apps/passport-server/src/database/multitableQueries/checkInOfflineTickets.ts
+++ apps/passport-server/src/database/multitableQueries/checkInOfflineTickets.ts
@@ -9,6 +9,12 @@ import {
 import { consumeDevconnectPretixTicket } from "../queries/devconnect_pretix_tickets/updateDevconnectPretixTicket";
 import { fetchUserByCommitment } from "../queries/users";
 
+/**
+ * Attempts to check in the given devconnect tickets on behalf of the user
+ * with the given commitment. Does not error in cases where the user does not
+ * have permission, or the ticket does not exist, etc., in order to be robust
+ * to ticketing settings having changed since the user was last online.
+ */
 export async function checkInOfflineTickets(
   dbPool: Pool,
   checkerCommitment: string,
