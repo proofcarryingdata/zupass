6e42aa8a5e25c18220669291ba59a0f53d3bfc0a
Richard Liu <richard.y.liu@berkeley.edu>
2023-10-25 12:21:31 -0700

    Revert "Switch to sendgrid (#1084)"
    This reverts commit e10adfbc0c40564c7f4668dc429fb038193bd8db.

--- apps/passport-server/src/apis/emailAPI.ts
+++ apps/passport-server/src/apis/emailAPI.ts
@@ -1,34 +1,13 @@
-import sendgrid from "@sendgrid/mail";
 import request from "request";
-import { logger } from "../util/logger";
-
-interface SendEmailParams {
-  from: string;
-  to: string;
-  subject: string;
-  text: string;
-  html: string;
-}
 
 export interface IEmailAPI {
-  send: (args: SendEmailParams) => Promise<void>;
-}
-
-export async function sendgridSendEmail({
-  from,
-  to,
-  subject,
-  text,
-  html
-}: SendEmailParams): Promise<void> {
-  const message = await sendgrid.send({
-    to,
-    from,
-    subject,
-    text,
-    html
-  });
-  logger("[EMAIL] Sending email via Sendgrid", message);
+  send: (args: {
+    from: string;
+    to: string;
+    subject: string;
+    text: string;
+    html: string;
+  }) => Promise<void>;
 }
 
 export function mailgunSendEmail({
@@ -37,7 +16,13 @@ export function mailgunSendEmail({
   subject,
   text,
   html
-}: SendEmailParams): Promise<void> {
+}: {
+  from: string;
+  to: string;
+  subject: string;
+  text: string;
+  html: string;
+}): Promise<void> {
   return new Promise<void>((resolve, reject) => {
     request(
       "https://api.mailgun.net/v3/zupass.org/messages",
