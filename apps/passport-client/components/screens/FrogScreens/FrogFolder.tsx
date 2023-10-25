f8e93361c7f34f38e752d082a7f482c2a4f03428
Forest Fang <saurfang@users.noreply.github.com>
2023-10-25 13:17:06 -0700

    bounce frog text (#1078)
    https://github.com/proofcarryingdata/zupass/issues/1056
    
    
    
    https://github.com/proofcarryingdata/zupass/assets/4317392/a4a7d9c8-5007-44ff-8b65-b1681a17cf81
    
    ---------
    
    Co-authored-by: Forest Fang <4317392+saurfang@users.noreply.github.com>
    Co-authored-by: Ivan Chub <ivansergchub@gmail.com>

--- apps/passport-client/components/screens/FrogScreens/FrogFolder.tsx
+++ apps/passport-client/components/screens/FrogScreens/FrogFolder.tsx
@@ -29,13 +29,13 @@ export function FrogFolder({
         width={18}
         height={18}
       />
-      <img
-        draggable="false"
-        src="/images/frogs/frogcrypto.svg"
-        height={12}
-        width={135}
-        style={{ transform: "translate(-2.5px, 1px)" }}
-      />
+      <FunkyFont>
+        {"FROGCRYPTO".split("").map((letter, i) => (
+          <BounceText key={i} delay={i * 0.1}>
+            {letter}
+          </BounceText>
+        ))}
+      </FunkyFont>
       <NewFont>
         <CountDown />
       </NewFont>
@@ -182,13 +182,81 @@ const NewFont = styled.div`
 
   @keyframes color-change {
     0% {
-      color: #fc9575;
+      color: #ff9900;
     }
     50% {
-      color: #83b692;
+      color: #afffbc;
     }
     100% {
-      color: #fc9575;
+      color: #ff9900;
+    }
+  }
+`;
+
+const FunkyFont = styled.div`
+  font-family: "SuperFunky";
+  font-size: 20px;
+  display: flex;
+
+  * {
+    background-size: 100%;
+    background-color: #ff9900;
+    background-image: linear-gradient(45deg, #ff9900, #afffbc);
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-text-fill-color: transparent;
+  }
+`;
+
+const BounceText = styled.span<{ delay: number }>`
+  animation: bounce 5s infinite ${(p) => p.delay}s;
+
+  @keyframes bounce {
+    0% {
+      transform: scale(1, 1) translateY(0);
+    }
+    2% {
+      transform: scale(1.1, 0.9) translateY(0);
+    }
+    5% {
+      transform: scale(0.9, 1.1) translateY(-10px);
+    }
+    10% {
+      transform: scale(1.05, 0.95) translateY(0);
+    }
+    12% {
+      transform: scale(1, 1) translateY(-2px);
+    }
+    15% {
+      transform: scale(1, 1) translateY(0);
+    }
+    100% {
+      transform: scale(1, 1) translateY(0);
+    }
+  }
+
+  @-webkit-keyframes bounce {
+    0% {
+      transform: scale(1, 1) translateY(0);
+    }
+    2% {
+      transform: scale(1.1, 0.9) translateY(0);
+    }
+    5% {
+      transform: scale(0.9, 1.1) translateY(-10px);
+    }
+    10% {
+      transform: scale(1.05, 0.95) translateY(0);
+    }
+    12% {
+      transform: scale(1, 1) translateY(-2px);
+    }
+    15% {
+      transform: scale(1, 1) translateY(0);
+    }
+    100% {
+      transform: scale(1, 1) translateY(0);
     }
   }
 `;
