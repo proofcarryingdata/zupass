1d8a96c2c3b1b282e41a1bd3f3b1134700c3097b
Eric Zhang <zhangeric99@gmail.com>
2023-10-25 18:09:38 -0400

    fix process env config

--- apps/kudosbot-client/src/constants.ts
+++ apps/kudosbot-client/src/constants.ts
@@ -1,7 +1,11 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 export const PCD_GITHUB_URL = "https://github.com/proofcarryingdata/pcd";
 export const IS_PROD = process.env.NODE_ENV === "production";
 export const IS_STAGING = process.env.NODE_ENV === "staging";
-export const IS_LOCAL_HTTPS = process.env.IS_LOCAL_HTTPS;
+export const IS_LOCAL_HTTPS = process.env.IS_LOCAL_HTTPS || "true";
 export const PASSPORT_CLIENT_URL =
   process.env.PASSPORT_CLIENT_URL || "https://dev.local:3000";
 export const PASSPORT_SERVER_URL =
