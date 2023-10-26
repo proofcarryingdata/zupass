ce9ea152b55dc43fdec2f40fa23e38a29ca9248e
ericzhang <zhangeric99@gmail.com>
2023-10-26 13:29:52 -0400

    Kudosbot -- working end-to-end (#1087)
    Closes #1042 
    
    - add migration for new `kudosbot_uploaded_proofs` table for storing
    uploaded kudosbot proofs
    - add db insertion and fetch helpers
    - insert proof into db in `GET /kudos/upload` route
    - add new `GET /kudos/list` route used by kudosbot-client for fetching
    uploaded kudos data
    - add new `GET /kudos/username` route used by kudosbot-client to get a
    username from semaphore id
    - add starter frontend that's working end-to-end -- it fetches all the
    kudoses, verifies the proof + parses/deserializes, and displays them

--- apps/kudosbot-client/src/util.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Popup window will redirect to Zupass to request a proof.
- * Open the popup window under the current domain, let it redirect there.
- */
-export function sendZupassRequest(proofUrl: string) {
-  const popupUrl = `#/popup?proofUrl=${encodeURIComponent(proofUrl)}`;
-  window.open(popupUrl, "_blank", "width=450,height=600,top=100,popup");
-}
