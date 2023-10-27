9a1bb1d69661af2e2f596425c5f2227460a4ed2f
Richard Liu <richard.y.liu@berkeley.edu>
2023-10-27 12:15:05 -0700

    Formatted anon messages (#1096)
    <img width="553" alt="Screenshot 2023-10-26 at 11 35 19 PM"
    src="https://github.com/proofcarryingdata/zupass/assets/36896271/d660ab56-38b1-4eb8-a7f6-a2f00c6bfbe3">

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -3,5 +3,6 @@ export * from "./CryptoHelpers";
 export * from "./Environment";
 export * from "./Errors";
 export * from "./NumericRepresentation";
+export * from "./Pseudonyms";
 export * from "./SNARKHelpers";
 export * from "./promises";
