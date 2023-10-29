ccb5ec321ae6e41e4bfbd380594b3314ae840a0e
lermchair <13699109+lermchair@users.noreply.github.com>
2023-10-29 11:46:21 +0300

    copy link

--- apps/anon-message-client/src/app/page.tsx
+++ apps/anon-message-client/src/app/page.tsx
@@ -117,6 +117,8 @@ export default function () {
   >();
   const [topicData, setTopicData] = useState<TopicData | undefined>();
   const [loadingProofUrl, setLoadingProofUrl] = useState(false);
+  const [showInfo, setShowInfo] = useState<boolean>(true);
+  const [copied, setCopied] = useState<boolean>(false);
   const searchParams = useSearchParams();
   const topicDataRaw = searchParams.get("tgWebAppStartParam");
 
@@ -202,7 +204,49 @@ export default function () {
               )}
             </>
           )}
-          <span className="text-white mt-2">🔒 Anonymous posting</span>
+          {showInfo && (
+            <div className="flex flex-col gap-2 text-left mx-auto mt-4 bg-[#50ACF9] p-4 rounded-lg">
+              <div className="flex justify-between items-center">
+                <span className="text-white font-semibold">
+                  What is anonymous posting?
+                </span>
+                <div
+                  className="p-2 bg-white bg-opacity-20 rounded cursor-pointer"
+                  onClick={() => setShowInfo(false)}
+                >
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+              <span className="text-white opacity-80">
+                We never learn your Zupass identity and only see a proof you
+                have a ticket and a unique ID not linked to your email.
+              </span>
+              <div className="flex item-center gap-4 mx-auto mt-4">
+                <button
+                  className="flex items-center rounded-lg bg-white text-[#50acf9] px-6 py-2 cursor-pointer mx-auto font-medium"
+                  onClick={() => {
+                    navigator.clipboard.writeText(window.location.href);
+                    setCopied(true);
+                  }}
+                >
+                  {copied ? "Link copied!" : "Copy link"}
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
