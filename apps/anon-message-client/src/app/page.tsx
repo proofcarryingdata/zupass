df947efd789566757b8e358c8d1da5ae1b23ddbc
lermchair <13699109+lermchair@users.noreply.github.com>
2023-10-30 09:17:31 +0300

    DMS/ZMS ZuRat client improvements (#1106)
    Closes #1018, #1075 
    ![CleanShot 2023-10-29 at 11 48
    12@2x](https://github.com/proofcarryingdata/zupass/assets/13699109/658ba54d-411d-4245-a40b-5276cdd26dbc)
    
    ---------
    
    Co-authored-by: Richard Liu <richard.y.liu@berkeley.edu>

--- apps/anon-message-client/src/app/page.tsx
+++ apps/anon-message-client/src/app/page.tsx
@@ -29,6 +29,10 @@ interface TopicData {
   validEventIds: string[];
 }
 
+interface InvalidMessage {
+  reason: string | undefined;
+}
+
 async function requestProof(
   message: string,
   chatId: string,
@@ -108,9 +112,13 @@ async function requestProof(
 
 export default function () {
   const [message, setMessage] = useState("");
-  const [messageInvalid, setMessageInvalid] = useState(false);
+  const [invalidMessage, setInvalidMessage] = useState<
+    InvalidMessage | undefined
+  >();
   const [topicData, setTopicData] = useState<TopicData | undefined>();
   const [loadingProofUrl, setLoadingProofUrl] = useState(false);
+  const [showInfo, setShowInfo] = useState<boolean>(true);
+  const [copied, setCopied] = useState<boolean>(false);
   const searchParams = useSearchParams();
   const topicDataRaw = searchParams.get("tgWebAppStartParam");
 
@@ -125,9 +133,13 @@ export default function () {
 
   useEffect(() => {
     if (message.length > MAX_HEADER_SIZE) {
-      setMessageInvalid(true);
-    } else if (messageInvalid) {
-      setMessageInvalid(false);
+      setInvalidMessage({ reason: "Message is too long" });
+    } else if (message.startsWith("/")) {
+      setInvalidMessage({ reason: "Message cannot start with '/'" });
+    } else if (message.length === 0) {
+      setInvalidMessage({ reason: undefined });
+    } else if (invalidMessage) {
+      setInvalidMessage(undefined);
     }
   }, [message]);
 
@@ -167,20 +179,12 @@ export default function () {
             placeholder="Type your anonymous message here"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            className={`border-2 ${
-              messageInvalid ? "border-red-500" : ""
-            } text-2xl rounded-lg text-black resize-none p-2 h-[25vh] select-text`}
+            className={`border-2 text-2xl rounded-lg text-black resize-none p-2 h-[25vh] select-text`}
             autoFocus
           />
         </div>
         <div className="mt-8 text-center flex flex-col w-full">
-          {messageInvalid && (
-            <div className="p-2 bg-red-500 rounded-md mb-2">
-              <span className="text-white pb-2">Message is too long!</span>
-            </div>
-          )}
-          <span className="text-white pb-2">🔒 Anonymous posting</span>
-          {message.length != 0 && !messageInvalid && (
+          {!invalidMessage ? (
             <button
               onClick={onClick}
               className="w-full bg-white text-[#037ee5] text-xl font-bold px-4 rounded-full focus:outline-none focus:shadow-outline py-4"
@@ -189,6 +193,63 @@ export default function () {
                 ? `Loading...`
                 : `Send to ${topicData.topicName}`}
             </button>
+          ) : (
+            <>
+              {invalidMessage.reason && (
+                <div className="p-2 bg-red-500 rounded-md mb-2">
+                  <span className="text-white pb-2">
+                    {invalidMessage.reason}
+                  </span>
+                </div>
+              )}
+            </>
+          )}
+          {showInfo && (
+            <div className="flex flex-col gap-2 text-left mx-auto mt-4 bg-[#50ACF9] p-4 rounded-lg">
+              <div className="flex justify-between items-center">
+                <span className="text-white font-semibold">
+                  What is anonymous posting?
+                </span>
+                <div
+                  className="p-2 bg-white bg-opacity-20 rounded cursor-pointer"
+                  onClick={() => setShowInfo(false)}
+                >
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+                      fill="currentColor"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+              <span className="text-white opacity-80">
+                ZuRat never learns your Zupass account or email, only a proof that
+                you have a ticket and a unique nullifier not linked to your email.
+              </span>
+              <div className="flex item-center gap-4 mx-auto mt-4">
+                <button
+                  className="flex items-center rounded-lg bg-white text-[#50acf9] px-6 py-2 cursor-pointer mx-auto font-medium"
+                  onClick={() => {
+                    navigator.clipboard.writeText(
+                      window.location.origin +
+                        window.location.pathname +
+                        window.location.search
+                    );
+                    setCopied(true);
+                  }}
+                >
+                  {copied ? "Link copied!" : "Copy link"}
+                </button>
+              </div>
+            </div>
           )}
         </div>
       </div>
