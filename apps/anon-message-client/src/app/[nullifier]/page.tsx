74b5169724fe338bbd170983c6fd93e4969b696e
lermchair <13699109+lermchair@users.noreply.github.com>
2023-11-01 18:33:29 +0300

    loading state

--- apps/anon-message-client/src/app/[nullifier]/page.tsx
+++ apps/anon-message-client/src/app/[nullifier]/page.tsx
@@ -1,5 +1,7 @@
 import Post from "@/components/post";
 import { bigIntToPseudonymEmoji, bigIntToPseudonymName } from "@pcd/util";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 interface AnonMessageWithDetails {
   id: number;
@@ -29,47 +31,49 @@ export default async function Page({
   const messages = await getData();
 
   return (
-    <div className="flex flex-col items-center bg-white p-4">
-      <div className="flex items-center justify-center p-2 w-full">
-        <span className="font-bold text-[#2e2e35]">ZK-TG</span>
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col items-center bg-white p-4">
+        <div className="flex items-center justify-center p-2 w-full">
+          <span className="font-bold text-[#2e2e35]">ZK-TG</span>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <div className="flex flex-col items-center">
+            <span className="text-4xl mb-1">
+              {bigIntToPseudonymEmoji(BigInt(params.nullifier))}
+            </span>
+            <span className="text-[#2E2E35] font-bold text-xl">
+              {bigIntToPseudonymName(BigInt(params.nullifier))}
+            </span>
+          </div>
+          <span className="font-mono text-[#2e2e35] opacity-30">
+            #{params.nullifier.substring(0, 4)}
+          </span>
+        </div>
+        {messages.length === 0 ? (
+          <div className="flex flex-col items-center mt-4">
+            <span className="text-[#2e2e35] opacity-30 text-xl ">
+              No messages ... yet
+            </span>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2 mt-4">
+            {messages
+              .sort(
+                (a, b) =>
+                  new Date(b.message_timestamp).getTime() -
+                  new Date(a.message_timestamp).getTime()
+              )
+              .map((message: AnonMessageWithDetails, i: number) => (
+                <Post
+                  title={message.chat_name}
+                  content={message.content}
+                  key={i}
+                  timestamp={message.message_timestamp}
+                />
+              ))}
+          </div>
+        )}
       </div>
-      <div className="flex flex-col gap-2 items-center">
-        <div className="flex flex-col items-center">
-          <span className="text-4xl mb-1">
-            {bigIntToPseudonymEmoji(BigInt(params.nullifier))}
-          </span>
-          <span className="text-[#2E2E35] font-bold text-xl">
-            {bigIntToPseudonymName(BigInt(params.nullifier))}
-          </span>
-        </div>
-        <span className="font-mono text-[#2e2e35] opacity-30">
-          #{params.nullifier.substring(0, 4)}
-        </span>
-      </div>
-      {messages.length === 0 ? (
-        <div className="flex flex-col items-center mt-4">
-          <span className="text-[#2e2e35] opacity-30 text-xl ">
-            No messages ... yet
-          </span>
-        </div>
-      ) : (
-        <div className="flex flex-col gap-2 mt-4">
-          {messages
-            .sort(
-              (a, b) =>
-                new Date(b.message_timestamp).getTime() -
-                new Date(a.message_timestamp).getTime()
-            )
-            .map((message: AnonMessageWithDetails, i: number) => (
-              <Post
-                title={message.chat_name}
-                content={message.content}
-                key={i}
-                timestamp={message.message_timestamp}
-              />
-            ))}
-        </div>
-      )}
-    </div>
+    </Suspense>
   );
 }
