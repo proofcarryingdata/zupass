1706596a742d7345774bb4e0e197e9d87414168a
lermchair <13699109+lermchair@users.noreply.github.com>
2023-11-01 18:41:38 +0300

    shorten timestamp

--- apps/anon-message-client/src/components/post.tsx
+++ apps/anon-message-client/src/components/post.tsx
@@ -12,7 +12,7 @@ const Post: React.FC<PostProps> = ({ title, content, timestamp }) => {
     <div className="flex flex-col border border-black border-opacity-10 rounded-lg p-4">
       <div className="text-[#037EE5] flex items-center justify-between mb-2">
         <span>{title}</span>
-        <span className="opacity-40">{moment(timestamp).fromNow()}</span>
+        <span className="opacity-40">{moment(timestamp).fromNow(true)}</span>
       </div>
       <span className="text-[#2e2e35]">{content}</span>
     </div>
