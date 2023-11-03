fda0bc880543ce4b2eb1b66579d2738001534798
lermchair <13699109+lermchair@users.noreply.github.com>
2023-11-03 15:20:35 +0300

    allow null values in chat_topic_id

--- apps/passport-server/migrations/53_anon_nullifier_topics.sql
+++ apps/passport-server/migrations/53_anon_nullifier_topics.sql
@@ -8,4 +8,4 @@ alter table telegram_chat_anon_nullifiers
 	add foreign key (chat_topic_id) references telegram_chat_topics(id);
 
 alter table telegram_chat_anon_nullifiers
-	add primary key (nullifier, chat_topic_id);
+	add constraint nullifier_chat_topic_id_unique UNIQUE NULLS NOT DISTINCT (nullifier, chat_topic_id);
