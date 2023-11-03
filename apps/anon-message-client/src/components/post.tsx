a49bc7770d92834d523dbdad7d68f4f25f88ad2f
lermchair <13699109+lermchair@users.noreply.github.com>
2023-11-03 12:40:20 +0300

    use zucast format time

--- apps/anon-message-client/src/components/post.tsx
+++ apps/anon-message-client/src/components/post.tsx
@@ -1,4 +1,3 @@
-import moment from "moment";
 import React from "react";
 
 interface PostProps {
@@ -7,12 +6,23 @@ interface PostProps {
   timestamp: string;
 }
 
+// from: https://github.com/dcposch/zucast/blob/master/src/components/PostBox.tsx
+function formatTime(timeMs: number) {
+  const secsAgo = Math.floor((Date.now() - timeMs) / 1000);
+  if (secsAgo < 60) return "Now";
+  if (secsAgo < 60 * 60) return `${Math.floor(secsAgo / 60)}m`;
+  if (secsAgo < 60 * 60 * 24) return `${Math.floor(secsAgo / 60 / 60)}h`;
+  return `${Math.floor(secsAgo / 60 / 60 / 24)}d`;
+}
+
 const Post: React.FC<PostProps> = ({ title, content, timestamp }) => {
   return (
     <div className="flex flex-col border border-black border-opacity-10 rounded-lg p-4">
       <div className="text-[#037EE5] flex items-center justify-between mb-2">
         <span>{title}</span>
-        <span className="opacity-40">{moment(timestamp).fromNow(true)}</span>
+        <span className="opacity-40">
+          {formatTime(new Date(timestamp).getTime())}
+        </span>
       </div>
       <span className="text-[#2e2e35]">{content}</span>
     </div>
