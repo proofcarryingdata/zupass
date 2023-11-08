c4e9bd732de1ff49a81ff4eec99b4e44b82e545b
Richard Liu <richard.y.liu@berkeley.edu>
2023-11-08 15:54:42 +0300

    test

---
+++ apps/passport-server/test/util.spec.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-restricted-globals */
+import { expect } from "chai";
+import "mocha";
+import { isValidEmoji } from "../src/util/util";
+
+describe("passport-server utility functions", function () {
+  this.timeout(15_000);
+  it("isValidEmoji", () => {
+    expect(isValidEmoji("🐸")).to.be.true;
+    expect(isValidEmoji("🦔")).to.be.true;
+    expect(isValidEmoji("👍")).to.be.true;
+    expect(isValidEmoji("❤️")).to.be.true;
+    expect(isValidEmoji("🐀")).to.be.true;
+    expect(isValidEmoji("")).to.be.false;
+    expect(isValidEmoji("a")).to.be.false;
+    expect(isValidEmoji("abc")).to.be.false;
+    expect(isValidEmoji("1")).to.be.false;
+    expect(isValidEmoji(".")).to.be.false;
+    expect(isValidEmoji("*")).to.be.false;
+  });
+});
