495ca03eca578610bff264387c1649e77a8587f4
Rob Knight <mail@robknight.org.uk>
2023-11-13 13:00:41 +0300

    Temporarily disable soft-deletion of tickets (#1238)
    Co-authored-by: Ivan Chub <ivansergchub@gmail.com>

--- apps/passport-server/test/devconnect.spec.ts
+++ apps/passport-server/test/devconnect.spec.ts
@@ -937,11 +937,14 @@ describe("devconnect functionality", function () {
 
     await devconnectPretixSyncService.trySync();
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const tickets = await fetchAllNonDeletedDevconnectPretixTickets(
       application.context.dbPool
     );
 
     // Because two tickets are removed - see comment above
+    // @todo restore the rest of this test
+    /**
     expect(tickets).to.have.length(11);
 
     const ticketsWithEmailEventAndItems = tickets.map((o) => ({
@@ -1002,7 +1005,7 @@ describe("devconnect functionality", function () {
         email: mocker.get().organizer1.EMAIL_4,
         itemInfoID: item2EventAInfoID
       }
-    ]);
+    ]); */
   });
 
   /**
@@ -2629,11 +2632,13 @@ describe("devconnect functionality", function () {
 
     await os.run();
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const tickets = await fetchDevconnectPretixTicketsByEvent(
       db,
       eventConfigID
     );
-    expect(tickets.length).to.eq(0);
+    // @todo restore this check
+    //expect(tickets.length).to.eq(0);
     const ordersForEvent = org.ordersByEventID.get(
       eventID
     ) as DevconnectPretixOrder[];
@@ -2677,7 +2682,8 @@ describe("devconnect functionality", function () {
       db,
       testEmail
     );
-    expect(unredactedTickets.length).to.eq(0);
+    // @todo restore this check
+    // expect(unredactedTickets.length).to.eq(0);
     expect(redactedTickets.length).to.eq(3);
 
     unredactUser = await testLogin(application, testEmail, {
@@ -2814,7 +2820,9 @@ describe("devconnect functionality", function () {
       db,
       testEmail
     );
-    expect(unredactedTickets.length).to.eq(0);
+
+    // @todo restore this check
+    // expect(unredactedTickets.length).to.eq(0);
     expect(redactedTickets.length).to.eq(3);
 
     const result = await agreeTerms(
