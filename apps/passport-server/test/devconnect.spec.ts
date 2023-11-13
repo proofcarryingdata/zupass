d087f03e1351b388dd312fe7526322e8fc357ccc
Rob Knight <mail@robknight.org.uk>
2023-11-13 21:34:56 +0300

    Revert "Temporarily disable soft-deletion of tickets (#1238)" (#1242)
    This reverts commit 495ca03eca578610bff264387c1649e77a8587f4.
    
    Since we have a proper fix for this now, we can revert this.

--- apps/passport-server/test/devconnect.spec.ts
+++ apps/passport-server/test/devconnect.spec.ts
@@ -937,14 +937,11 @@ describe("devconnect functionality", function () {
 
     await devconnectPretixSyncService.trySync();
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const tickets = await fetchAllNonDeletedDevconnectPretixTickets(
       application.context.dbPool
     );
 
     // Because two tickets are removed - see comment above
-    // @todo restore the rest of this test
-    /**
     expect(tickets).to.have.length(11);
 
     const ticketsWithEmailEventAndItems = tickets.map((o) => ({
@@ -1005,7 +1002,7 @@ describe("devconnect functionality", function () {
         email: mocker.get().organizer1.EMAIL_4,
         itemInfoID: item2EventAInfoID
       }
-    ]); */
+    ]);
   });
 
   /**
@@ -2635,13 +2632,11 @@ describe("devconnect functionality", function () {
 
     await os.run();
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const tickets = await fetchDevconnectPretixTicketsByEvent(
       db,
       eventConfigID
     );
-    // @todo restore this check
-    //expect(tickets.length).to.eq(0);
+    expect(tickets.length).to.eq(0);
     const ordersForEvent = org.ordersByEventID.get(
       eventID
     ) as DevconnectPretixOrder[];
@@ -2685,8 +2680,7 @@ describe("devconnect functionality", function () {
       db,
       testEmail
     );
-    // @todo restore this check
-    // expect(unredactedTickets.length).to.eq(0);
+    expect(unredactedTickets.length).to.eq(0);
     expect(redactedTickets.length).to.eq(3);
 
     unredactUser = await testLogin(application, testEmail, {
@@ -2826,9 +2820,7 @@ describe("devconnect functionality", function () {
       db,
       testEmail
     );
-
-    // @todo restore this check
-    // expect(unredactedTickets.length).to.eq(0);
+    expect(unredactedTickets.length).to.eq(0);
     expect(redactedTickets.length).to.eq(3);
 
     const result = await agreeTerms(
