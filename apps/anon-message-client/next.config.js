6e3d3049efe75ab369b9c90a3beb3e4c5a56295f
Rob Knight <mail@robknight.org.uk>
2023-11-16 21:00:54 +0300

    No longer necessary to transpile passport-interface

--- apps/anon-message-client/next.config.js
+++ apps/anon-message-client/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  transpilePackages: ["@pcd/passport-interface"],
   webpack(config, { nextRuntime }) {
     // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
     if (typeof nextRuntime === "undefined") {
