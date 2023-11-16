e6d71ddbd2e0f8c4593381d1e683a1e46556b526
Forest Fang <saurfang@users.noreply.github.com>
2023-11-17 01:23:56 +0300

    [frogcrypto] more button (#1259)
    <img width="412" alt="image"
    src="https://github.com/proofcarryingdata/zupass/assets/4317392/c563f346-a2a2-4d9e-abb2-a57aa0612cf3">
    
    
    queue up more button for next release
    
    ---------
    
    Co-authored-by: Forest Fang <4317392+saurfang@users.noreply.github.com>

--- apps/passport-client/components/screens/FrogScreens/Button.tsx
+++ apps/passport-client/components/screens/FrogScreens/Button.tsx
@@ -1,4 +1,5 @@
 import React, {
+  CSSProperties,
   forwardRef,
   useCallback,
   useEffect,
@@ -7,8 +8,12 @@ import React, {
 } from "react";
 import { useInView } from "react-intersection-observer";
 import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax";
-import styled from "styled-components";
-import { useFrogParticles } from "./useFrogParticles";
+import styled, { keyframes } from "styled-components";
+import {
+  useCelestialPondParticles,
+  useFrogParticles,
+  useWrithingVoidParticles
+} from "./useFrogParticles";
 
 /**
  * A button that shows a loading spinner while the action is in progress.
@@ -193,14 +198,21 @@ export const TheCapitalSearchButton = forwardRef(
   ) => {
     return (
       <ParallaxProvider>
-        <FrogSearchButton {...props} ref={buttonRef} style={{ padding: 0 }}>
+        <FrogSearchButton
+          {...props}
+          ref={buttonRef}
+          style={{
+            padding: 0,
+            filter: props.disabled ? "brightness(80%)" : ""
+          }}
+        >
           <ParallaxBanner
             layers={[
               {
                 image: "/images/frogs/thecapital.jpg",
                 speed: -10,
                 style: {
-                  filter: "brightness(50%)"
+                  filter: props.disabled ? "brightness(30%)" : "brightness(50%)"
                 },
                 shouldAlwaysCompleteAnimation: true
               },
@@ -233,9 +245,11 @@ const TextureSearchButton = forwardRef(
     {
       backgroundImage,
       children,
+      buttonStyle,
       ...props
     }: React.ComponentPropsWithRef<typeof Button> & {
       backgroundImage: string;
+      buttonStyle?: CSSProperties;
     },
     buttonRef: React.Ref<HTMLButtonElement>
   ) => {
@@ -246,7 +260,9 @@ const TextureSearchButton = forwardRef(
         style={{
           backgroundImage,
           backgroundRepeat: "repeat",
-          backdropFilter: "brightness(60%)"
+          filter: props.disabled ? "brightness(70%)" : "",
+          padding: "8px",
+          ...buttonStyle
         }}
       >
         {children}
@@ -284,3 +300,206 @@ export const JungleSearchButton = forwardRef(
     );
   }
 );
+
+export const CelestialPondSearchButton = forwardRef(
+  (
+    {
+      children,
+      ...props
+    }: React.ComponentPropsWithRef<typeof TextureSearchButton>,
+    buttonRef: React.Ref<HTMLButtonElement>
+  ) => {
+    const ref = useRef<HTMLDivElement>(null);
+    useCelestialPondParticles(ref);
+
+    return (
+      <TextureSearchButton
+        ref={buttonRef}
+        backgroundImage="url(/images/frogs/celestialpond.jpg)"
+        buttonStyle={{
+          padding: 0
+        }}
+        {...props}
+      >
+        <div style={{ position: "relative", width: "100%", height: "48px" }}>
+          <div
+            ref={ref}
+            style={{
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              left: 0,
+              right: 0
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              transform: "translateY(-50%)",
+              width: "100%",
+              textAlign: "center"
+            }}
+          >
+            {children}
+          </div>
+        </div>
+      </TextureSearchButton>
+    );
+  }
+);
+
+export const WrithingVoidSearchButton = forwardRef(
+  (
+    {
+      children,
+      onClick,
+      ...props
+    }: React.ComponentPropsWithRef<typeof TextureSearchButton>,
+    buttonRef: React.Ref<HTMLButtonElement>
+  ) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const animate = useWrithingVoidParticles(ref);
+    const [animating, setAnimating] = useState(false);
+    const onClickAnimated = useCallback(async () => {
+      setAnimating(true);
+      let container;
+      try {
+        container = await animate();
+      } catch {
+        console.error("Unable to start animation");
+      }
+
+      await onClick();
+
+      setTimeout(() => {
+        setAnimating(false);
+
+        try {
+          if (container && !container.destroyed) {
+            container.destroy();
+          }
+        } catch {
+          console.debug("Failed to destroy container");
+        }
+      }, 25 * 1000);
+    }, [animate, onClick]);
+
+    return (
+      <>
+        <TextureSearchButton
+          ref={buttonRef}
+          backgroundImage="url(/images/frogs/writhingvoid.png)"
+          buttonStyle={{
+            padding: 0,
+            backgroundColor: "black",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center"
+          }}
+          onClick={onClickAnimated}
+          {...props}
+        >
+          <div style={{ position: "relative", width: "100%", height: "48px" }}>
+            <div
+              ref={ref}
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                left: 0,
+                right: 0
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                width: "100%",
+                textAlign: "center"
+              }}
+            >
+              {children}
+            </div>
+          </div>
+        </TextureSearchButton>
+        <WrithingVoidCover visible={animating}>
+          <WrithingImage visible={animating} />
+        </WrithingVoidCover>
+      </>
+    );
+  }
+);
+
+const WrithingVoidCover = styled.div<{ visible: boolean }>`
+  pointer-events: none;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 2000;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${({ visible }) => (visible ? "#000000" : "transparent")};
+  transition: ${({ visible }) =>
+    visible
+      ? "background-color 4s ease-in"
+      : "background-color 3s ease-out 1s"};
+`;
+
+const rotating = keyframes`
+  from {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+`;
+
+const pulse = keyframes`
+	0% { box-shadow: 0 0 0 0  rgba(255,255,255,0.2) inset, 0 0 0 0  rgba(255,255,255,0.2);
+  opacity: 100% }
+	47% { box-shadow: 0 0 10px 5px rgba(255,255,255,0.80) inset, 0 0 20px 10px rgba(255,255,255, 1);
+  opacity: 100% }
+	50% { box-shadow: 0 0 10px 5px rgba(255,255,255,0.80) inset, 0 0 20px 10px rgba(255,255,255, 1);
+  opacity: 60% }
+	53% { box-shadow: 0 0 10px 5px rgba(255,255,255,0.80) inset, 0 0 20px 10px rgba(255,255,255, 1);
+  opacity: 100% }
+	100% { box-shadow: 0 0 0 rgba(255,255,255,0.2) inset, 0 0 0 rgba(255,255,255,0.2);
+  opacity: 100% }
+`;
+
+const WrithingImage = styled.div<{ visible: boolean }>`
+  background-image: url("/images/frogs/writhingvoid.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+
+  border-radius: 50%;
+
+  opacity: ${({ visible }) => (visible ? "100%" : "20%")};
+  width: ${({ visible }) => (visible ? "min(80vw, 80vw)" : "0")};
+  height: ${({ visible }) => (visible ? "min(80vw, 80vw)" : "0")};
+
+  transition: ${({ visible }) =>
+    visible
+      ? "width 16s ease-in, height 16s ease-in, opacity 4s ease-in"
+      : "all 4s cubic-bezier(1,0,1,.6)"};
+  animation: ${rotating} 600s linear 1,
+    ${pulse} 8s linear ${({ visible }) => (visible ? "16s infiite" : "0")};
+  -webkit-animation: ${rotating} 600s linear infinite,
+    ${pulse} 8s linear ${({ visible }) => (visible ? "16s infinite" : "0")};
+`;
