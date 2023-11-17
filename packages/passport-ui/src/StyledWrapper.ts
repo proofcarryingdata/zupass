1cbe96889d35b1e62357e3dfd2aeb64dfc732f63
Rob Knight <mail@robknight.org.uk>
2023-11-17 13:10:10 +0300

    Add comment to StyledWrapper

--- packages/passport-ui/src/StyledWrapper.ts
+++ packages/passport-ui/src/StyledWrapper.ts
@@ -1,4 +1,8 @@
 // Based on pattern described here: https://github.com/lidofinance/ui/pull/460
+// This is necessary because Styled Components 5.x does not conform fully to
+// the ESM standard, so we have to wrap it here to create a proper default
+// export.
+// If we upgrade to Styled 6.x then we can remove this.
 import _styled, { StyledInterface } from "styled-components";
 
 // @ts-expect-error Property 'default' does not exist on type 'StyledInterface'.
