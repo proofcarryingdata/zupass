dffb0e687fe6e29cbc5d07f22283a08a20410a7f
Rob Knight <mail@robknight.org.uk>
2023-11-19 15:18:24 +0300

    Add comments to nextjs example

--- examples/nextjs/src/pages/index.tsx
+++ examples/nextjs/src/pages/index.tsx
@@ -25,39 +25,65 @@ function openZKEdDSAEventTicketPopup(
   validEventIds: string[],
   validProductIds: string[]
 ) {
+  // These arguments determine the options that will be passed in to the ZK
+  // proving screen. Where `userProvided` is true, the user must input or
+  // select an option.
+  // This particular proof will be a proof about an EdDSA ticket that the user
+  // owns, and so the options relate to the selection of the ticket and the
+  // claim that the user will make about it.
   const args: ZKEdDSAEventTicketPCDArgs = {
+    // This allows the user to choose a ticket to make a proof about.
     ticket: {
       argumentType: ArgumentTypeName.PCD,
       pcdType: EdDSATicketPCDPackage.name,
       value: undefined,
       userProvided: true,
       validatorParams: {
+        // It is possible to filter the tickets the user can choose from by
+        // event ID and product ID. Empty arrays mean that no filtering will
+        // occur.
         eventIds: validEventIds,
         productIds: validProductIds,
         notFoundMessage: "No eligible PCDs found"
       }
     },
+    // This is the user's Semaphore identity, which is required in order to
+    // prove that the user controls the identity that the ticket was created
+    // for.
     identity: {
       argumentType: ArgumentTypeName.PCD,
       pcdType: SemaphoreIdentityPCDPackage.name,
       value: undefined,
       userProvided: true
     },
+    // If we want the proof to show that the ticket belongs to a certain event
+    // or set of events, the event IDs can be passed in here.
     validEventIds: {
       argumentType: ArgumentTypeName.StringArray,
       value: validEventIds.length != 0 ? validEventIds : undefined,
       userProvided: false
     },
+    // The fields to reveal in the claim.
     fieldsToReveal: {
       argumentType: ArgumentTypeName.ToggleList,
       value: fieldsToReveal,
       userProvided: false
     },
+    // The watermark can be used to ensure that the proof is used only once.
+    // This can be done by, for example, generating a random number, passing
+    // it in as the watermark, and then checking that the proof contains a
+    // matching watermark.
     watermark: {
       argumentType: ArgumentTypeName.BigInt,
       value: watermark.toString(),
       userProvided: false
     },
+    // The external nullifier is an input into the nullifier hash, which is a
+    // function of the user's identity and the external nullifier. The
+    // nullifier hash can be used to determine that two proofs with identical
+    // nullifier hashes where produced by the same user, without revealing the
+    // user's identity (provided that the external nullifier is the same in
+    // both cases).
     externalNullifier: {
       argumentType: ArgumentTypeName.BigInt,
       value: "12345",
@@ -66,10 +92,11 @@ function openZKEdDSAEventTicketPopup(
   };
 
   const popupUrl = window.location.origin + "/popup";
-  const ZUPASS_URL = "https://zupass.org";
+  const ZUPASS_PRODUCTION_URL = "https://zupass.org";
 
+  // Create the Zupass URL which will be loaded in the popup window.
   const proofUrl = constructZupassPcdGetRequestUrl(
-    ZUPASS_URL,
+    process.env.ZUPASS_SERVER_URL ?? ZUPASS_PRODUCTION_URL,
     popupUrl,
     ZKEdDSAEventTicketPCDPackage.name,
     args,
@@ -80,6 +107,9 @@ function openZKEdDSAEventTicketPopup(
     }
   );
 
+  // Open the popup window. This points /popup on the local site, with the
+  // Zupass URL as a query parameter. In `popup.tsx` we do a redirect to the
+  // Zupass URL.
   openZupassPopup(popupUrl, proofUrl);
 }
 
