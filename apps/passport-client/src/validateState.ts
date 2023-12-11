65a7536efabc68a50006f5358bc0611f19173251
Ivan Chub <ivansergchub@gmail.com>
2023-12-11 12:29:40 -0800

    create function that validates appState and returns list of errors as human interpretable strings

---
+++ apps/passport-client/src/validateState.ts
@@ -0,0 +1,74 @@
+import {
+  SemaphoreIdentityPCD,
+  SemaphoreIdentityPCDPackage
+} from "@pcd/semaphore-identity-pcd";
+import { AppState } from "./state";
+
+/**
+ * Determines whether the appState object contains valid data. If it does not,
+ * returns the set of things that are incorrect about it in an array of human
+ * interpretable strings.
+ */
+export function validateState(appState: AppState): string[] {
+  const validationErrors: string[] = [];
+
+  if (!appState.self) {
+    validationErrors.push("missing 'self' field from app state");
+  }
+
+  if (!appState.identity) {
+    validationErrors.push("missing 'identity' field from app state");
+  }
+
+  if (!appState.encryptionKey) {
+    validationErrors.push("missing 'encryption' field from app state key");
+  }
+
+  if (!appState.pcds) {
+    validationErrors.push("missing 'pcds' field from app state");
+  }
+
+  if (appState.pcds.size() === 0) {
+    validationErrors.push("'pcds' field in app state contains no pcds");
+  }
+
+  const identityPCDFromCollection = appState.pcds.getPCDsByType(
+    SemaphoreIdentityPCDPackage.name
+  )[0] as SemaphoreIdentityPCD | undefined;
+
+  if (!identityPCDFromCollection) {
+    validationErrors.push(
+      "'pcds' field in app state does not contain an identity PCD"
+    );
+  }
+
+  const identityFromPCDCollection = identityPCDFromCollection?.claim?.identity;
+  const commitmentOfIdentityPCDInCollection =
+    identityFromPCDCollection?.commitment?.toString();
+  const commitmentFromSelfField = appState?.self?.commitment;
+  const commitmentFromIdentityField =
+    appState?.identity?.commitment?.toString();
+
+  if (commitmentOfIdentityPCDInCollection !== commitmentFromSelfField) {
+    validationErrors.push(
+      `commitment of identity pcd in collection (${commitmentOfIdentityPCDInCollection})` +
+        ` does not match commitment in 'self' field of app state (${commitmentFromSelfField})`
+    );
+  }
+
+  if (commitmentFromSelfField !== commitmentFromIdentityField) {
+    validationErrors.push(
+      `commitment in 'self' field of app state (${commitmentFromSelfField})` +
+        ` does not match commitment of 'identity' field of app state (${commitmentFromIdentityField})`
+    );
+  }
+
+  if (commitmentFromIdentityField !== commitmentOfIdentityPCDInCollection) {
+    validationErrors.push(
+      `commitment of 'identity' field of app state (${commitmentFromIdentityField})` +
+        ` does not match commitment of identity pcd in collection (${commitmentOfIdentityPCDInCollection})`
+    );
+  }
+
+  return validationErrors;
+}
