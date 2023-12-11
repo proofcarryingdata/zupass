1ca17bcc388195756f8d2aec64c85fb2e36ffd30
Rob Knight <mail@robknight.org.uk>
2023-12-11 19:42:13 +0000

    Fix lint

--- apps/passport-client/components/shared/AccountImportButton.tsx
+++ apps/passport-client/components/shared/AccountImportButton.tsx
@@ -6,7 +6,7 @@ export function AccountImportButton() {
   const dispatch = useDispatch();
   const showImportModal = useCallback(() => {
     dispatch({ type: "set-modal", modal: { modalType: "account-import" } });
-  }, []);
+  }, [dispatch]);
 
   return <Button onClick={showImportModal}>Import Account Data</Button>;
 }
