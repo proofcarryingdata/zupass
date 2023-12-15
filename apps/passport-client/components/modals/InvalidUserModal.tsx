cebe1b91856e7c7de3c706c0a917525b288a6f2f
Ivan Chub <ivansergchub@gmail.com>
2023-12-15 15:53:45 -0800

    update copy in response to feedback

--- apps/passport-client/components/modals/InvalidUserModal.tsx
+++ apps/passport-client/components/modals/InvalidUserModal.tsx
@@ -17,14 +17,14 @@ export function InvalidUserModal() {
       <H2>Invalid Zupass</H2>
       <Spacer h={24} />
       <p>
-        You've reset your Zupass account on another device, invalidating this
-        one. Click the button below to log out. Then you'll be able to sync your
-        existing Zupass account onto this device.
+        You've reset your Zupass account on another device, or your app has
+        ended up in an invalid state. Click the button below to log out. Then
+        you'll be able to sync your existing Zupass account onto this device.
       </p>
       <Spacer h={16} />
       <p>
-        You can export your account data using the button below in case you need
-        it later.
+        You can export a copy of your local account data using the button below
+        in case you need it later.
       </p>
       <Spacer h={16} />
       <AccountExportButton />
