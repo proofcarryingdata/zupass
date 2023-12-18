e11aacdbdf5f34d26dcad9fea7e3b9d026cea1b2
Ivan Chub <ivansergchub@gmail.com>
2023-12-18 12:37:09 -0800

    more validation tests

--- apps/passport-client/test/stateValidation.spec.ts
+++ apps/passport-client/test/stateValidation.spec.ts
@@ -31,11 +31,20 @@ describe("validateAppState", async function () {
   const crypto = await PCDCrypto.newInstance();
   const pcdPackages = [SemaphoreIdentityPCDPackage, EdDSAPCDPackage];
   const identity1 = new Identity(
-    '["0xaa5fa3165e1ca129bd7a2b3bada18c5f81350faacf2edff59cf44eeba2e2d","0xa944ed90153fc2be5759a0d18eda47266885aea0966ef4dbb96ff979c29ed4"]'
+    '["0xaa5fa3165e1ca129bd7a2b3bada18c5f81350faacf2edff59cf44eeba2e2d",' +
+      '"0xa944ed90153fc2be5759a0d18eda47266885aea0966ef4dbb96ff979c29ed4"]'
   );
+  const commitment1 = identity1.commitment;
   const identity2 = new Identity(
-    '["0x8526e030dbd593833f24bf73b60f0bcc58690c590b9953acc741f2eb71394d","0x520e4ae6f5d5e4526dd517e61defe16f90bd4aef72b41394285e77463e0c69"]'
+    '["0x8526e030dbd593833f24bf73b60f0bcc58690c590b9953acc741f2eb71394d",' +
+      '"0x520e4ae6f5d5e4526dd517e61defe16f90bd4aef72b41394285e77463e0c69"]'
   );
+  const commitment2 = identity2.commitment;
+  const identity3 = new Identity(
+    '["0x4837c6f88904d1dfefcb7dc6486e95c06cda6eb76d76a9888167c0993e40f0",' +
+      '"0x956f9e03b0cc324045d24f9b20531e547272fab8e8ee2f96c0cf2e50311468"]'
+  );
+  const commitment3 = identity3.commitment;
 
   it("logged out ; no errors", async function () {
     expect(
@@ -47,7 +56,7 @@ describe("validateAppState", async function () {
     } satisfies ErrorReport);
   });
 
-  it("logged out ; forceCheckPCDs=true; all error states caught", async function () {
+  it("logged out ; forceCheckPCDs=true; test of all error states", async function () {
     expect(
       validateAppState(
         TAG_STR,
@@ -211,7 +220,61 @@ describe("validateAppState", async function () {
     } satisfies ErrorReport);
   });
 
-  it("logged in ; no errors", async function () {
+  it("logged in ; self commitment wrong ; errors", async function () {
+    const saltAndEncryptionKey = await crypto.generateSaltAndEncryptionKey(
+      "testpassword123!@#asdf"
+    );
+    const self: ZupassUserJson = {
+      commitment: identity2.commitment.toString(),
+      email: randomEmail(),
+      salt: saltAndEncryptionKey.salt,
+      terms_agreed: 1,
+      uuid: uuid()
+    };
+    const pcds = new PCDCollection(pcdPackages);
+    pcds.add(
+      await SemaphoreIdentityPCDPackage.prove({
+        identity: identity1
+      })
+    );
+    expect(validateAppState(TAG_STR, self, identity1, pcds)).to.deep.eq({
+      userUUID: self.uuid,
+      errors: [
+        `commitment of identity pcd in collection (${commitment1}) does not match commitment in 'self' field of app state (${commitment2})`,
+        `commitment in 'self' field of app state (${commitment2}) does not match commitment of 'identity' field of app state (${commitment1})`
+      ],
+      ...TAG
+    } satisfies ErrorReport);
+  });
+
+  it("logged in ; pcd collection identity wrong ; errors", async function () {
+    const saltAndEncryptionKey = await crypto.generateSaltAndEncryptionKey(
+      "testpassword123!@#asdf"
+    );
+    const self: ZupassUserJson = {
+      commitment: identity1.commitment.toString(),
+      email: randomEmail(),
+      salt: saltAndEncryptionKey.salt,
+      terms_agreed: 1,
+      uuid: uuid()
+    };
+    const pcds = new PCDCollection(pcdPackages);
+    pcds.add(
+      await SemaphoreIdentityPCDPackage.prove({
+        identity: identity2
+      })
+    );
+    expect(validateAppState(TAG_STR, self, identity1, pcds)).to.deep.eq({
+      userUUID: self.uuid,
+      errors: [
+        `commitment of identity pcd in collection (${commitment2}) does not match commitment in 'self' field of app state (${commitment1})`,
+        `commitment of 'identity' field of app state (${commitment1}) does not match commitment of identity pcd in collection (${commitment2})`
+      ],
+      ...TAG
+    } satisfies ErrorReport);
+  });
+
+  it("logged in ; appState identity wrong ; errors", async function () {
     const saltAndEncryptionKey = await crypto.generateSaltAndEncryptionKey(
       "testpassword123!@#asdf"
     );
@@ -228,9 +291,40 @@ describe("validateAppState", async function () {
         identity: identity1
       })
     );
-    expect(validateAppState(TAG_STR, self, identity1, pcds)).to.deep.eq({
+    expect(validateAppState(TAG_STR, self, identity2, pcds)).to.deep.eq({
       userUUID: self.uuid,
-      errors: [],
+      errors: [
+        `commitment in 'self' field of app state (${commitment1}) does not match commitment of 'identity' field of app state (${commitment2})`,
+        `commitment of 'identity' field of app state (${commitment2}) does not match commitment of identity pcd in collection (${commitment1})`
+      ],
+      ...TAG
+    } satisfies ErrorReport);
+  });
+
+  it("logged in ; all identities mistmatched ; errors", async function () {
+    const saltAndEncryptionKey = await crypto.generateSaltAndEncryptionKey(
+      "testpassword123!@#asdf"
+    );
+    const self: ZupassUserJson = {
+      commitment: identity1.commitment.toString(),
+      email: randomEmail(),
+      salt: saltAndEncryptionKey.salt,
+      terms_agreed: 1,
+      uuid: uuid()
+    };
+    const pcds = new PCDCollection(pcdPackages);
+    pcds.add(
+      await SemaphoreIdentityPCDPackage.prove({
+        identity: identity2
+      })
+    );
+    expect(validateAppState(TAG_STR, self, identity3, pcds)).to.deep.eq({
+      userUUID: self.uuid,
+      errors: [
+        `commitment of identity pcd in collection (${commitment2}) does not match commitment in 'self' field of app state (${commitment1})`,
+        `commitment in 'self' field of app state (${commitment1}) does not match commitment of 'identity' field of app state (${commitment3})`,
+        `commitment of 'identity' field of app state (${commitment3}) does not match commitment of identity pcd in collection (${commitment2})`
+      ],
       ...TAG
     } satisfies ErrorReport);
   });
