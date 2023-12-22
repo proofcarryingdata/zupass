036892b3228474cd43b7d745cf99299abf303fe4
Rob Knight <mail@robknight.org.uk>
2023-12-22 11:56:56 +0000

    Remove outdated comment

--- packages/ui/eddsa-ticket-pcd-ui/src/index.ts
+++ packages/ui/eddsa-ticket-pcd-ui/src/index.ts
@@ -1,11 +1,1 @@
 export * from "./CardBody";
-
-/**
- * UI is going to need access to:
- *
- * - Identity PCD for generating ZK QR codes
- * - Product and event IDs
- * - Verification stuff?
- *
- *
- */
