e0076576ca913508f1438c37d5169b820ee74065
Rob Knight <mail@robknight.org.uk>
2023-12-22 19:46:24 +0000

    Remove default from column

--- apps/passport-server/migrations/60_known_ticket_names.sql
+++ apps/passport-server/migrations/60_known_ticket_names.sql
@@ -1,2 +1,9 @@
 ALTER TABLE known_ticket_types
 ADD COLUMN event_name VARCHAR NOT NULL DEFAULT '';
+
+UPDATE known_ticket_types SET event_name = 'Zuzalu ''23' WHERE ticket_group = 'Zuzalu23';
+UPDATE known_ticket_types SET event_name = 'ZuConnect ''23' WHERE ticket_group = 'Zuconnect23';
+UPDATE known_ticket_types SET event_name = 'Devconnet ''23' WHERE ticket_group = 'Devconnect23';
+
+ALTER TABLE known_ticket_types
+ALTER COLUMN event_name DROP DEFAULT;
