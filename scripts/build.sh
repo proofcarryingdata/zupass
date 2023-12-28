b49c58799f97632a71661fef6d05773657f17ddb
Rob Knight <mail@robknight.org.uk>
2023-12-28 13:10:57 +0100

    Use build:ts and build:types for build and dev commands

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 if [[ -z $DISABLE_BUILD_CACHE ]]; then
-  yarn turbo run build
+  yarn turbo run build:ts
+  yarn turbo run build:types
 else
-  yarn turbo run build --no-cache --force
+  yarn turbo run build:ts --no-cache --force
+  yarn turbo run build:types --no-cache --force
 fi
