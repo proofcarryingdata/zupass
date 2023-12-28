1f14a10e76878eeef9146f1b67c3129651eb1136
Rob Knight <mail@robknight.org.uk>
2023-12-28 18:03:27 +0100

    Tidy up commented-out eslint rule

--- packages/tools/eslint-config-custom/index.js
+++ packages/tools/eslint-config-custom/index.js
@@ -30,6 +30,7 @@ module.exports = {
     "react/no-unescaped-entities": "off",
     "import/no-named-as-default-member": "off",
     "import/no-extraneous-dependencies": "error",
+<<<<<<< HEAD
     "prettier/prettier": "error",
     "no-restricted-imports": [
       "error",
@@ -44,7 +45,8 @@ module.exports = {
       }
     ],
     "@typescript-eslint/no-require-imports": "error",
-    "import/no-unresolved": "off"
+    "import/no-unresolved": "off",
+    "prettier/prettier": "error"
   },
   settings: {
     "import/resolver": {
