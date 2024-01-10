d41390b1d9fe07cf5071b0cd68f8cd0fc99b4e5c
Rob Knight <mail@robknight.org.uk>
2024-01-10 13:03:05 +0100

    Tidy up project reference scripts

---
+++ scripts/check-references.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Checks that tsconfig project references are up-to-date.
+# 
+# See fix-references.sh for an explanation of project references and the
+# purpose of these scripts.
+
+yarn workspaces-to-typescript-project-references --tsconfigPath tsconfig.json --check 1>/dev/null 2>/dev/null
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Missing tsconfig project reference. Run \"yarn fix-references\" to resolve this problem."
+    exit
+fi
+
+yarn workspaces-to-typescript-project-references --tsconfigPath tsconfig.cjs.json --includesRoot --check 1>/dev/null 2>/dev/null
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Missing tsconfig project reference. Run \"yarn fix-references\" to resolve this problem."
+    exit
+fi
+
+yarn workspaces-to-typescript-project-references --tsconfigPath tsconfig.esm.json --includesRoot --check 1>/dev/null 2>/dev/null
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Missing tsconfig project reference. Run \"yarn fix-references\" to resolve this problem."
+    exit
+fi
+
+echo "tsconfig project references up to date!"
---
+++ scripts/fix-references.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# This script sets up TypeScript references between each workspace in our
+# monorepo. With references in place, TypeScript's language server can track
+# types across packages, even when those packages do not have type declaration
+# files on-disk. The "generic" project references support this, and are held
+# in the tsconfig.json file for each workspace.
+#
+# For packaging, we do two kinds of build for our packages: CJS and ESM.
+# Because these have slightly different configuration, they need different
+# tsconfig files - tsconfig.cjs.json, and tsconfig.esm.json respectively.
+# For these files, we also pass "--includesRoot", which updates both a
+# tsconfig.cjs.json and tsconfig.esm.json at the root of the repository.
+# Because this has references to all of our packages, we can pass this file to
+# `tsc -b` and have it build *all* packages simultaneously, in a single
+# process. This is much faster and more memory-efficient than spawning a
+# separate process to build each package (or two processes, one for
+# transpilation and one for type generation, as we previously did).
+#
+# After updating, we run `prettier` to ensure that the formatting of the files
+# is not negatively affected.
+#
+# This script should be run regularly to ensure that project references remain
+# up to date, especially when:
+# - Adding or removing dependencies between packages
+# - Adding or removing entire packages from the repository
+
+echo "Fixing generic references\r\n"
+yarn workspaces-to-typescript-project-references --tsconfigPath tsconfig.json && yarn prettier -w "{apps/*,packages/*/*}/tsconfig.esm.json"
+
+echo "\r\nFixing references for ESM builds\r\n"
+yarn workspaces-to-typescript-project-references --tsconfigPath tsconfig.esm.json --includesRoot && yarn prettier -w "{apps/*,packages/*/*}/tsconfig.esm.json"
+
+echo "\r\nFixing references for CJS builds\r\n"
+yarn workspaces-to-typescript-project-references --tsconfigPath tsconfig.cjs.json --includesRoot && yarn prettier -w "{apps/*,packages/*/*}/tsconfig.cjs.json"
