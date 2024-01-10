0def60badd99df55e46b0aca2d577d7226bd9184
Rob Knight <mail@robknight.org.uk>
2024-01-10 11:46:12 +0100

    Turn icons into strings

---
+++ packages/lib/passport-ui/convert-icons.ts
@@ -0,0 +1,31 @@
+import camelCase from 'camelcase';
+import * as fs from "fs";
+import { glob } from "glob";
+import path from "path";
+import svgToUrl from "svg-to-url";
+
+/**
+ * This file converts the icons in the ./icons directory into data URLs, then
+ * creates a src/icons/index.ts file which exports the resulting strings.
+ * This performs the same task that a webpack/esbuild data loader would do, but
+ * without requiring a bundler.
+ * This is helpful because it means we can build this package using only tsc.
+ */
+async function main() {
+  const iconPaths = (await glob("icons/*.svg"));
+
+  const iconIndex = await Promise.all(iconPaths.map(async (iconPath) => {
+    return [
+      path.basename(iconPath, ".svg"),
+      await svgToUrl(iconPath)
+    ]
+  }));
+
+  const output = iconIndex.map(([name, dataUrl]) => {
+    return `export const ${camelCase(name)} = "${dataUrl}";`;
+  }).join("\r\n\r\n");
+  
+  fs.writeFileSync("src/icons/index.ts", output);
+}
+
+main();
