d1b04b4b9694a818daa6d99e6797a8ac5911e7bc
Rob Knight <mail@robknight.org.uk>
2024-01-10 12:21:09 +0100

    Fix icon conversion code

--- packages/lib/passport-ui/convert-icons.ts
+++ packages/lib/passport-ui/convert-icons.ts
@@ -1,7 +1,8 @@
-import camelCase from 'camelcase';
+import camelCase from "camelcase";
 import * as fs from "fs";
 import { glob } from "glob";
 import path from "path";
+import * as prettier from "prettier";
 import svgToUrl from "svg-to-url";
 
 /**
@@ -12,20 +13,24 @@ import svgToUrl from "svg-to-url";
  * This is helpful because it means we can build this package using only tsc.
  */
 async function main() {
-  const iconPaths = (await glob("icons/*.svg"));
+  const iconPaths = await glob("icons/*.svg");
 
-  const iconIndex = await Promise.all(iconPaths.map(async (iconPath) => {
-    return [
-      path.basename(iconPath, ".svg"),
-      await svgToUrl(iconPath)
-    ]
-  }));
+  const iconIndex = await Promise.all(
+    iconPaths.map(async (iconPath) => {
+      return [path.basename(iconPath, ".svg"), await svgToUrl(iconPath)];
+    })
+  );
 
-  const output = iconIndex.map(([name, dataUrl]) => {
-    return `export const ${camelCase(name)} = "${dataUrl}";`;
-  }).join("\r\n\r\n");
-  
-  fs.writeFileSync("src/icons/index.ts", output);
+  const output = iconIndex
+    .map(([name, dataUrl]) => {
+      return `export const ${camelCase(name)} = "${dataUrl}";`;
+    })
+    .join("\r\n\r\n");
+
+  fs.writeFileSync(
+    "src/icons/index.ts",
+    await prettier.format(output, { parser: "typescript" })
+  );
 }
 
 main();
