f8fd7c707bf9df24d02e3366effadc36cc782083
Richard Liu <richard.y.liu@berkeley.edu>
2024-02-08 14:05:27 -0800

    comment

--- packages/lib/passport-interface/src/vitalia.ts
+++ packages/lib/passport-interface/src/vitalia.ts
@@ -1,5 +1,6 @@
 import { EdDSAPublicKey } from "@pcd/eddsa-pcd";
 
+// IDs copied from https://github.com/robknight/zuconnect-badges/blob/19b05e67c3d82dcf066a5e211ca233ca30068dfb/src/pages/api/generate-badge.ts#L11-L12.
 export const VITALIA_EVENT_ID = "9ccc53cb-3b0a-415b-ab0d-76cfa21c72ac";
 export const VITALIA_ATTENDEE_PRODUCT_ID =
   "cd3f2b06-e520-4eff-b9ed-c52365c60848";
