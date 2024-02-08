892583e7f28420b936862e5443ab8169e7137cda
Ivan Chub <ivansergchub@gmail.com>
2024-02-07 19:37:50 -0800

    fix auth loop

--- apps/generic-issuance-client/src/constants.ts
+++ apps/generic-issuance-client/src/constants.ts
@@ -5,3 +5,4 @@ export const ZUPASS_SERVER_URL = process.env.PASSPORT_SERVER_URL as string;
 
 // 24 hours maximum
 export const SESSION_DURATION_MINUTES = 24 * 60;
+export const SESSION_DURATION_MS = SESSION_DURATION_MINUTES * 60 * 1000;
