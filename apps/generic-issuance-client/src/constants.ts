331047f0c32a7b3735a5f257930939f73e37965c
Richard Liu <richard.y.liu@berkeley.edu>
2024-02-12 22:16:26 -0800

    generic issuance: longer session time in non-production environments

--- apps/generic-issuance-client/src/constants.ts
+++ apps/generic-issuance-client/src/constants.ts
@@ -1,8 +1,10 @@
 export const PCD_GITHUB_URL = "https://github.com/proofcarryingdata/pcd";
 
+export const IS_PROD = process.env.NODE_ENV === "production";
+
 export const ZUPASS_URL = process.env.PASSPORT_CLIENT_URL as string;
 export const ZUPASS_SERVER_URL = process.env.PASSPORT_SERVER_URL as string;
 
-// 24 hours maximum
-export const SESSION_DURATION_MINUTES = 24 * 60;
+// One hour timeout in production; one week timeout in other environments
+export const SESSION_DURATION_MINUTES = IS_PROD ? 24 * 60 : 24 * 60 * 7;
 export const SESSION_DURATION_MS = SESSION_DURATION_MINUTES * 60 * 1000;
