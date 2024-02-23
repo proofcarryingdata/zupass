f97cf206166ec39ac62922d5ce9d8ffd8c7ef401
Ivan Chub <ivansergchub@gmail.com>
2024-02-22 17:47:04 -0800

    wip

---
+++ apps/passport-client/components/screens/ScannedTicketScreens/PodboxScannedTicketScreen/sections/actions/issueBadge/IssueBadgeAction.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export function IssueBadgeAction(): ReactNode {
+  return <div>issue badge</div>;
+}
---
+++ apps/passport-client/components/screens/ScannedTicketScreens/PodboxScannedTicketScreen/sections/actions/issueContact/IssueContactAction.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export function IssueContactAction(): ReactNode {
+  return <div>issue contact</div>;
+}
