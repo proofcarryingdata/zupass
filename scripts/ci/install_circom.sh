31c18583e23b14e1781d1e9fdee815e96e1a2a94
Andrew Twyman <artwyman@gmail.com>
2024-03-04 14:21:59 -0800

    Circom installation script

---
+++ scripts/ci/install_circom.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+
+if [ -z "$1" ]
+then
+      echo "usage: $0 [circom-version-tag]"
+      exit 1
+fi
+
+# Create cibuild folder for output
+mkdir -p cibuild
+cd cibuild
+
+# Install rustup and cargo
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal --default-toolchain nightly
+
+# Clone circom from tag specified in $1
+git clone -b $1 https://github.com/iden3/circom.git
+cd circom
+
+# Build and install circom in path
+cargo build --release
+cargo install --path circom
