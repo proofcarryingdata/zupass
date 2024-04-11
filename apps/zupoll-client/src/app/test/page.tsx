00e6b1d5e2c55942e3e6e09242668cd7ed5a3b2b
Ivan Chub <ivansergchub@gmail.com>
2024-04-11 11:51:07 -0700

    more

--- apps/zupoll-client/src/app/test/page.tsx
+++ apps/zupoll-client/src/app/test/page.tsx
@@ -7,14 +7,14 @@ import { Spinner } from "@/components/ui/spinner";
 export default function TestPage() {
   return (
     <Center>
-      <ContentContainer className="mt-4 flex columns-1 gap-1">
+      <ContentContainer className="mt-4 flex columns-1 gap-0">
         <Spinner className="w-12 h-12 m-2" />
         <Button variant="default">default</Button>
         <Button variant="destructive">destructive</Button>
         <Button variant="ghost">ghost</Button>
         <Button variant="outline">outline</Button>
-        <Button variant="secondary">secondary</Button>
         <Button variant="link">link</Button>
+        <Button variant="secondary">secondary</Button>
         <Button variant="accent">accent</Button>
         <Button variant="warning">warning</Button>
         <Button variant="creative">creative</Button>
