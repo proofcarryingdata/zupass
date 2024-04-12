8641f638c1ef29e6db3ef733691117abf9ea1da8
Ivan Chub <ivansergchub@gmail.com>
2024-04-12 13:04:10 -0700

    fix error dialog color

--- apps/zupoll-client/src/@/components/ui/ErrorDialog.tsx
+++ apps/zupoll-client/src/@/components/ui/ErrorDialog.tsx
@@ -44,7 +44,7 @@ export default function ErrorDialog({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-background px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div className="sm:flex sm:items-start">
                   <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                     <ExclamationTriangleIcon
@@ -70,7 +70,6 @@ export default function ErrorDialog({
                   <Button
                     variant={"outline"}
                     type="button"
-                    // className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
                     onClick={() => {
                       window.location.reload();
                     }}
