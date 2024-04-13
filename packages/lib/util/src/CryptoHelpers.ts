f2a80d55bd9fa2c9a2c216a3acce23e086536a25
Ivan Chub <ivansergchub@gmail.com>
2024-04-12 22:46:22 -0700

    undo changes to cryptohelpers

--- packages/lib/util/src/CryptoHelpers.ts
+++ packages/lib/util/src/CryptoHelpers.ts
@@ -1,4 +1,30 @@
-import secureRandom from "secure-random";
+import nodeCrypto from "crypto";
+import { isBrowser, isNode } from "./Environment";
+
+// The 'crypto' variable provides access to the functions defined
+// in the standard Web Crypto API (https://www.w3.org/TR/WebCryptoAPI).
+let crypto: Crypto | undefined;
+
+/**
+ * Initialize the 'crypto' variable with an implementation that
+ * is compatible with the Web Crypto API standard in both
+ * browser and Node.js environments, requiring no additional
+ * polyfills or code.
+ * This code is compatible with Node.js < v18 too.
+ */
+function initCryptoAPI(): Crypto {
+  if (!crypto) {
+    if (isBrowser()) {
+      crypto = globalThis.crypto;
+    } else if (isNode()) {
+      crypto = nodeCrypto.webcrypto as Crypto;
+    } else {
+      throw new Error("Crypto API is not defined");
+    }
+  }
+
+  return crypto;
+}
 
 /**
  * Provide cryptographically strong random values.
@@ -6,7 +32,7 @@ import secureRandom from "secure-random";
  * @returns Random value.
  */
 export function getRandomValues(numberOfBytes: number): Uint8Array {
-  // const crypto = initCryptoAPI();
-  return secureRandom(numberOfBytes, { type: "Uint8Array" });
-  // return crypto.getRandomValues(new Uint8Array(numberOfBytes));
+  const crypto = initCryptoAPI();
+
+  return crypto.getRandomValues(new Uint8Array(numberOfBytes));
 }
