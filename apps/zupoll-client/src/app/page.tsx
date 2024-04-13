b0c722efd8a5362e007e2bc9c84fff58f2a3f6d8
Ivan Chub <ivansergchub@gmail.com>
2024-04-12 22:21:39 -0700

    remove console.log

--- apps/zupoll-client/src/app/page.tsx
+++ apps/zupoll-client/src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { Buffer } from "buffer";
 import { Suspense } from "react";
 import { HomePage } from "./home";
-
-console.log(Buffer);
 
 export default function Page() {
   return (
