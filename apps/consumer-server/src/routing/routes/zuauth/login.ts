de62f889143721271c94ed0bd98de061c5c7819d
Rob Knight <mail@robknight.org.uk>
2024-04-15 19:28:44 +0100

    Improved comments

--- apps/consumer-server/src/routing/routes/zuauth/login.ts
+++ apps/consumer-server/src/routing/routes/zuauth/login.ts
@@ -6,12 +6,8 @@ import { ApplicationContext } from "../../../types";
 import { SessionData } from "../../types";
 
 /**
- * The login checks the validity of the PCD, ensures that the ticket
- * is indeed supported by Zupass, and that it has been signed with the correct
- * EdDSA key. The watermark used to create the PCD and as a nonce in the
- * authentication mechanism must be the same as the one in the current session.
- * Once all checks are passed, a user session is created in which the watermark
- * and nullifier are saved.
+ * Checks that the PCD is valid by calling ZuAuth's {@link authenticate}
+ * function, then saves the ticket data to session storage if valid.
  */
 export function login(
   app: express.Application,
@@ -33,42 +29,6 @@ export function login(
         req.body.eventMetadata
       );
 
-      /*  const pcd = await ZKEdDSAEventTicketPCDPackage.deserialize(req.body.pcd);
-
-      if (!(await ZKEdDSAEventTicketPCDPackage.verify(pcd))) {
-        console.error(`[ERROR] ZK ticket PCD is not valid`);
-
-        res.status(401).send();
-        return;
-      }
-
-      if (pcd.claim.watermark.toString() !== session.watermark) {
-        console.error(`[ERROR] PCD watermark doesn't match`);
-
-        res.status(401).send();
-        return;
-      }
-
-      if (!pcd.claim.nullifierHash) {
-        console.error(`[ERROR] PCD ticket nullifier has not been defined`);
-
-        res.status(401).send();
-        return;
-      }
-
-      if (nullifiers.has(pcd.claim.nullifierHash)) {
-        console.error(`[ERROR] PCD ticket has already been used`);
-
-        res.status(401).send();
-        return;
-      }
-
-      // The PCD's nullifier is saved so that it prevents the
-      // same PCD from being reused for another login.
-      nullifiers.add(pcd.claim.nullifierHash);*/
-
-      // Save the data related to the fields revealed during the generation
-      // of the zero-knowledge proof.
       session.ticket = pcd.claim.partialTicket;
 
       await session.save();
