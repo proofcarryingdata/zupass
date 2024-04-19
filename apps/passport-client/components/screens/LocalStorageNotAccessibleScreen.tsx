56366906f44d517295146a8efae8d3c92274335d
Richard Liu <richard.y.liu@berkeley.edu>
2024-04-18 19:02:06 -0700

    copy

--- apps/passport-client/components/screens/LocalStorageNotAccessibleScreen.tsx
+++ apps/passport-client/components/screens/LocalStorageNotAccessibleScreen.tsx
@@ -1,4 +1,4 @@
-import { H2, Spacer, TextCenter } from "../core";
+import { H2, Spacer, SupportLink, TextCenter } from "../core";
 import { AppContainer } from "../shared/AppContainer";
 
 export function LocalStorageNotAccessibleScreen(): JSX.Element {
@@ -9,13 +9,15 @@ export function LocalStorageNotAccessibleScreen(): JSX.Element {
         <TextCenter>
           <H2>Please update your browser</H2>
           <Spacer h={24} />
-          Your browser does not currently support accessing local storage, which
-          is required for Zupass to store cryptographic data locally. Try
+          Your browser does not support accessing local storage, which is
+          required for Zupass to store cryptographic data. Try{" "}
           <a href="https://www.chromium.org/for-testers/bug-reporting-guidelines/uncaught-securityerror-failed-to-read-the-localstorage-property-from-window-access-is-denied-for-this-document/">
             enabling third-party cookies
           </a>{" "}
           or switching/updating your browser. You can view a list of supported
           browsers <a href="https://caniuse.com/?search=localstorage">here</a>.
+          <Spacer h={24} />
+          If you continue having issues, please email <SupportLink />.
         </TextCenter>
         <Spacer h={64} />
       </AppContainer>
