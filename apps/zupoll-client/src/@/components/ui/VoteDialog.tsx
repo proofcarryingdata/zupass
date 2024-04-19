4118e93b6989a8543d6dd1d57979caf2fef84346
Ivan Chub <ivansergchub@gmail.com>
2024-04-19 04:09:28 -0700

    center vote dialog

--- apps/zupoll-client/src/@/components/ui/VoteDialog.tsx
+++ apps/zupoll-client/src/@/components/ui/VoteDialog.tsx
@@ -29,7 +29,7 @@ export default function VoteDialog({
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
