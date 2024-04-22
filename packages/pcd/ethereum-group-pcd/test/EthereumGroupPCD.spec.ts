794be25390e5ff701d228f82d8989aa2bb3faffb
Rob Knight <mail@robknight.org.uk>
2024-04-22 18:58:47 +0200

    Restore custom timeout for Ethereum Group PCD

--- packages/pcd/ethereum-group-pcd/test/EthereumGroupPCD.spec.ts
+++ packages/pcd/ethereum-group-pcd/test/EthereumGroupPCD.spec.ts
@@ -131,6 +131,8 @@ async function happyPathEthGroupPCD(
 }
 
 describe("Ethereum Group PCD", function () {
+  // eslint-disable-next-line no-restricted-syntax
+  this.timeout(60 * 1000 * 10);
   let ethGroupPCD: EthereumGroupPCD;
 
   this.beforeAll(async function () {
