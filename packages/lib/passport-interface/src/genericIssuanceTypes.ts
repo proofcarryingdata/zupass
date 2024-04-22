4190845502f67782a998631958367b497b9a3b54
Rob Knight <mail@robknight.org.uk>
2024-04-22 08:54:45 +0200

    Issuable PODTicketPCD (#1640)
    Closes https://linear.app/0xparc-pcd/issue/0XP-724/make-a-podticketpcd
    
    I've followed the pattern from `EDdSAPCD` and `EdDSATicketPCD`, such
    that we have a `PODTicketPCD` class which contains a `PODPCD` in its
    proof.
    
    I had to make a change to `PODPCDPackage`, because the types of `PODPCD`
    and `PCD<PODClaim, PODProof>` are not equivalent. The latter describes a
    class which has an `id`, a `type`, a `claim`, and `proof`. PODPCD has
    all of those, but also has a `_pod` member, which is not accounted for
    by the generic PCD class.
    
    Because some `PCDPackage` member functions have a return type of `PCD<C,
    P>`, this means that I could not do `const pcd: PODPCD = await
    PODPCDPackage.prove(/* params */)`, because `PCD<PODClaim, PODProof>`
    does not satisfy the constraints of the `PODPCD` class. I've solved this
    by introducing a `PODPCDPackage` interface, which inherits from
    `PCDPackage<PODTicketPCDClaim, PODTicketPCDProof, PODTicketPCDArgs>` but
    provides narrower return types for `prove` and `deserialize`. Everything
    now type-checks.
    
    An alternative to this approach would be to *not* include the `_pod`
    member in the `PODPCD` class, but instead to instantiate one as needed.
    Since it's only used for verification of the PCD, it's not strictly
    necessary in the most common Zupass use-cases. Where we do verify PCDs,
    we often have a cache which would mitigate the need to keep the POD
    object in-memory for further verifications.
    
    ---
    
    Aside from this, everything is pretty straightforward. Podbox is able to
    issue `PODTicketPCD`s from the CSV pipeline, and I'll add similar
    capabilities to the Lemonade and Pretix pipelines in
    https://linear.app/0xparc-pcd/issue/0XP-533/issue-both-eddsa-and-pod-tickets.
    Here is a PODPCDTicket in my PCD collection:
    
    <img width="409" alt="image"
    src="https://github.com/proofcarryingdata/zupass/assets/20022/db016177-fbb6-4531-9c64-eae9aa38d53f">
    
    I will extend the `PODTicketPCD` tests further in the subsequent issues,
    and would appreciate any suggestions for things that should be tested
    for.

--- packages/lib/passport-interface/src/genericIssuanceTypes.ts
+++ packages/lib/passport-interface/src/genericIssuanceTypes.ts
@@ -466,7 +466,8 @@ export enum CSVPipelineOutputType {
    * {@link EdDSAMessagePCD}
    */
   Message = "EdDSAMessage",
-  Ticket = "EdDSATicket"
+  Ticket = "EdDSATicket",
+  PODTicket = "PODTicketPCD"
 }
 
 const CSVPipelineOptionsSchema = BasePipelineOptionsSchema.extend({
---
+++ packages/pcd/pod-ticket-pcd/src/PODTicketPCD.ts
@@ -0,0 +1,84 @@
+import { ObjectArgument, PCD, StringArgument } from "@pcd/pcd-types";
+import type { PODPCDProof } from "@pcd/pod-pcd";
+import type { IPODTicketData } from "./schema";
+
+/**
+ * The globally unique type name of the {@link PODTicketPCD}.
+ */
+export const PODTicketPCDTypeName = "pod-ticket-pcd";
+
+/**
+ * Copied from {@link TicketCategory} in {@link EdDSATicketPCD}.
+ */
+export enum TicketCategory {
+  ZuConnect = 0,
+  Devconnect = 1,
+  PcdWorkingGroup = 2,
+  Zuzalu = 3,
+  Generic = 4
+}
+
+/**
+ * Defines the essential parameters required for creating an {@link PODTicketPCD}.
+ */
+export type PODTicketPCDArgs = {
+  /**
+   * The EdDSA private key is a 32-byte value used to sign the message.
+   * {@link newEdDSAPrivateKey} is recommended for generating highly secure private keys.
+   */
+  privateKey: StringArgument;
+
+  /**
+   * A {@link IPODTicketData} object containing ticket information that is encoded into this PCD.
+   */
+  ticket: ObjectArgument<IPODTicketData>;
+
+  /**
+   * A string that uniquely identifies an {@link PODTicketPCD}. If this argument is not specified a random
+   * id will be generated.
+   */
+  id: StringArgument;
+};
+
+/**
+ * Defines the claim, consisting of ticket data and the public key of the
+ * signer.
+ */
+export interface PODTicketPCDClaim {
+  ticket: IPODTicketData;
+  signerPublicKey: string;
+}
+
+/**
+ * Proofs for POD ticket PCDs are identical to POD PCDs, consisting solely of
+ * a string representation of a signature.
+ */
+export type PODTicketPCDProof = PODPCDProof;
+
+export class PODTicketPCD implements PCD<PODTicketPCDClaim, PODTicketPCDProof> {
+  public type = PODTicketPCDTypeName;
+  public claim: PODTicketPCDClaim;
+  public proof: PODTicketPCDProof;
+  public id: string;
+
+  /**
+   * The PODTicketPCD consists of a claim about {@link IPODTicketData}, and a
+   * standard {@link PODPCDProof}.
+   */
+  public constructor(
+    id: string,
+    claim: PODTicketPCDClaim,
+    proof: PODTicketPCDProof
+  ) {
+    this.id = id;
+    this.claim = claim;
+    this.proof = proof;
+  }
+}
+
+/**
+ * Returns true if {@link pcd} is an {@link PODTicketPCD} or false otherwise.
+ */
+export function isPODTicketPCD(pcd: PCD): pcd is PODTicketPCD {
+  return pcd.type === PODTicketPCDTypeName;
+}
