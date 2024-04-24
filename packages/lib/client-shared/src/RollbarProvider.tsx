8a0ce6dc70cc426d7252cdc4bf10bcb05ce816cc
Ivan Chub <ivansergchub@gmail.com>
2024-04-24 15:33:58 -0700

    moving rollbar provider into client shared package

---
+++ packages/lib/client-shared/src/RollbarProvider.tsx
@@ -0,0 +1,42 @@
+import { Provider } from "@rollbar/react";
+import React from "react";
+import { Configuration } from "rollbar";
+
+export interface ClientRollbarConfig {
+  accessToken: string;
+  environmentName: string;
+}
+
+export function RollbarProvider({
+  children,
+  config
+}: {
+  children: React.ReactNode;
+  config: ClientRollbarConfig;
+}): JSX.Element {
+  if (
+    config.accessToken === undefined ||
+    config.environmentName === undefined
+  ) {
+    console.log("[ROLLBAR] missing environment variable - not starting");
+    return <>{children}</>;
+  }
+
+  const rollbarConfig: Configuration = {
+    accessToken: config.accessToken,
+    environment: config.environmentName,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      client: {
+        javascript: {
+          source_map_enabled: true
+        }
+      }
+    }
+  };
+
+  console.log("[ROLLBAR] started");
+
+  return <Provider config={rollbarConfig}>{children}</Provider>;
+}
--- packages/lib/client-shared/src/index.ts
+++
@@ -1,3 +0,0 @@
-export default function (): void {
-  // Package typescript goes here
-}
