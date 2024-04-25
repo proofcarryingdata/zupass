c4d8b5668f90a94df4024ada09d4faf12e4f4dcc
Ivan Chub <ivansergchub@gmail.com>
2024-04-25 15:57:30 -0700

    animated logged-in-as

--- apps/zupoll-client/src/app/main/LoggedInAs.tsx
+++ apps/zupoll-client/src/app/main/LoggedInAs.tsx
@@ -2,6 +2,7 @@ import { LogoutButton } from "@/components/ui/Headers";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { FaUserAstronaut } from "react-icons/fa";
+import styled from "styled-components";
 import { LoginState } from "../../types";
 
 export function LoggedInAs({
@@ -17,11 +18,11 @@ export function LoggedInAs({
 
   return (
     <div className="flex justify-between items-center w-full">
-      <span className="mr-2 flex flex-row gap-2 justify-center items-center shrink overflow-hidden">
+      <span className="mr-2 flex flex-row gap-2 justify-center items-center shrink overflow-hidden rounded text-sm bg-green-950/20 px-3 py-1">
         <FaUserAstronaut className="shrink-0" />
-        <span className="shrink text-nowrap overflow-hidden text-ellipsis text-nowrap">
+        <AnimContainer className="shrink overflow-hidden text-ellipsis text-nowrap">
           {loginState.config.name}
-        </span>
+        </AnimContainer>
       </span>
       <span className="flex flex-row gap-2">
         {showHomeButton && (
@@ -40,3 +41,27 @@ export function LoggedInAs({
     </div>
   );
 }
+
+const AnimContainer = styled.span`
+  user-select: none;
+  animation: move-bg 8s linear infinite;
+  --bg-size: 400%;
+  --color-one: rgb(152, 228, 129);
+  --color-two: rgb(250, 250, 207);
+  font-family: sans-serif;
+  background: linear-gradient(
+      90deg,
+      var(--color-one),
+      var(--color-two),
+      var(--color-one)
+    )
+    0 0 / var(--bg-size) 100%;
+  color: transparent;
+  background-clip: text;
+
+  @keyframes move-bg {
+    to {
+      background-position: var(--bg-size) 0;
+    }
+  }
+`;
