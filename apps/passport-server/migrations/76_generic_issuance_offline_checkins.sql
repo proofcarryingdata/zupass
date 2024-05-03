68fc00a023b04f0934609401cb7103e4c61c9b54
Rob Knight <mail@robknight.org.uk>
2024-05-03 13:44:50 +0100

    Remove unused field

--- apps/passport-server/migrations/76_generic_issuance_offline_checkins.sql
+++ apps/passport-server/migrations/76_generic_issuance_offline_checkins.sql
@@ -4,7 +4,5 @@ CREATE TABLE generic_issuance_offline_checkins (
   checkin_timestamp TIMESTAMPTZ NOT NULL,
   checker_email VARCHAR NOT NULL,
   attempts INT NOT NULL DEFAULT 0,
-  -- failure_reason can be null if we have not attempted this check-in yet
-  failure_reason VARCHAR,
   PRIMARY KEY(ticket_id)
 );
