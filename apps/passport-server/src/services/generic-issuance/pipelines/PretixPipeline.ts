3f26832b620c6a4d7fda81dbf8126669255e113e
Ivan Chub <ivansergchub@gmail.com>
2024-05-24 16:11:37 +0200

    invalidate ticket cache

--- apps/passport-server/src/services/generic-issuance/pipelines/PretixPipeline.ts
+++ apps/passport-server/src/services/generic-issuance/pipelines/PretixPipeline.ts
@@ -1100,11 +1100,11 @@ export class PretixPipeline implements BasePipeline {
     const ticketCopy: Partial<ITicketData> = { ...ticketData };
     // the reason we remove `timestampSigned` from the cache key
     // is that it changes every time we instantiate `ITicketData`
-    // for a particular devconnect ticket, rendering the caching
-    // ineffective.
+    // for a particular ticket, rendering the caching ineffective.
     delete ticketCopy.timestampSigned;
     const hash = await getHash(
       stable_stringify(ticketCopy) +
+        "2" +
         eddsaPrivateKey +
         pipelineId +
         ticketPCDType
