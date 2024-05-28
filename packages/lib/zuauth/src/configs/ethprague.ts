44c5f4ca3ff04632dc4235fc8d8ff650f017bdd3
Richard Liu <richard.y.liu@berkeley.edu>
2024-05-29 01:44:03 +0200

    ETHPrague zuauth

---
+++ packages/lib/zuauth/src/configs/ethprague.ts
@@ -0,0 +1,13 @@
+import type { PipelineEdDSATicketZuAuthConfig } from "@pcd/passport-interface";
+
+export const ETHPRAGUE_TICKET: PipelineEdDSATicketZuAuthConfig[] = [
+  {
+    pcdType: "eddsa-ticket-pcd",
+    publicKey: [
+      "1ebfb986fbac5113f8e2c72286fe9362f8e7d211dbc68227a468d7b919e75003",
+      "10ec38f11baacad5535525bbe8e343074a483c051aa1616266f3b1df3fb7d204"
+    ],
+    eventId: "508313ea-f16b-4729-bdf0-281c64493ca9",
+    eventName: "ETHPrague 2024"
+  }
+];
