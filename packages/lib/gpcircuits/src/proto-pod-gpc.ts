3bd19c150d3ce0bdf7eeb2154da57c73ad5f7ddc
Ahmad <root@ahmadafuni.com>
2024-06-06 11:04:38 +1000

    npm update

--- packages/lib/gpcircuits/src/proto-pod-gpc.ts
+++ packages/lib/gpcircuits/src/proto-pod-gpc.ts
@@ -561,5 +561,5 @@ export class ProtoPODGPC {
    * Version of the published artifacts on NPM which are compatible with this
    * version of the GPC circuits.
    */
-  public static ARTIFACTS_NPM_VERSION = "0.1.0";
+  public static ARTIFACTS_NPM_VERSION = "0.2.0";
 }
