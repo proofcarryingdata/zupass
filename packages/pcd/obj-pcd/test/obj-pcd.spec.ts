5afebe80d15b48e98c2b107a0e90305ec8b714bc
Ivan Chub <ivansergchub@gmail.com>
2024-06-06 18:00:13 -0700

    add obj-pcd

---
+++ packages/pcd/obj-pcd/test/obj-pcd.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import "mocha";
+
+describe("obj-pcd should work", async function () {
+  it("obj-pcd should do something", function () {
+    expect(true).to.eq(true);
+  });
+});
