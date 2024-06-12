eb71347b6f618a2c7dedd866f661b4cfccb8d893
Ivan Chub <ivansergchub@gmail.com>
2024-06-12 12:59:59 -0700

    piping through pipeline email sending through to the podbox frontend

--- apps/generic-issuance-client/src/pages/pipeline/DetailsSections/SendEmailSection.tsx
+++ apps/generic-issuance-client/src/pages/pipeline/DetailsSections/SendEmailSection.tsx
@@ -1,15 +1,43 @@
-import { PipelineInfoResponseValue } from "@pcd/passport-interface";
-import { ReactNode } from "react";
+import { Button } from "@chakra-ui/react";
+import {
+  PipelineEmailType,
+  PipelineInfoResponseValue,
+  requestGenericIssuanceSendPipelineEmail
+} from "@pcd/passport-interface";
+import { sleep } from "@pcd/util";
+import { ReactNode, useCallback, useState } from "react";
+import { ZUPASS_SERVER_URL } from "../../../constants";
+import { useJWT } from "../../../helpers/userHooks";
 
 export function SendEmailSection({
   pipelineInfo
 }: {
   pipelineInfo: PipelineInfoResponseValue;
 }): ReactNode {
+  const jwt = useJWT();
+  const [sending, setSending] = useState(false);
+  const sendEmail = useCallback(async () => {
+    if (!jwt) {
+      alert("you need to be logged in to be able to send an email");
+      return;
+    }
+
+    setSending(true);
+    requestGenericIssuanceSendPipelineEmail(
+      ZUPASS_SERVER_URL,
+      jwt,
+      PipelineEmailType.EsmeraldaOneClick
+    );
+    await sleep(2000);
+    setSending(false);
+  }, [jwt]);
+
   return (
     <div>
       this is the pipeline send section for the pipeline{" "}
-      <pre>{JSON.stringify(pipelineInfo, null, 2).substring(0, 100)}</pre>
+      <Button onClick={sendEmail} disabled={sending}>
+        {sending ? "Sending..." : "Send Esmeralda One Click Email"}
+      </Button>
     </div>
   );
 }
---
+++ packages/lib/passport-interface/src/api/requestGenericIssuanceSendPipelineEmail.ts
@@ -0,0 +1,40 @@
+import urlJoin from "url-join";
+import {
+  GenericIssuanceSendPipelineEmailRequest,
+  GenericIssuanceSendPipelineEmailResponseValue,
+  PipelineEmailType
+} from "../RequestTypes";
+import { APIResult } from "./apiResult";
+import { httpPostSimple } from "./makeRequest";
+
+/**
+ * Asks the server to get information about the currently logged in user.
+ *
+ * Never rejects. All information encoded in the resolved response.
+ */
+export async function requestGenericIssuanceSendPipelineEmail(
+  zupassServerUrl: string,
+  jwt: string,
+  pipelineId: string,
+  email: PipelineEmailType
+): Promise<GenericIssuanceSendPipelineEmailResult> {
+  return httpPostSimple(
+    urlJoin(zupassServerUrl, "/generic-issuance/api/send-pipeline-email"),
+    async (resText) => {
+      return {
+        success: true,
+        value: JSON.parse(
+          resText
+        ) as GenericIssuanceSendPipelineEmailResponseValue
+      };
+    },
+    {
+      jwt,
+      pipelineId,
+      email
+    } satisfies GenericIssuanceSendPipelineEmailRequest
+  );
+}
+
+export type GenericIssuanceSendPipelineEmailResult =
+  APIResult<GenericIssuanceSendPipelineEmailResponseValue>;
