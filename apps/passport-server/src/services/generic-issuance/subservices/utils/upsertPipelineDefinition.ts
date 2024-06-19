34bf5f0335ed97ccb429adb2578a687986269ac4
Ivan Chub <ivansergchub@gmail.com>
2024-06-19 12:34:30 -0700

    don't clear pipeline atoms on pipeline restart

--- apps/passport-server/src/services/generic-issuance/subservices/utils/upsertPipelineDefinition.ts
+++ apps/passport-server/src/services/generic-issuance/subservices/utils/upsertPipelineDefinition.ts
@@ -173,7 +173,6 @@ export async function upsertPipelineDefinition(
     validatedNewDefinition.id,
     undefined
   );
-  await pipelineSubservice.clearAtomsForPipeline(validatedNewDefinition.id);
 
   // purposely not awaited as this also performs a Pipeline load,
   // which can take an arbitrary amount of time.
