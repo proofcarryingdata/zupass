38bbd80db606689bafcc28f8df5066b130eece74
Richard Liu <richard.y.liu@berkeley.edu>
2024-06-21 02:04:51 -0700

    styling final

--- apps/passport-client/components/screens/ProtocolWorldsScreens/ProtocolWorldsHome.tsx
+++ apps/passport-client/components/screens/ProtocolWorldsScreens/ProtocolWorldsHome.tsx
@@ -1,6 +1,5 @@
 import { ProtocolWorldsFolderName } from "@pcd/passport-interface";
 import { Separator } from "@pcd/passport-ui";
-import { useEffect } from "react";
 import styled from "styled-components";
 import {
   useFolders,
@@ -9,33 +8,15 @@ import {
 } from "../../../src/appHooks";
 import { RippleLoader } from "../../core/RippleLoader";
 import { PCDCardList } from "../../shared/PCDCardList";
+import { ProtocolWorldsStyling } from "./ProtocolWorldsStyling";
 
 export function ProtocolWorldsHome(): JSX.Element {
-  useEffect(() => {
-    const rootStyle = document.documentElement.style;
-    rootStyle.setProperty("--bg-dark-primary", "rgb(141,141,141)");
-    rootStyle.setProperty("--bg-dark-gray", "rgb(109,109,109)");
-    rootStyle.setProperty("--bg-lite-primary", "black");
-    rootStyle.setProperty("--primary-dark", "rgb(174,174,174)");
-    rootStyle.setProperty("--primary-lite", "white");
-    rootStyle.setProperty("--accent-dark", "white");
-    rootStyle.setProperty("--accent-lite", "white");
-    // rootStyle.setProperty("--accent-lite", "white");
-    return () => {
-      rootStyle.removeProperty("--bg-dark-primary");
-      rootStyle.removeProperty("--bg-dark-gray");
-      rootStyle.removeProperty("--bg-lite-primary");
-      rootStyle.removeProperty("--primary-dark");
-      rootStyle.removeProperty("--primary-lite");
-      rootStyle.removeProperty("--accent-dark");
-      rootStyle.removeProperty("--accent-lite");
-    };
-  }, []);
   const foldersInFolder = useFolders(ProtocolWorldsFolderName);
   const pcdsInFolder = usePCDsInFolder(ProtocolWorldsFolderName);
   const loadedIssuedPCDs = useLoadedIssuedPCDs();
   return (
     <>
+      <ProtocolWorldsStyling />
       {!(foldersInFolder.length === 0) && <Separator />}
       {pcdsInFolder.length > 0 ? (
         <PCDCardList hideRemoveButton allExpanded pcds={pcdsInFolder} />
