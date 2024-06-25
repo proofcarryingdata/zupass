44786f5cb8684f94bb004ce15bc01eca230c30ae
Rob Knight <mail@robknight.org.uk>
2024-06-25 16:08:38 -0700

    'Add Ticket' button for CSV pipelines

---
+++ apps/generic-issuance-client/src/components/AddDataModal.tsx
@@ -0,0 +1,231 @@
+import {
+  Button,
+  ButtonGroup,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay
+} from "@chakra-ui/react";
+import { CSVPipelineDefinition } from "@pcd/passport-interface";
+import { validateEmail } from "@pcd/util";
+import { CreatableSelect } from "chakra-react-select";
+import { stringify as stringifyCSV } from "csv-stringify/sync";
+import { ReactNode, useCallback, useMemo, useState } from "react";
+import { savePipeline } from "../helpers/Mutations";
+import { useJWT } from "../helpers/userHooks";
+import { syncParseCSV } from "../pages/pipeline/PipelineEditSection/parseCSV";
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+export function AddDataModal({
+  addingData,
+  pipeline,
+  onClose
+}: {
+  addingData: boolean;
+  pipeline: CSVPipelineDefinition;
+  onClose: () => void;
+}): ReactNode {
+  const userJWT = useJWT();
+  const [inProgress, setInProgress] = useState(false);
+
+  const parsedTickets = useMemo(() => {
+    const csv = pipeline.options.csv;
+    return syncParseCSV(csv);
+  }, [pipeline.options.csv]);
+
+  const [eventNames, ticketNames, imageUrls] = useMemo(() => {
+    const eventNames = new Set<string>();
+    const ticketNames = new Set<string>();
+    const imageUrls = new Set<string>();
+
+    for (let i = 1; i < parsedTickets.length; i++) {
+      const row = parsedTickets[i];
+      eventNames.add(row[0]);
+      ticketNames.add(row[1]);
+      if (row[4]) {
+        imageUrls.add(row[4]);
+      }
+    }
+
+    return [
+      [...eventNames].map((eventName) => ({
+        label: eventName,
+        value: eventName
+      })),
+      [...ticketNames].map((ticketName) => ({
+        label: ticketName,
+        value: ticketName
+      })),
+      [...imageUrls].map((imageUrl) => ({
+        label: imageUrl,
+        value: imageUrl
+      }))
+    ];
+  }, [parsedTickets]);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [eventName, setEventName] = useState<Option | null>(eventNames[0]);
+  const [ticketName, setTicketName] = useState<Option | null>(ticketNames[0]);
+  const [imageUrl, setImageUrl] = useState<Option | null>(imageUrls[0]);
+
+  const canBeAdded =
+    name.length > 0 &&
+    email.length > 0 &&
+    validateEmail(email) &&
+    !!eventName &&
+    !!ticketName;
+
+  const addTicket = useCallback(async () => {
+    if (!eventName || !ticketName || !name || !email || !imageUrl) {
+      return;
+    }
+
+    if (
+      !confirm(
+        `are you sure you want to add this ticket?
+
+name: ${name}
+email: ${email}
+event: ${eventName.label}
+ticket: ${ticketName.label}`
+      )
+    ) {
+      return;
+    }
+
+    const pipelineCopy = JSON.parse(
+      JSON.stringify(pipeline)
+    ) as CSVPipelineDefinition;
+
+    const newTickets = parsedTickets.concat([
+      [eventName.value, ticketName.value, name, email, imageUrl.value, ""]
+    ]);
+
+    pipelineCopy.options.csv = stringifyCSV(newTickets);
+
+    if (!userJWT) {
+      alert("not logged in");
+      return;
+    }
+
+    setInProgress(true);
+    const res = await savePipeline(userJWT, JSON.stringify(pipelineCopy));
+
+    if (res.success) {
+      window.location.reload();
+    } else {
+      setInProgress(false);
+      alert(res.error);
+    }
+  }, [
+    email,
+    eventName,
+    imageUrl,
+    name,
+    parsedTickets,
+    pipeline,
+    ticketName,
+    userJWT
+  ]);
+
+  const triggerClose = useCallback(() => {
+    setName("");
+    setEmail("");
+    setEventName(eventNames[0]);
+    setTicketName(ticketNames[0]);
+    setImageUrl(imageUrls[0]);
+    setInProgress(false);
+    onClose();
+  }, [eventNames, imageUrls, onClose, ticketNames]);
+
+  return (
+    <Modal
+      onClose={triggerClose}
+      isOpen={addingData}
+      isCentered
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add Ticket</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormControl mb={2}>
+            <FormLabel>Event Name</FormLabel>
+            <CreatableSelect
+              options={eventNames}
+              value={eventName}
+              onChange={(newValue) => setEventName(newValue)}
+            />
+          </FormControl>
+          <FormControl mb={2}>
+            <FormLabel>Ticket Name</FormLabel>
+            <CreatableSelect
+              options={ticketNames}
+              value={ticketName}
+              onChange={(newValue) => setTicketName(newValue)}
+            />
+          </FormControl>
+          <FormControl mb={2}>
+            <FormLabel>Attendee Name</FormLabel>
+            <Input
+              value={name}
+              onChange={(e): void => setName(e.target.value)}
+              placeholder="Bob Glob"
+              type="text"
+              width="sm"
+              maxW={"100%"}
+            />
+          </FormControl>
+
+          <FormControl mb={2}>
+            <FormLabel>Attendee Email</FormLabel>
+            <Input
+              isInvalid={!validateEmail(email) && email.length !== 0}
+              value={email}
+              onChange={(e): void => setEmail(e.target.value)}
+              placeholder="email@provider.tld"
+              type="email"
+              width="sm"
+              maxW={"100%"}
+              autoComplete="off"
+              data-1p-ignore
+            />
+          </FormControl>
+
+          <FormControl mb={2}>
+            <FormLabel>Image URL</FormLabel>
+            <CreatableSelect
+              options={imageUrls}
+              value={imageUrl}
+              onChange={(newValue) => setImageUrl(newValue)}
+            />
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <ButtonGroup>
+            <Button
+              onClick={addTicket}
+              colorScheme="blue"
+              isDisabled={!canBeAdded || inProgress}
+            >
+              {inProgress ? "Adding..." : "Add"}
+            </Button>
+            <Button onClick={triggerClose}>Cancel</Button>
+          </ButtonGroup>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
