4abbb562693acd23d32b87a45b687e79bbdeaaf7
Ivan Chub <ivansergchub@gmail.com>
2024-07-24 13:14:28 -0700

    install tailwind, add it to build/dev workflows

---
+++ apps/passport-client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
