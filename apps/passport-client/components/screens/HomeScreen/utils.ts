b1a617004a8d9fcf2f3345e59294a11af85df31e
Ivan Chub <ivansergchub@gmail.com>
2024-07-25 17:38:51 -0700

    fixed up the dates

--- apps/passport-client/components/screens/HomeScreen/utils.ts
+++ apps/passport-client/components/screens/HomeScreen/utils.ts
@@ -12,27 +12,31 @@ export interface EventInfo {
 
 export const EVENTS: Record<string, EventInfo> = {
   "ETH Berlin 04": {
-    start: "2023-04-01",
-    end: "2023-04-03",
+    start: "2024-05-24",
+    end: "2024-05-26",
     image:
       "https://ethberlin.org/static/ligi-mask-2cf891892bc3a7e7b3fa7d18d27edf9c.jpg"
   },
-  "0xPARC Summer '24": { start: "2023-05-15", end: "2023-05-18" },
+  "0xPARC Summer '24": {
+    start: "2024-06-17",
+    end: "2024-08-25",
+    image: "https://pbs.twimg.com/media/F_F2axBXQAAx9V7?format=jpg&name=large"
+  },
   "Edge Esmeralda": {
-    start: "2023-06-10",
-    end: "2023-06-12",
+    start: "2024-06-02",
+    end: "2024-06-30",
     image:
       "https://cdn.prod.website-files.com/65e8d8e39d148666896efd73/65e8d9c0db3e30b4fd35b335_kuri%201-c.png"
   },
   "ETH Prague 2024": {
-    start: "2023-07-22",
-    end: "2023-07-25",
+    start: "2024-05-31",
+    end: "2024-06-02",
     image:
       "https://lp-cms-production.imgix.net/2023-08/PRAGUE-GettyImages-1182432355.jpg?w=1920&h=640&fit=crop&crop=faces%2Cedges&auto=format&q=75"
   },
   "ETH LATAM SPS": {
-    start: "2023-08-05",
-    end: "2023-08-07",
+    start: "2024-03-13",
+    end: "2024-03-14",
     image:
       "https://www.state.gov/wp-content/uploads/2023/07/shutterstock_671409553v2.jpg"
   },
@@ -43,8 +47,8 @@ export const EVENTS: Record<string, EventInfo> = {
   //     "https://cdn.prod.website-files.com/65b2cb5abdecf7cd7747e170/65c139390d09b586db66b032_bg-image_v01.png"
   // },
   EthDenver: {
-    start: "2023-11-18",
-    end: "2023-11-21",
+    start: "2024-02-23",
+    end: "2024-03-03",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Denver_Skyline_in_Winter.JPG/800px-Denver_Skyline_in_Winter.JPG"
   },
@@ -94,7 +98,8 @@ export async function initTestData(state: AppState): Promise<void> {
       "ETH Berlin 04",
       "ETH Prague 2024",
       "Old Event 1",
-      "Old Event 2"
+      "Old Event 2",
+      "0xPARC Summer '24"
     ]
   } as const;
 
