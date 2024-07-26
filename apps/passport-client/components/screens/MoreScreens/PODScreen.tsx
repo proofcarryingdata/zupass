2024f624dc250a0224361f715706b457f4c91825
Ivan Chub <ivansergchub@gmail.com>
2024-07-26 11:51:28 -0700

    implementing the pod screen

--- apps/passport-client/components/screens/MoreScreens/PODScreen.tsx
+++ apps/passport-client/components/screens/MoreScreens/PODScreen.tsx
@@ -1,11 +1,15 @@
 import { Spacer } from "@pcd/passport-ui";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { NewButton } from "../../NewButton";
-import { H1, Placeholder, ZuLogo } from "../../core";
+import { BigInput, H1, Placeholder, ZuLogo } from "../../core";
 import { MaybeModal } from "../../modals/Modal";
 import { AppContainer } from "../../shared/AppContainer";
 
 export function PODScreen(): ReactNode {
+  const [title, setTitle] = useState("TITLE");
+  const [description, setDescription] = useState("DESCRIPTION");
+  const [image, setImage] = useState("");
+
   return (
     <>
       <MaybeModal />
@@ -16,14 +20,33 @@ export function PODScreen(): ReactNode {
         </div>
         <Spacer h={24} />
         <Placeholder minH={540}>
-          <NewButton
-            onClick={() => {
-              window.location.href = "#/more";
-            }}
-          >
-            Back
-          </NewButton>
-          Duck
+          <div className="flex flex-col gap-2">
+            <NewButton
+              onClick={() => {
+                window.location.href = "#/more";
+              }}
+            >
+              Back
+            </NewButton>
+
+            <BigInput
+              placeholder="POD Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <BigInput
+              placeholder="POD Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <BigInput
+              placeholder="POD IMAGE"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            />
+
+            <NewButton>CREATE POD</NewButton>
+          </div>
         </Placeholder>
         <Spacer h={24} />
       </AppContainer>
