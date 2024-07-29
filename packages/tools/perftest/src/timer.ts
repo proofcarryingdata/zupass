de3ba3ba9e08422352c038dac2c34af22ce5988c
Rob Knight <mail@robknight.org.uk>
2024-07-29 15:10:29 +0200

    Benchmarking contentID

--- packages/tools/perftest/src/timer.ts
+++ packages/tools/perftest/src/timer.ts
@@ -10,7 +10,11 @@ import {
   EdDSATicketVerifyCase
 } from "./cases/EdDSATicketTimer";
 import { GPCPCDProveCase, GPCPCDVerifyCase } from "./cases/GPCTimer";
-import { PODProveCase, PODVerifyCase } from "./cases/PODTimer";
+import {
+  PODGetMerkleTreeCase,
+  PODProveCase,
+  PODVerifyCase
+} from "./cases/PODTimer";
 import {
   SemaphoreSignatureProveCase,
   SemaphoreSignatureVerifyCase
@@ -28,7 +32,8 @@ const TIME_TEST_CONFIGS: Record<string, Record<string, () => TimerCase>> = {
   },
   [PODPCDPackage.name]: {
     prove: () => new PODProveCase(),
-    verify: () => new PODVerifyCase()
+    verify: () => new PODVerifyCase(),
+    contentID: () => new PODGetMerkleTreeCase()
   },
   [SemaphoreSignaturePCDPackage.name]: {
     prove: () => new SemaphoreSignatureProveCase(),
