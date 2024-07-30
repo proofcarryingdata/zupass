e03634369f5fd0d29f697e5992ce7158d4359232
Ivan Chub <ivansergchub@gmail.com>
2024-07-30 12:39:30 -0700

    update the migration to actually make sense

--- apps/passport-server/migrations/82_user_emails.sql
+++ apps/passport-server/migrations/82_user_emails.sql
@@ -1,17 +1,14 @@
 
--- Create new table referencing users table
 CREATE TABLE user_emails (
     user_id UUID NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES users(uuid) ON DELETE CASCADE,
     PRIMARY KEY (user_id, email)
 );
 
--- Copy all user emails into the new table
 INSERT INTO user_emails (user_id, email)
-SELECT uuid as user_id, unnest(emails) AS email
+SELECT uuid as user_id, email
 FROM users
-WHERE emails IS NOT NULL;
+WHERE email IS NOT NULL;
 
--- Drop the email column from the users table
-ALTER TABLE users DROP COLUMN emails;
+ALTER TABLE users DROP COLUMN email;
