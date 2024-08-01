4dc89f0f65ad719edd22478bcc9f4887b5c1ac3a
Andrew Twyman <artwyman@users.noreply.github.com>
2024-07-31 17:33:51 -0700

    Nicer display for GPC PCD (#1820)
    This gives the GPCPCD display card the same improvements as the PODPCD:
    - Scrolling text boxes
    - Prettier button for verification
    - Verification error display
    - Link to docs in description
    - General text cleanup
    
    Note that most users won't ever see this display, which only appears if
    a proof PCD is added to your Zupass. It may as well be consistent and
    nice, though, if/when it comes up. The prove screen is unchanged, and
    already looks fine.

--- packages/ui/gpc-pcd-ui/src/CardBody.tsx
+++ packages/ui/gpc-pcd-ui/src/CardBody.tsx
@@ -1,52 +1,95 @@
 import { serializeGPCBoundConfig, serializeGPCRevealedClaims } from "@pcd/gpc";
 import { GPCPCD, GPCPCDPackage } from "@pcd/gpc-pcd";
-import { FieldLabel, Separator, Spacer, styled } from "@pcd/passport-ui";
+import {
+  Button,
+  ErrorContainer,
+  FieldLabel,
+  Separator,
+  Spacer,
+  TextContainer,
+  styled
+} from "@pcd/passport-ui";
 import { PCDUI } from "@pcd/pcd-types";
+import { getErrorMessage } from "@pcd/util";
 import { useState } from "react";
 
 export const GPCPCDUI: PCDUI<GPCPCD> = {
   renderCardBody: GPCCardBody
 };
 
-function GPCCardBody({ pcd }: { pcd: GPCPCD }): JSX.Element {
-  const [verifyStatus, setVerifyStatus] = useState("unverified");
-
-  return (
-    <Container>
-      <p>
-        This PCD represents ZK proof about one or more POD (Provable Object
-        Data) objects, generated using a GPC (General Purpose Circuit). The
-        proof configuration determines what specifically has been proven. This
-        may include revealing some of the POD data, or keeping it all hidden.
-      </p>
-
-      <Separator />
-
-      <FieldLabel>GPC Proof Config</FieldLabel>
-      <pre>{serializeGPCBoundConfig(pcd.claim.config, 2)}</pre>
-      <Spacer h={8} />
-
-      <FieldLabel>Revealed Claims</FieldLabel>
-      <pre>{serializeGPCRevealedClaims(pcd.claim.revealed, 2)}</pre>
-      <Spacer h={8} />
-      <label>
-        <button
-          onClick={async (): Promise<void> =>
-            setVerifyStatus(
-              (await GPCPCDPackage.verify(pcd)) ? "valid ✅" : "invalid ❌"
-            )
-          }
-        >
-          Check
-        </button>
-        Proof is {verifyStatus}
-      </label>
-    </Container>
-  );
-}
-
 const Container = styled.div`
   padding: 16px;
   overflow: hidden;
   width: 100%;
 `;
+
+function GPCCardBody({ pcd }: { pcd: GPCPCD }): JSX.Element {
+  const [error, setError] = useState<string | undefined>();
+  const [verifyStatus, setVerifyStatus] = useState<number>(0);
+
+  const proofButtonColor: React.CSSProperties = {};
+  if (verifyStatus > 0) {
+    proofButtonColor.color = "white";
+    proofButtonColor.background = "green";
+  } else if (verifyStatus < 0) {
+    proofButtonColor.color = "white";
+    proofButtonColor.background = "var(--danger)";
+  }
+
+  return (
+    <Container>
+      <p>
+        This is a ZK proof of info about PODs using a General Purpose Circuit.
+        You can learn more about PODs and GPCs{" "}
+        <a href="https://zupass.org/pod">here</a>.
+      </p>
+
+      <Separator />
+
+      <FieldLabel>Proof Config</FieldLabel>
+      <p>This specifies what has been proven and revealed.</p>
+      <TextContainer style={{ overflowX: "auto" }}>
+        <pre>{serializeGPCBoundConfig(pcd.claim.config, 2)}</pre>
+      </TextContainer>
+      <Spacer h={8} />
+
+      <FieldLabel>Revealed Claims</FieldLabel>
+      <p>These are the entries and metadata revealed in the proof.</p>
+      <TextContainer style={{ overflowX: "auto" }}>
+        <pre>{serializeGPCRevealedClaims(pcd.claim.revealed, 2)}</pre>
+      </TextContainer>
+      <Spacer h={8} />
+      <Button
+        style="primary"
+        size="small"
+        onClick={async (): Promise<void> => {
+          const verifyResult = await verifyProof(pcd);
+          setError(verifyResult.errorMessage);
+          setVerifyStatus(verifyResult.isValid ? 1 : -1);
+        }}
+        styles={{ float: "right", ...proofButtonColor }}
+      >
+        {verifyStatus === 0
+          ? "Check proof"
+          : verifyStatus > 0
+          ? "Valid proof"
+          : error !== undefined
+          ? "Verification error!"
+          : "Invalid proof!"}
+      </Button>
+      {error === undefined ? null : <ErrorContainer>{error}</ErrorContainer>}
+    </Container>
+  );
+}
+
+async function verifyProof(pcd: GPCPCD): Promise<{
+  isValid: boolean;
+  errorMessage?: string;
+}> {
+  try {
+    const isValid = await GPCPCDPackage.verify(pcd);
+    return { isValid };
+  } catch (e) {
+    return { isValid: false, errorMessage: getErrorMessage(e) };
+  }
+}
