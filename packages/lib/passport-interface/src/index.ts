e2a1521f15e3b959a60664c8c561335683ad7ab1
Ivan Chub <ivansergchub@gmail.com>
2024-08-02 12:51:00 -0700

    fix build

--- packages/lib/passport-interface/src/index.ts
+++ packages/lib/passport-interface/src/index.ts
@@ -37,8 +37,6 @@ export * from "./api/requestIssuanceServiceEnabled";
 export * from "./api/requestKnownTickets";
 export * from "./api/requestListFeeds";
 export * from "./api/requestLogToServer";
-export * from "./api/requestOfflineTickets";
-export * from "./api/requestOfflineTicketsCheckin";
 export * from "./api/requestOneClickLogin";
 export * from "./api/requestPasswordSalt";
 export * from "./api/requestPipelineInfo";
