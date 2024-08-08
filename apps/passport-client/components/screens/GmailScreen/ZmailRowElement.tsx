62363a8999c4cdc7a37fc23a8cd1e96c5661d9cb
Ivan Chub <ivansergchub@gmail.com>
2024-08-08 14:55:09 -0700

    more

--- apps/passport-client/components/screens/GmailScreen/ZmailRowElement.tsx
+++ apps/passport-client/components/screens/GmailScreen/ZmailRowElement.tsx
@@ -4,8 +4,13 @@ import { ZmailRow } from "./ZmailTable";
 
 export function ZmailRowElement({ row }: { row: Row<ZmailRow> }): ReactNode {
   return (
-    <div className="border-b-2 border-gray-200 px-4 py-[0.1em] cursor-pointer hover:bg-gray-100">
-      <span className="font-bold">{row.original.name}</span>
+    <div className="border-b-2 border-gray-200 px-4 py-[0.1em] cursor-pointer hover:bg-gray-100 select-none flex flex-row items-center justify-between whitespace-nowrap">
+      <span className="font-bold flex-grow">{row.original.name}</span>
+      <span className="">
+        <span className="italic">{row.original.folder}</span>
+        {" · "}
+        {row.original.type}
+      </span>
     </div>
   );
 }
