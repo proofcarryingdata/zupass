a234e9134ac841b1f4d85d5f9131db709dc2b0d6
Ivan Chub <ivansergchub@gmail.com>
2024-08-09 10:12:07 -0700

    more

--- apps/passport-client/components/screens/GmailScreen/ZmailRowElement.tsx
+++ apps/passport-client/components/screens/GmailScreen/ZmailRowElement.tsx
@@ -18,7 +18,7 @@ export function ZmailRowElement({ row }: { row: Row<ZmailRow> }): ReactNode {
       onClick={() => {
         ctx.update({ viewingPCDID: row.original.pcd.id });
       }}
-      className="border-b-2 border-gray-200 px-4 py-[0.1em] cursor-pointer hover:bg-gray-100 select-none flex flex-row items-center justify-between whitespace-nowrap"
+      className="border-b-2 border-gray-200 px-4 py-[0.1em] cursor-pointer hover:bg-gray-100 active:bg-gray-200 select-none flex flex-row items-center justify-between whitespace-nowrap transition-colors duration-100"
     >
       <span className={cn("flex-grow pr-2", meta?.viewed ? "" : "font-bold")}>
         {row.original.name}
