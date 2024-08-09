80590b0e51b75246d0ddc98f2cfaa18191e15b01
Ivan Chub <ivansergchub@gmail.com>
2024-08-09 10:44:07 -0700

    display quantity of pcds in the sidebar

--- apps/passport-client/components/screens/GmailScreen/ZmailSidebar.tsx
+++ apps/passport-client/components/screens/GmailScreen/ZmailSidebar.tsx
@@ -10,46 +10,56 @@ export function ZmailSidebar(): ReactNode {
   const sortedFolders = useMemo(() => {
     return folders.sort();
   }, [folders]);
-  const onClick = useCallback(() => {
-    let filters = ctx.filters;
-    if (filters.find((filter) => filter.id === folderNameToFilterId(f))) {
-      filters = filters.filter(
-        (filter) => filter.id !== folderNameToFilterId(f)
-      );
-    } else {
-      filters = filters.filter((f) => !isFolderFilterId(f.id));
-      filters.push({
-        filter: (pcd, pcds) => {
-          return pcds.getFolderOfPCD(pcd.id) === f;
-        },
-        id: folderNameToFilterId(f)
-      });
-    }
-    ctx.update({ filters, viewingPCDID: undefined });
-  }, [ctx]);
+
+  const onClick = useCallback(
+    (f: string) => {
+      let filters = ctx.filters;
+      if (filters.find((filter) => filter.id === folderNameToFilterId(f))) {
+        filters = filters.filter(
+          (filter) => filter.id !== folderNameToFilterId(f)
+        );
+      } else {
+        filters = filters.filter((f) => !isFolderFilterId(f.id));
+        filters.push({
+          filter: (pcd, pcds) => {
+            return pcds.getFolderOfPCD(pcd.id) === f;
+          },
+          id: folderNameToFilterId(f)
+        });
+      }
+      ctx.update({ filters, viewingPCDID: undefined });
+    },
+    [ctx]
+  );
 
   return (
     <div className="h-full flex flex-col gap-4 p-4">
       <ZmailSearch />
 
       <div className="select-none flex flex-col gap-1 flex-grow overflow-y-scroll">
-        {sortedFolders.map((f) => (
-          <div
-            key={f}
-            onClick={onClick}
-            className={cn(
-              "bg-[#206b5e] border-2 box-border border-[#206b5e] hover:bg-[#1b8473] active:bg-[#239b87]",
-              "cursor-pointer px-2 py-1 rounded transition-colors duration-100 active:border-[#eee]",
-              ctx.filters.find(
-                (filter) => filter.id === folderNameToFilterId(f)
-              )
-                ? "bg-[#1b8473] hover:[#154940] text-white border-[#eee]"
-                : ""
-            )}
-          >
-            {f}
-          </div>
-        ))}
+        {sortedFolders.map((f) => {
+          const pcdsInFolder = ctx.pcds.getAllPCDsInFolder(f);
+          return (
+            <div
+              key={f}
+              onClick={() => onClick(f)}
+              className={cn(
+                "bg-[#206b5e] border-2 box-border border-[#206b5e] hover:bg-[#1b8473] active:bg-[#239b87]",
+                "cursor-pointer px-2 py-1 rounded transition-colors duration-100 active:border-[#eee]",
+                ctx.filters.find(
+                  (filter) => filter.id === folderNameToFilterId(f)
+                )
+                  ? "bg-[#1b8473] hover:[#154940] text-white border-[#eee]"
+                  : ""
+              )}
+            >
+              <div className="flex flex-row justify-between items-center w-full">
+                <span>{f}</span>
+                <span>{pcdsInFolder.length}</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
