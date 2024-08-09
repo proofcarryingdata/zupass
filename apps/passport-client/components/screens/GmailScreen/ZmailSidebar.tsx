4d2e61efebd28deb3637178b87bf92e39062f1cd
Ivan Chub <ivansergchub@gmail.com>
2024-08-09 10:37:26 -0700

    more

--- apps/passport-client/components/screens/GmailScreen/ZmailSidebar.tsx
+++ apps/passport-client/components/screens/GmailScreen/ZmailSidebar.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useMemo } from "react";
+import { ReactNode, useCallback, useMemo } from "react";
 import { cn } from "../../../src/util";
 import { useZmailContext } from "./ZmailContext";
 import { folderNameToFilterId, isFolderFilterId } from "./ZmailFilter";
@@ -10,6 +10,23 @@ export function ZmailSidebar(): ReactNode {
   const sortedFolders = useMemo(() => {
     return folders.sort();
   }, [folders]);
+  const onClick = useCallback(() => {
+    let filters = ctx.filters;
+    if (filters.find((filter) => filter.id === folderNameToFilterId(f))) {
+      filters = filters.filter(
+        (filter) => filter.id !== folderNameToFilterId(f)
+      );
+    } else {
+      filters = filters.filter((f) => !isFolderFilterId(f.id));
+      filters.push({
+        filter: (pcd, pcds) => {
+          return pcds.getFolderOfPCD(pcd.id) === f;
+        },
+        id: folderNameToFilterId(f)
+      });
+    }
+    ctx.update({ filters, viewingPCDID: undefined });
+  }, [ctx]);
 
   return (
     <div className="h-full flex flex-col gap-4 p-4">
@@ -19,25 +36,7 @@ export function ZmailSidebar(): ReactNode {
         {sortedFolders.map((f) => (
           <div
             key={f}
-            onClick={() => {
-              let filters = ctx.filters;
-              if (
-                filters.find((filter) => filter.id === folderNameToFilterId(f))
-              ) {
-                filters = filters.filter(
-                  (filter) => filter.id !== folderNameToFilterId(f)
-                );
-              } else {
-                filters = filters.filter((f) => !isFolderFilterId(f.id));
-                filters.push({
-                  filter: (pcd, pcds) => {
-                    return pcds.getFolderOfPCD(pcd.id) === f;
-                  },
-                  id: folderNameToFilterId(f)
-                });
-              }
-              ctx.update({ filters, viewingPCDID: undefined });
-            }}
+            onClick={onClick}
             className={cn(
               "bg-[#206b5e] border-2 box-border border-[#206b5e] hover:bg-[#1b8473] active:bg-[#239b87]",
               "cursor-pointer px-2 py-1 rounded transition-colors duration-100 active:border-[#eee]",
