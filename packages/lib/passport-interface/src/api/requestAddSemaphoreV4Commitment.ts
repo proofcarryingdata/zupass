b45915adf99ac05faad0b3439b4849f86fd10959
Ivan Chub <ivansergchub@gmail.com>
2024-08-16 15:01:48 -0700

    wrote some code that creates a semaphore v4 update-ion request

--- packages/lib/passport-interface/src/api/requestAddSemaphoreV4Commitment.ts
+++ packages/lib/passport-interface/src/api/requestAddSemaphoreV4Commitment.ts
@@ -1,3 +1,16 @@
+import { PCDCollection } from "@pcd/pcd-collection";
+import { ArgumentTypeName } from "@pcd/pcd-types";
+import { PODPCDPackage } from "@pcd/pod-pcd";
+import {
+  SemaphoreIdentityPCD,
+  SemaphoreIdentityPCDPackage
+} from "@pcd/semaphore-identity-pcd";
+import {
+  SemaphoreIdentityV4PCD,
+  SemaphoreIdentityV4PCDPackage
+} from "@pcd/semaphore-identity-v4";
+import { SemaphoreSignaturePCDPackage } from "@pcd/semaphore-signature-pcd";
+import { randomUUID, toHexString } from "@pcd/util";
 import urljoin from "url-join";
 import {
   AddV4CommitmentRequest,
@@ -26,3 +39,56 @@ export async function requestAddSemaphoreV4Commitment(
 }
 
 export type AddV4CommitmentResult = APIResult<AddV4CommitmentResponseValue>;
+
+export async function makeAddV4CommitmentRequest(
+  pcdCollection: PCDCollection
+): Promise<AddV4CommitmentRequest> {
+  const v3PCD = pcdCollection.getPCDsByType(
+    SemaphoreIdentityPCDPackage.name
+  )[0] as SemaphoreIdentityPCD;
+  const v4PCD = pcdCollection.getPCDsByType(
+    SemaphoreIdentityV4PCDPackage.name
+  )[0] as SemaphoreIdentityV4PCD;
+
+  if (!v3PCD || !v4PCD) {
+    throw new Error("Expected exactly one v3 and v4 PCD");
+  }
+
+  const v4SigOfV3Claim = await PODPCDPackage.prove({
+    entries: {
+      argumentType: ArgumentTypeName.Object,
+      value: {
+        signedValue: {
+          type: "string",
+          value: JSON.stringify(v3PCD)
+        }
+      }
+    },
+    privateKey: {
+      argumentType: ArgumentTypeName.String,
+      value:
+        typeof v4PCD.claim.identity.privateKey === "string"
+          ? v4PCD.claim.identity.privateKey
+          : toHexString(v4PCD.claim.identity.privateKey)
+    },
+    id: {
+      argumentType: ArgumentTypeName.String,
+      value: randomUUID()
+    }
+  });
+
+  const v3SigOfV4Sig = await SemaphoreSignaturePCDPackage.prove({
+    identity: {
+      argumentType: ArgumentTypeName.PCD,
+      value: await SemaphoreIdentityPCDPackage.serialize(v3PCD)
+    },
+    signedMessage: {
+      argumentType: ArgumentTypeName.String,
+      value: JSON.stringify(await PODPCDPackage.serialize(v4SigOfV3Claim))
+    }
+  });
+
+  return {
+    pcd: await SemaphoreSignaturePCDPackage.serialize(v3SigOfV4Sig)
+  };
+}
