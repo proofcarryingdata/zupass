8a8d2c26f4c35846865f73c085ff925ed7cc98e5
Rob Knight <mail@robknight.org.uk>
2024-08-17 00:16:02 +0200

    Tuple matching in queries

--- packages/lib/podspec/test/podspec.spec.ts
+++ packages/lib/podspec/test/podspec.spec.ts
@@ -3,6 +3,7 @@ import { derivePublicKey } from "@zk-kit/eddsa-poseidon";
 import { assert, expect } from "chai";
 import crypto from "crypto";
 import "mocha";
+import { v4 as uuidv4 } from "uuid";
 import { p } from "../src/index";
 
 function generateRandomHex(byteLength: number): string {
@@ -210,7 +211,60 @@ describe("podspec should work", async function () {
     const pods = [
       POD.sign({ foo: { type: "int", value: 1n } }, key),
       POD.sign({ foo: { type: "int", value: 11n } }, key),
-      POD.sign({ foo: { type: "int", value: 0n } }, key)
+      POD.sign({ foo: { type: "int", value: 0n } }, key),
+      POD.sign({ foo: { type: "int", value: 10n } }, key)
+    ];
+
+    const queryResult = myPodSpec.query(pods);
+
+    expect(queryResult.matches).to.eql([pods[0], pods[3]]);
+    expect(queryResult.matchingIndexes).to.eql([0, 3]);
+  });
+
+  it("should match on tuples in queries", function () {
+    const key = generateRandomHex(32);
+    const pubKey = encodePublicKey(derivePublicKey(key));
+    const eventId = "d1390b7b-4ccb-42bf-8c8b-e397b7c26e6c";
+    const productId = "d38f0c3f-586b-44c6-a69a-1348481e927d";
+
+    const myPodSpec = p
+      .entries({
+        eventId: p.string(),
+        productId: p.string(),
+        // @todo add a special check for signer public keys
+        // for now, treat it as an entry
+        signerPublicKey: p.eddsaPubKey()
+      })
+      .matchTuple({
+        name: "test",
+        exclude: false,
+        entries: ["eventId", "productId", "signerPublicKey"],
+        members: [
+          [
+            { type: "string", value: eventId },
+            { type: "string", value: productId },
+            { type: "eddsa_pubkey", value: pubKey }
+          ]
+        ]
+      });
+
+    const pods = [
+      POD.sign(
+        {
+          eventId: { type: "string", value: eventId },
+          productId: { type: "string", value: productId },
+          signerPublicKey: { type: "eddsa_pubkey", value: pubKey }
+        },
+        key
+      ),
+      POD.sign(
+        {
+          eventId: { type: "string", value: uuidv4() },
+          productId: { type: "string", value: uuidv4() },
+          signerPublicKey: { type: "eddsa_pubkey", value: pubKey }
+        },
+        key
+      )
     ];
 
     const queryResult = myPodSpec.query(pods);
