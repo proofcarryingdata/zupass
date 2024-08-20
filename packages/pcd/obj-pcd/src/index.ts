aeeed64d86f99028c382cf279d785147333d5302
Ivan Chub <ivansergchub@gmail.com>
2024-08-20 11:02:14 -0700

    completely remove obj pcd

--- packages/pcd/obj-pcd/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./ObjPCD";
-export * from "./ObjPCDPackage";
--- packages/pcd/obj-pcd/test/obj-pcd.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import "mocha";
-
-describe("obj-pcd should work", async function () {
-  it("obj-pcd should do something", function () {
-    expect(true).to.eq(true);
-  });
-});
