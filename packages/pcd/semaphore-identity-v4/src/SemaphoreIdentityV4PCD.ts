a46233a32a3d67fc8c150a07a4dc65cf84eb2746
Ivan Chub <ivansergchub@gmail.com>
2024-08-29 16:26:31 -0700

    comments to semaphore identity v4 pcd

--- packages/pcd/semaphore-identity-v4/src/SemaphoreIdentityV4PCD.ts
+++ packages/pcd/semaphore-identity-v4/src/SemaphoreIdentityV4PCD.ts
@@ -3,22 +3,34 @@ import { Identity } from "@semaphore-protocol/identity";
 
 export const SemaphoreIdentityV4PCDTypeName = "semaphore-identity-v4-pcd";
 
+/**
+ * @param identity - The semaphore v4 identity to be stored in the PCD.
+ */
 export type SemaphoreIdentityV4PCDArgs = {
   identity: Identity;
 };
 
+/**
+ * @param identity - The semaphore v4 identity represented by the PCD.
+ */
 export interface SemaphoreIdentityV4PCDClaim {
   identity: Identity;
 }
 
 export type SemaphoreIdentityV4PCDProof = undefined;
 
+/**
+ * @returns true if the PCD is a semaphore identity v4 PCD.
+ */
 export function isSemaphoreIdentityV4PCD(
   pcd: PCD
 ): pcd is SemaphoreIdentityV4PCD {
   return pcd.type === SemaphoreIdentityV4PCDTypeName;
 }
 
+/**
+ * A PCD representing a semaphore v4 identity.
+ */
 export class SemaphoreIdentityV4PCD
   implements PCD<SemaphoreIdentityV4PCDClaim, SemaphoreIdentityV4PCDProof>
 {
