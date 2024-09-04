d1dce6e284a80a846558ed70d77e705867865afb
Ivan Chub <ivansergchub@gmail.com>
2024-09-03 19:18:40 -0700

    copy over semaphore v4 tests from semaphore v4 identity pcd package into the original semaphore identity pcd package

---
+++ packages/pcd/semaphore-identity-pcd/test/V4Functionality.spec.ts
@@ -0,0 +1,48 @@
+import { decodePrivateKey, encodePublicKey, POD } from "@pcd/pod";
+import {
+  v4PublicKey,
+  v4PublicKeyToCommitment
+} from "@pcd/semaphore-identity-pcd";
+import { expect } from "chai";
+import "mocha";
+import { v3tov4Identity, v4PrivateKey } from "../src";
+import { IdentityV3, IdentityV4 } from "../src/forwardedTypes";
+
+describe("Semaphore Identity PCD", function () {
+  it("public and private key extraction should work", async function () {
+    const original = new IdentityV4();
+    expect(original.export()).to.eq(
+      IdentityV4.import(v4PrivateKey(original)).export()
+    );
+    const pod = POD.sign(
+      { a: { type: "int", value: 0n } },
+      v4PrivateKey(original)
+    );
+    expect(pod.signerPublicKey).to.eq(v4PublicKey(original));
+  });
+
+  it("v4PublicKey", function () {
+    const identity = new IdentityV4();
+    const privateKey = identity.export();
+    const pod = POD.sign({ a: { type: "int", value: 0n } }, privateKey);
+    expect(pod.signerPublicKey).to.eq(v4PublicKey(identity));
+    expect(encodePublicKey(identity.publicKey)).to.eq(v4PublicKey(identity));
+  });
+
+  it("v4PublicKeyToCommitment", function () {
+    const identity = new IdentityV4();
+    expect(identity.commitment.toString()).to.eq(
+      v4PublicKeyToCommitment(v4PublicKey(identity))
+    );
+  });
+
+  it("v3tov4Identity is deterministic", function () {
+    const v3Identity = new IdentityV3();
+    expect(v3tov4Identity(v3Identity)).to.deep.eq(v3tov4Identity(v3Identity));
+    const privateKey = decodePrivateKey(v3tov4Identity(v3Identity).export());
+    expect(privateKey.length).to.eq(32);
+  });
+
+  // TODO: test that serializing a semaphore identity PCD and then deserializing it
+  // results in the same v4 identity being generated in the claim.
+});
