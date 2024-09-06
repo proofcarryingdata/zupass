48c1e341a88c2f136e32cce6f0dc3fa86394e859
Ivan Chub <ivansergchub@gmail.com>
2024-09-06 13:53:21 -0700

    move v4 upgrade request to spot in loadinitialstate AFTER state validation

--- apps/passport-client/src/loadInitialState.ts
+++ apps/passport-client/src/loadInitialState.ts
@@ -35,40 +35,6 @@ export async function loadInitialState(): Promise<AppState> {
 
   const pcds = await loadPCDs(self);
 
-  // in the case that the user has not upgraded their account to v4,
-  // (as indicated by missing semaphore_v4_commitment or semaphore_v4_pubkey in self)
-  // upgrade their account to include their v4 identity details. this is a one-time
-  // migration step necessary to upgrade an account from semaphore v3 to v4.
-  if (
-    pcds &&
-    self &&
-    (!self.semaphore_v4_commitment || !self.semaphore_v4_pubkey)
-  ) {
-    const identityPCD = v3IdentityToPCD(identityV3);
-
-    // there isn't really any to way to handle an error in this process, so
-    // we'll just proceed regardless of the response, and let this code-path
-    // execute again later to try again.
-    await requestUpgradeUserWithV4Commitment(
-      appConfig.zupassServer,
-      await makeUpgradeUserWithV4CommitmentRequest(
-        new PCDCollection(await getPackages(), [identityPCD])
-      )
-    );
-
-    const newSelfResponse = await requestUser(
-      appConfig.zupassServer,
-      self.uuid
-    );
-
-    if (newSelfResponse.success) {
-      self = newSelfResponse.value;
-      saveSelf(self);
-    } else {
-      // proceed to the next step anyway, since we don't have any other option
-    }
-  }
-
   const encryptionKey = loadEncryptionKey();
   const subscriptions = await loadSubscriptions();
 
@@ -92,7 +58,7 @@ export async function loadInitialState(): Promise<AppState> {
     self,
     encryptionKey,
     pcds,
-    identityV3: identityV3,
+    identityV3,
     identityV4: v3tov4Identity(identityV3),
     modal,
     subscriptions,
@@ -108,6 +74,40 @@ export async function loadInitialState(): Promise<AppState> {
   if (!validateAndLogInitialAppState("loadInitialState", state)) {
     state.userInvalid = true;
     state.modal = { modalType: "invalid-participant" };
+  } else {
+    // in the case that the user has not upgraded their account to v4,
+    // (as indicated by missing semaphore_v4_commitment or semaphore_v4_pubkey in self)
+    // upgrade their account to include their v4 identity details. this is a one-time
+    // migration step necessary to upgrade an account from semaphore v3 to v4.
+    if (
+      pcds &&
+      self &&
+      (!self.semaphore_v4_commitment || !self.semaphore_v4_pubkey)
+    ) {
+      const identityPCD = v3IdentityToPCD(identityV3);
+
+      // there isn't really any to way to handle an error in this process, so
+      // we'll just proceed regardless of the response, and let this code-path
+      // execute again later to try again.
+      await requestUpgradeUserWithV4Commitment(
+        appConfig.zupassServer,
+        await makeUpgradeUserWithV4CommitmentRequest(
+          new PCDCollection(await getPackages(), [identityPCD])
+        )
+      );
+
+      const newSelfResponse = await requestUser(
+        appConfig.zupassServer,
+        self.uuid
+      );
+
+      if (newSelfResponse.success) {
+        self = newSelfResponse.value;
+        saveSelf(self);
+      } else {
+        // proceed to the next step anyway, since we don't have any other option
+      }
+    }
   }
 
   return state;
