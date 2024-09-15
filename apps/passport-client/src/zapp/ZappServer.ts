9695e729ea85255e492a45ba9e76ed7af8e7fb89
Rob Knight <mail@robknight.org.uk>
2024-09-15 14:03:23 +0200

    Use Parcnet client libraries to implement Zapp API
    Only partially complete: still needs GPC canProve and verify, and POD
    query subscriptions.

--- apps/passport-client/src/zapp/ZappServer.ts
+++ apps/passport-client/src/zapp/ZappServer.ts
@@ -1,52 +1,27 @@
-import { EmailPCDTypeName } from "@pcd/email-pcd";
-import { GPCPCDArgs, GPCPCDPackage, GPCPCDTypeName } from "@pcd/gpc-pcd";
-import { PCDGetRequest, PCDRequestType } from "@pcd/passport-interface";
-import { SerializedPCD } from "@pcd/pcd-types";
-import { POD } from "@pcd/pod";
-import { isPODPCD, PODPCD, PODPCDPackage, PODPCDTypeName } from "@pcd/pod-pcd";
-import { p } from "@pcd/podspec";
+import { ConnectorAdvice } from "@parcnet-js/client-helpers";
 import {
-  ZupassAPI,
-  ZupassAPISchema,
-  ZupassFeeds,
-  ZupassFileSystem,
-  ZupassFolderContent,
-  ZupassGPC,
-  ZupassIdentity,
-  ZupassPOD
-} from "@pcd/zupass-client";
+  ParcnetGPCRPC,
+  ParcnetIdentityRPC,
+  ParcnetPODRPC,
+  ParcnetRPC,
+  PODQuery,
+  ProveResult
+} from "@parcnet-js/client-rpc";
+import * as p from "@parcnet-js/podspec";
+import { encodePrivateKey, POD, PODEntries } from "@pcd/pod";
+import { isPODPCD, PODPCD, PODPCDPackage, PODPCDTypeName } from "@pcd/pod-pcd";
+import { v3tov4Identity } from "@pcd/semaphore-identity-pcd";
 import { v4 as uuidv4 } from "uuid";
-import { z } from "zod";
 import { StateContextValue } from "../dispatch";
 import { EmbeddedScreenType } from "../embedded";
-import { ClientChannel } from "./useZappServer";
 
-const ZAPP_POD_SPECIAL_FOLDER_NAME = "PODs from Zapps";
-
-function safeInput<This extends BaseZappServer, Args extends unknown[], Return>(
-  parser: z.ZodSchema<Args>
-) {
-  return function actualDecorator(
-    originalMethod: (this: This, ...args: Args) => Return,
-    context: ClassMethodDecoratorContext
-  ): (this: This, ...args: Args) => Return {
-    function replacementMethod(this: This, ...args: Args): Return {
-      const input = parser.safeParse(args);
-      if (!input.success) {
-        throw new Error(`Invalid arguments for ${context.name.toString()}`);
-      }
-      return originalMethod.call(this, ...input.data);
-    }
-
-    return replacementMethod;
-  };
-}
+export const ZAPP_POD_SPECIAL_FOLDER_NAME = "PODs from Zapps";
 
 abstract class BaseZappServer {
   constructor(
     private context: StateContextValue,
     private zapp: PODPCD,
-    private clientChannel: ClientChannel
+    private advice: ConnectorAdvice
   ) {}
 
   public getZapp(): PODPCD {
@@ -57,198 +32,57 @@ abstract class BaseZappServer {
     return this.context;
   }
 
-  public getClientChannel(): ClientChannel {
-    return this.clientChannel;
+  public getAdvice(): ConnectorAdvice {
+    return this.advice;
   }
 }
 
-class FileSystem extends BaseZappServer implements ZupassFileSystem {
+export class ZupassIdentityRPC
+  extends BaseZappServer
+  implements ParcnetIdentityRPC
+{
   public constructor(
     context: StateContextValue,
     zapp: PODPCD,
-    clientChannel: ClientChannel
+    clientChannel: ConnectorAdvice
   ) {
     super(context, zapp, clientChannel);
   }
 
-  @safeInput(ZupassAPISchema.shape.fs.shape.list.parameters())
-  public async list(path: string): Promise<ZupassFolderContent[]> {
-    const state = this.getContext().getState();
-    const pcds = state.pcds.getAllPCDsInFolder(path);
-    const folders = state.pcds.getFoldersInFolder(path);
-    const result: ZupassFolderContent[] = [];
-
-    for (const folder of folders) {
-      result.push({
-        type: "folder",
-        name: folder
-      });
-    }
-    for (const pcd of pcds) {
-      result.push({
-        type: "pcd",
-        id: pcd.id,
-        pcdType: pcd.type
-      });
-    }
-    console.log(result);
-    return result;
-  }
-
-  @safeInput(ZupassAPISchema.shape.fs.shape.get.parameters())
-  public async get(path: string): Promise<SerializedPCD> {
-    const pathElements = path.split("/");
-    // @todo validate path, check permissions
-    const pcdId = pathElements.pop();
-    if (!pcdId) {
-      throw new Error("No PCD ID found in path");
-    }
-    const pcdCollection = this.getContext().getState().pcds;
-    const pcd = pcdCollection.getById(pcdId);
-    if (!pcd) {
-      throw new Error(`PCD with ID ${pcdId} does not exist`);
-    }
-    const serializedPCD = pcdCollection.serialize(pcd);
-
-    return serializedPCD;
-  }
-
-  @safeInput(ZupassAPISchema.shape.fs.shape.put.parameters())
-  public async put(path: string, content: SerializedPCD): Promise<void> {
-    // @todo validate path
-    console.log("adding ", path, content);
-    await this.getContext().dispatch({
-      type: "add-pcds",
-      folder: path,
-      pcds: [content],
-      upsert: true
-    });
-  }
-
-  @safeInput(ZupassAPISchema.shape.fs.shape.delete.parameters())
-  public async delete(_path: string): Promise<void> {
-    throw new Error("Not implemented");
-  }
-}
-
-class GPC extends BaseZappServer implements ZupassGPC {
-  public constructor(
-    context: StateContextValue,
-    zapp: PODPCD,
-    clientChannel: ClientChannel
-  ) {
-    super(context, zapp, clientChannel);
-  }
-
-  public async prove(args: GPCPCDArgs): Promise<SerializedPCD> {
-    const req: PCDGetRequest<typeof GPCPCDPackage> = {
-      type: PCDRequestType.Get,
-      returnUrl: "",
-      args,
-      pcdType: GPCPCDTypeName,
-      postMessage: false
-    };
-    this.getClientChannel().showZupass();
-    return new Promise((resolve) => {
-      this.getContext().dispatch({
-        type: "show-embedded-screen",
-        screen: {
-          type: EmbeddedScreenType.EmbeddedGetRequest,
-          request: req,
-          callback: (serialized: SerializedPCD) => {
-            this.getClientChannel().hideZupass();
-            this.getContext().dispatch({
-              type: "hide-embedded-screen"
-            });
-            resolve(serialized);
-          }
-        }
-      });
-    });
-  }
-}
-
-export class Feeds extends BaseZappServer implements ZupassFeeds {
-  public constructor(
-    context: StateContextValue,
-    zapp: PODPCD,
-    clientChannel: ClientChannel
-  ) {
-    super(context, zapp, clientChannel);
-  }
-
-  @safeInput(ZupassAPISchema.shape.feeds.shape.requestAddSubscription)
-  public async requestAddSubscription(
-    feedUrl: string,
-    feedId: string
-  ): Promise<void> {
-    this.getContext().dispatch({
-      type: "show-embedded-screen",
-      screen: {
-        type: EmbeddedScreenType.EmbeddedAddSubscription,
-        feedUrl,
-        feedId
-      }
-    });
-    this.getClientChannel().showZupass();
-  }
-}
-
-export class Identity extends BaseZappServer implements ZupassIdentity {
-  public constructor(
-    context: StateContextValue,
-    zapp: PODPCD,
-    clientChannel: ClientChannel
-  ) {
-    super(context, zapp, clientChannel);
-  }
-
-  public async getIdentityCommitment(): Promise<bigint> {
+  public async getSemaphoreV3Commitment(): Promise<bigint> {
     return this.getContext().getState().identityV3.getCommitment();
   }
-
-  public async getAttestedEmails(): Promise<SerializedPCD[]> {
-    const emailPCDs = this.getContext()
-      .getState()
-      .pcds.getPCDsByType(EmailPCDTypeName);
-    return Promise.all(
-      emailPCDs.map((pcd) => this.getContext().getState().pcds.serialize(pcd))
-    );
-  }
 }
 
-class PODServer extends BaseZappServer implements ZupassPOD {
+class ZupassPODRPC extends BaseZappServer implements ParcnetPODRPC {
   public constructor(
     context: StateContextValue,
     zapp: PODPCD,
-    clientChannel: ClientChannel
+    advice: ConnectorAdvice
   ) {
-    super(context, zapp, clientChannel);
+    super(context, zapp, advice);
+    this.getContext()
+      .getState()
+      .pcds.changeEmitter.listen(() => {});
   }
 
-  @safeInput(ZupassAPISchema.shape.pod.shape.query.parameters())
-  public async query(query: unknown): Promise<string[]> {
-    let q;
-    try {
-      // @todo need a better way of defining the possibly type of the query
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      q = p.deserialize(query as any);
-      console.log("querying with ", q);
-    } catch (e) {
-      console.log(e);
-      throw e;
-    }
+  public async subscribe(_query: PODQuery): Promise<string> {
+    return "1";
+  }
+
+  public async unsubscribe(_subscriptionId: string): Promise<void> {}
+
+  public async query(query: PODQuery): Promise<string[]> {
     const allPCDs = this.getContext()
       .getState()
       .pcds.getAllPCDsInFolder(ZAPP_POD_SPECIAL_FOLDER_NAME);
     const pods = allPCDs.filter(isPODPCD).map((pcd) => pcd.pod);
 
-    const result = q.query(pods);
+    const result = p.pod(query).query(pods);
 
     return result.matches.map((match) => match.serialize());
   }
 
-  @safeInput(ZupassAPISchema.shape.pod.shape.insert.parameters())
   /**
    * Insert a POD into the PCD collection.
    */
@@ -272,7 +106,6 @@ class PODServer extends BaseZappServer implements ZupassPOD {
     });
   }
 
-  @safeInput(ZupassAPISchema.shape.pod.shape.delete.parameters())
   /**
    * Delete all PODs with the given signature.
    */
@@ -301,26 +134,108 @@ class PODServer extends BaseZappServer implements ZupassPOD {
       )
     );
   }
+
+  public async sign(entries: PODEntries): Promise<string> {
+    const pod = POD.sign(
+      entries,
+      encodePrivateKey(
+        Buffer.from(
+          v3tov4Identity(this.getContext().getState().identityV3).export(),
+          "base64"
+        )
+      )
+    );
+    return pod.serialize();
+  }
 }
 
-export class ZappServer extends BaseZappServer implements ZupassAPI {
-  public fs: ZupassFileSystem;
-  public gpc: ZupassGPC;
-  public feeds: ZupassFeeds;
-  public identity: ZupassIdentity;
-  public pod: ZupassPOD;
-  public _version = "1" as const;
-
-  constructor(
+class ZupassGPCRPC extends BaseZappServer implements ParcnetGPCRPC {
+  public constructor(
     context: StateContextValue,
     zapp: PODPCD,
-    clientChannel: ClientChannel
+    advice: ConnectorAdvice
   ) {
-    super(context, zapp, clientChannel);
-    this.fs = new FileSystem(context, zapp, clientChannel);
-    this.gpc = new GPC(context, zapp, clientChannel);
-    this.feeds = new Feeds(context, zapp, clientChannel);
-    this.identity = new Identity(context, zapp, clientChannel);
-    this.pod = new PODServer(context, zapp, clientChannel);
+    super(context, zapp, advice);
+  }
+
+  public async prove(request: p.PodspecProofRequest): Promise<ProveResult> {
+    console.log("prove", request);
+    const prs = p.proofRequest(request);
+    const pods = this.getContext()
+      .getState()
+      .pcds.getAllPCDsInFolder(ZAPP_POD_SPECIAL_FOLDER_NAME)
+      .filter(isPODPCD)
+      .map((pcd) => pcd.pod);
+
+    const inputPods = prs.queryForInputs(pods);
+    if (
+      Object.values(inputPods).some((candidates) => candidates.length === 0)
+    ) {
+      return {
+        success: false,
+        error: "Not enough PODs"
+      };
+    }
+
+    return new Promise((resolve) => {
+      this.getContext().dispatch({
+        type: "show-embedded-screen",
+        screen: {
+          type: EmbeddedScreenType.EmbeddedGPCProof,
+          proofRequest: request,
+          callback: (result: ProveResult) => {
+            this.getContext().dispatch({
+              type: "hide-embedded-screen"
+            });
+            this.getAdvice().hideClient();
+            resolve(result);
+          }
+        }
+      });
+      this.getAdvice().showClient();
+    });
+  }
+
+  public async verify(): Promise<boolean> {
+    return true;
+  }
+
+  public async canProve(): Promise<boolean> {
+    return true;
+  }
+}
+
+export class ZupassRPCProcessor extends BaseZappServer implements ParcnetRPC {
+  _version: "1" = "1" as const;
+  //private readonly subscriptions: QuerySubscriptions;
+  public readonly identity: ZupassIdentityRPC;
+  public readonly pod: ZupassPODRPC;
+  public readonly gpc: ZupassGPCRPC;
+
+  public constructor(
+    context: StateContextValue,
+    zapp: PODPCD,
+    advice: ConnectorAdvice
+  ) {
+    super(context, zapp, advice);
+    // this.subscriptions = new QuerySubscriptions(this.pods);
+    // this.subscriptions.onSubscriptionUpdated((update, serial) => {
+    //   this.clientChannel.subscriptionUpdate(update, serial);
+    // });
+    this.pod = new ZupassPODRPC(
+      this.getContext(),
+      this.getZapp(),
+      this.getAdvice()
+    );
+    this.identity = new ZupassIdentityRPC(
+      this.getContext(),
+      this.getZapp(),
+      this.getAdvice()
+    );
+    this.gpc = new ZupassGPCRPC(
+      this.getContext(),
+      this.getZapp(),
+      this.getAdvice()
+    );
   }
 }
--- apps/passport-client/src/zapp/useZappServer.ts
+++ apps/passport-client/src/zapp/useZappServer.ts
@@ -1,187 +1,98 @@
+import { listen } from "@parcnet-js/client-helpers/connection/iframe";
 import { ArgumentTypeName } from "@pcd/pcd-types";
+import { encodePrivateKey } from "@pcd/pod";
 import { isPODPCD, PODPCD, PODPCDPackage } from "@pcd/pod-pcd";
-import { getErrorMessage } from "@pcd/util";
-import {
-  deepGet,
-  postRPCMessage,
-  RPCMessage,
-  RPCMessageSchema,
-  RPCMessageType,
-  WindowMessageSchema,
-  WindowMessageType
-} from "@pcd/zupass-client";
+import { v3tov4Identity } from "@pcd/semaphore-identity-pcd";
 import { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useStateContext } from "../appHooks";
-import { ZappServer } from "./ZappServer";
-
-export class ClientChannel {
-  constructor(private port: MessagePort) {}
-
-  public showZupass(): void {
-    this.port.postMessage({
-      type: RPCMessageType.ZUPASS_CLIENT_SHOW
-    });
-  }
-
-  public hideZupass(): void {
-    this.port.postMessage({
-      type: RPCMessageType.ZUPASS_CLIENT_HIDE
-    });
-  }
-}
-
-function setupPort(port: MessagePort, server: ZappServer): void {
-  port.addEventListener("message", async (event) => {
-    console.log(`SERVER RECEIVED ${event.data.type}`);
-    const message = RPCMessageSchema.parse(event.data);
-    if (message.type === RPCMessageType.ZUPASS_CLIENT_INVOKE) {
-      const path = message.fn.split(".");
-      const functionName = path.pop();
-      if (!functionName) {
-        throw new Error("Path does not contain a function name");
-      }
-      const object = deepGet(server, path);
-      const functionToInvoke = (object as Record<string, unknown>)[
-        functionName
-      ];
-      try {
-        if (functionToInvoke && typeof functionToInvoke === "function") {
-          console.log("invoking function", functionToInvoke, message.args);
-          try {
-            const result = await functionToInvoke.apply(object, message.args);
-            console.log(result);
-            port.postMessage({
-              type: RPCMessageType.ZUPASS_CLIENT_INVOKE_RESULT,
-              result,
-              serial: message.serial
-            } satisfies RPCMessage);
-          } catch (e) {
-            port.postMessage({
-              type: RPCMessageType.ZUPASS_CLIENT_INVOKE_ERROR,
-              serial: message.serial,
-              error: getErrorMessage(e)
-            } satisfies RPCMessage);
-          }
-        } else {
-          throw new Error("Function not found");
-        }
-      } catch (error) {
-        port.postMessage({
-          type: RPCMessageType.ZUPASS_CLIENT_INVOKE_ERROR,
-          error: getErrorMessage(error)
-        });
-      }
-    }
-  });
-}
-
-// In real usage, this would be the user's Semaphore v4 key
-const MAGIC_PRIVATE_KEY =
-  "00112233445566778899AABBCCDDEEFF00112233445566778899aabbccddeeff";
+import { ZupassRPCProcessor } from "./ZappServer";
 
 export function useZappServer(): void {
   const context = useStateContext();
 
   useEffect(() => {
-    window.addEventListener("message", async (event) => {
-      let port: MessagePort;
-      // @todo: handle repeat calls?
-      console.log(event);
-      const data = WindowMessageSchema.safeParse(event.data);
-      if (!data.success) {
-        return;
-      }
-      const msg = data.data;
-      console.log("passport-client received message ", msg);
-      if (msg.type === WindowMessageType.ZUPASS_CLIENT_CONNECT) {
-        const origin = event.origin;
-        port = event.ports[0];
-        port.start();
+    (async (): Promise<void> => {
+      const { zapp, advice } = await listen();
+      console.log("zapp", zapp);
 
-        if (!context.getState().self) {
-          console.log("user not logged in");
-          // User is not logged in
-          postRPCMessage(port, {
-            type: RPCMessageType.ZUPASS_CLIENT_SHOW
-          });
+      if (!context.getState().self) {
+        advice.showClient();
 
-          await new Promise<void>((resolve) => {
-            const unlisten = context.stateEmitter.listen((state) => {
-              if (state.self) {
-                postRPCMessage(port, {
-                  type: RPCMessageType.ZUPASS_CLIENT_HIDE
-                });
-                unlisten();
-                resolve();
-              }
-            });
-          });
-        }
-
-        const zapps = context.getState().pcds.getAllPCDsInFolder("Zapps");
-        let zapp = zapps.filter(isPODPCD).find((zapp) => {
-          return Object.entries(zapp.claim.entries).find(([key, entry]) => {
-            if (
-              key === "origin" &&
-              entry.type === "string" &&
-              entry.value === origin
-            ) {
-              return true;
+        await new Promise<void>((resolve) => {
+          const unlisten = context.stateEmitter.listen((state) => {
+            if (state.self) {
+              advice.hideClient();
+              unlisten();
+              resolve();
             }
-            return false;
           });
         });
+      }
 
-        let approved = !!zapp;
-
-        if (!zapp) {
-          approved = confirm(
-            `Allow ${msg.zapp.name} at ${origin} to connect to your Zupass account?\r\n\r\nThis is HIGHLY EXPERIMENTAL - make sure you trust this website.`
-          );
-          if (approved) {
-            const newZapp = (await PODPCDPackage.prove({
-              entries: {
-                argumentType: ArgumentTypeName.Object,
-                value: {
-                  origin: { type: "string", value: origin },
-                  name: { type: "string", value: msg.zapp.name }
-                }
-              },
-              privateKey: {
-                argumentType: ArgumentTypeName.String,
-                value: MAGIC_PRIVATE_KEY
-              },
-              id: {
-                argumentType: ArgumentTypeName.String,
-                value: uuidv4()
-              }
-            })) as PODPCD;
-            const newZappSerialized = await PODPCDPackage.serialize(newZapp);
-            context.dispatch({
-              type: "add-pcds",
-              pcds: [newZappSerialized],
-              folder: "Zapps"
-            });
-            zapp = newZapp;
-          } else {
-            return;
+      const zapps = context.getState().pcds.getAllPCDsInFolder("Zapps");
+      let zappPOD = zapps.filter(isPODPCD).find((zapp) => {
+        return Object.entries(zapp.claim.entries).find(([key, entry]) => {
+          if (
+            key === "origin" &&
+            entry.type === "string" &&
+            entry.value === origin
+          ) {
+            return true;
           }
-        }
+          return false;
+        });
+      });
+
+      let approved = !!zappPOD;
+
+      if (!zappPOD) {
+        approved = confirm(
+          `Allow ${zapp.name} at ${origin} to connect to your Zupass account?\r\n\r\nThis is HIGHLY EXPERIMENTAL - make sure you trust this website.`
+        );
         if (approved) {
-          const clientChannel = new ClientChannel(port);
-          const server = new ZappServer(context, zapp, clientChannel);
-
-          // @todo handle this with an action
-          context.update({ embeddedScreen: undefined });
-          window.location.hash = "embedded";
-
-          setupPort(port, server);
-          port.postMessage({
-            type: RPCMessageType.ZUPASS_CLIENT_READY
+          const newZapp = (await PODPCDPackage.prove({
+            entries: {
+              argumentType: ArgumentTypeName.Object,
+              value: {
+                origin: { type: "string", value: origin },
+                name: { type: "string", value: zapp.name }
+              }
+            },
+            privateKey: {
+              argumentType: ArgumentTypeName.String,
+              value: encodePrivateKey(
+                Buffer.from(
+                  v3tov4Identity(context.getState().identityV3).export(),
+                  "base64"
+                )
+              )
+            },
+            id: {
+              argumentType: ArgumentTypeName.String,
+              value: uuidv4()
+            }
+          })) as PODPCD;
+          const newZappSerialized = await PODPCDPackage.serialize(newZapp);
+          context.dispatch({
+            type: "add-pcds",
+            pcds: [newZappSerialized],
+            folder: "Zapps"
           });
+          zappPOD = newZapp;
+        } else {
+          return;
         }
       }
-    });
+      if (approved) {
+        const server = new ZupassRPCProcessor(context, zappPOD, advice);
+
+        // @todo handle this with an action
+        context.update({ embeddedScreen: undefined });
+        window.location.hash = "embedded";
+
+        advice.ready(server);
+      }
+    })();
   }, [context]);
 }
