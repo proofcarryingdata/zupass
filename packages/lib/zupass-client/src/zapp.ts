87f9f16f8cf776f1cd607fb7c465de2b0f4d72d2
Rob Knight <mail@robknight.org.uk>
2024-09-16 20:17:48 +0200

    Remove legacy package and example app

--- packages/lib/zupass-client/src/zapp.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const ZappSchema = z.object({
-  name: z.string(),
-  permissions: z.array(z.string())
-});
-
-export type Zapp = z.infer<typeof ZappSchema>;
--- packages/lib/zupass-client/test/zupass-client.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import "mocha";
-
-describe("zupass-client should work", async function () {
-  it("zupass-client should do something", function () {
-    expect(true).to.eq(true);
-  });
-});
