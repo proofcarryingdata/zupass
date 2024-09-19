46824d8bcad008eff1af478b218ce3625f6aeb2e
Ivan Chub <ivansergchub@gmail.com>
2024-09-19 12:12:44 -0700

    comment update

--- packages/lib/passport-interface/src/api/requestPodboxOneClickEmails.ts
+++ packages/lib/passport-interface/src/api/requestPodboxOneClickEmails.ts
@@ -4,7 +4,7 @@ import { APIResult } from "./apiResult";
 import { httpGetSimple } from "./makeRequest";
 
 /**
- * Hits an endpoint to download a semaphore protocol group.
+ * Hits an endpoint to download anonymized email to pretix order code mappings.
  *
  * Never rejects. All information encoded in the resolved response.
  */
