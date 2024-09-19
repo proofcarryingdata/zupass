a5bcb418c83fb7b58e80fb88d57c6fffe0eb79be
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-09-19 19:24:08 +0300

    get font working

--- apps/passport-client/new-components/Typography.tsx
+++ apps/passport-client/new-components/Typography.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const TypographyText = styled.span`
-  /* font-family: "Barlow", sans-serif; */
+  font-family: "Barlow", sans-serif;
+  color: rgba(0, 0, 0, 1);
 `;
 interface TypographyProps {
   children?: React.ReactNode;
