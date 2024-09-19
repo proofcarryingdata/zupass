3d53613782965bbdb8a7b8fe43bf602be6d96d6f
or orsatti <antonio@10play.dev>
2024-09-19 18:14:18 +0300

    chore(init): add new dev route for components

---
+++ apps/passport-client/new-components/ComponentsScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { AppContainer } from "../components/shared/AppContainer";
+import { TextCenter } from "../components/core";
+
+export default function ComponentsScreen() {
+  return (
+    <AppContainer bg="primary">
+      <TextCenter>Hello, world!</TextCenter>
+    </AppContainer>
+  );
+}
