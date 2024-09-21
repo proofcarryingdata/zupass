8885e90370422f249422bbcba047c75d1b343273
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-09-21 15:50:33 +0300

    doc live reload

--- apps/passport-client/src/liveReload.ts
+++ apps/passport-client/src/liveReload.ts
@@ -1,5 +1,6 @@
 import { appConfig } from "./appConfig";
 
+// Implement fe part for esbuild live reload - this does not preserve the js state https://esbuild.github.io/api/#live-reload
 export function enableLiveReload(): void {
   if (appConfig.devMode) {
     new EventSource("/esbuild").addEventListener("change", () => {
