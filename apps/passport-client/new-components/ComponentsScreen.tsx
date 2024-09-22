03080a34ccdd4a6584de6d20a63ca236a5905b57
or orsatti <antonio@10play.dev>
2024-09-22 18:27:24 +0300

    fix(list): change main file name to index

--- apps/passport-client/new-components/ComponentsScreen.tsx
+++ apps/passport-client/new-components/ComponentsScreen.tsx
@@ -1,15 +1,66 @@
 import { useState } from "react";
-import { CenterColumn, TextCenter } from "../components/core";
+import { CenterColumn, Spacer, TextCenter } from "../components/core";
 import { AppContainer } from "../components/shared/AppContainer";
 import { Avatar } from "./Avatar";
 import { Button2 } from "./Button";
 import { FloatingMenu } from "./FloatingMenu";
 import { Input2 } from "./Input";
-import { List } from "./List/List";
+import { List } from "./List";
 import { Ticket } from "./Ticket";
 import { TicketCard } from "./TicketCard";
 import { SettingsBottomModal } from "./settingsBottomModal";
 import { FaTrashCan } from "react-icons/fa6";
+const exampleList = [
+  {
+    title: "Event Passes",
+    isLastItemBorder: false,
+    children: [
+      {
+        title: "Devcon Pass",
+        LeftIcon: <Avatar imgSrc="https://i.imgur.com/Fzs5N9T.jpeg" />
+      },
+      {
+        title: "Berlin Event Pass",
+        variant: "danger",
+        LeftIcon: <FaTrashCan />
+      },
+      {
+        title: "Denver Event Pass",
+        variant: "danger",
+        LeftIcon: <FaTrashCan />
+      }
+    ]
+  },
+  {
+    title: "Puddle Crypto",
+    children: [
+      {
+        title: "American Bullfrog",
+        LeftIcon: (
+          <Avatar
+            imgSrc={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nrdc.org%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_100%2Fpublic%2Fmedia-uploads%2F02_b3ewye_2400.jpg.jpg%3Fitok%3D4cywc1Uq&f=1&nofb=1&ipt=d994e52f175331180fca4072983909856868e3f3687df2475a18206a29a2b29b&ipo=images`}
+          />
+        )
+      },
+      {
+        title: "Wood Frog",
+        LeftIcon: (
+          <Avatar
+            imgSrc={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fv2%2Fresize%3Afit%3A1200%2F1*EKAWH3tIOEed1vSrzzhDpg.jpeg&f=1&nofb=1&ipt=af41ba48a56ef7af73a0f953d337053dfd6f0e69963763ef2eafd55d489b4b72&ipo=images`}
+          />
+        )
+      }
+    ]
+  },
+  {
+    title: "FrogCraiglist",
+    children: [
+      {
+        title: "Digital Chair Listing"
+      }
+    ]
+  }
+];
 
 const ComponentsScreen = (): JSX.Element => {
   const [error, setError] = useState("");
@@ -69,62 +120,17 @@ const ComponentsScreen = (): JSX.Element => {
                 height: 630
               }}
             >
-              <List
-                list={[
-                  {
-                    title: "Event Passes",
-                    isLastItemBorder: false,
-                    children: [
-                      {
-                        title: "Devcon Pass",
-                        LeftIcon: (
-                          <Avatar imgSrc="https://i.imgur.com/Fzs5N9T.jpeg" />
-                        )
-                      },
-                      {
-                        title: "Berlin Event Pass",
-                        variant: "danger",
-                        LeftIcon: <FaTrashCan />
-                      },
-                      {
-                        title: "Denver Event Pass",
-                        variant: "danger",
-                        LeftIcon: <FaTrashCan />
-                      }
-                    ]
-                  },
-                  {
-                    title: "Puddle Crypto",
-                    children: [
-                      {
-                        title: "American Bullfrog",
-                        LeftIcon: (
-                          <Avatar
-                            imgSrc={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nrdc.org%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_100%2Fpublic%2Fmedia-uploads%2F02_b3ewye_2400.jpg.jpg%3Fitok%3D4cywc1Uq&f=1&nofb=1&ipt=d994e52f175331180fca4072983909856868e3f3687df2475a18206a29a2b29b&ipo=images`}
-                          />
-                        )
-                      },
-                      {
-                        title: "Wood Frog",
-                        LeftIcon: (
-                          <Avatar
-                            imgSrc={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fv2%2Fresize%3Afit%3A1200%2F1*EKAWH3tIOEed1vSrzzhDpg.jpeg&f=1&nofb=1&ipt=af41ba48a56ef7af73a0f953d337053dfd6f0e69963763ef2eafd55d489b4b72&ipo=images`}
-                          />
-                        )
-                      }
-                    ]
-                  },
-                  {
-                    title: "FrogCraiglist",
-                    children: [
-                      {
-                        title: "Digital Chair Listing"
-                      }
-                    ]
-                  }
-                ]}
-              />
+              <List list={exampleList} />
             </div>
+            <Spacer h={12} />
+            <div
+              style={{
+                background: "#fff",
+                borderRadius: 40,
+                width: 370,
+                height: 630
+              }}
+            ></div>
           </div>
         </CenterColumn>
         <div style={{ display: "flex", flexDirection: "row", gap: 40 }}>
