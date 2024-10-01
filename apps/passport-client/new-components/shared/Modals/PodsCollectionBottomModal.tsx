d82969d97ccbf5e9d7e3e5064f3b6b8b89c385cb
or orsatti <antonio@10play.dev>
2024-10-02 00:20:18 +0300

    fix(collection pods modal): show new ui when able

--- apps/passport-client/new-components/shared/Modals/PodsCollectionBottomModal.tsx
+++ apps/passport-client/new-components/shared/Modals/PodsCollectionBottomModal.tsx
@@ -1,7 +1,6 @@
 import { isEdDSATicketPCD } from "@pcd/eddsa-ticket-pcd";
 import { isPODTicketPCD } from "@pcd/pod-ticket-pcd";
 import { useMemo } from "react";
-import { FaTrashCan } from "react-icons/fa6";
 import styled from "styled-components";
 import { CardBody } from "../../../components/shared/PCDCard";
 import {
@@ -14,56 +13,6 @@ import { BottomModal } from "../BottomModal";
 import { Button2 } from "../Button";
 import { GroupType, List } from "../List";
 import { Typography } from "../Typography";
-
-const exampleList = [
-  {
-    title: "Event Passes",
-    isLastItemBorder: false,
-    children: [
-      {
-        title: "Devcon Pass",
-        LeftIcon: <Avatar imgSrc="https://i.imgur.com/Fzs5N9T.jpeg" />
-      },
-      {
-        title: "Berlin Event Pass",
-        LeftIcon: <FaTrashCan />
-      },
-      {
-        title: "Denver Event Pass",
-        LeftIcon: <FaTrashCan />
-      }
-    ]
-  },
-  {
-    title: "Puddle Crypto",
-    children: [
-      {
-        title: "American Bullfrog",
-        LeftIcon: (
-          <Avatar
-            imgSrc={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nrdc.org%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_100%2Fpublic%2Fmedia-uploads%2F02_b3ewye_2400.jpg.jpg%3Fitok%3D4cywc1Uq&f=1&nofb=1&ipt=d994e52f175331180fca4072983909856868e3f3687df2475a18206a29a2b29b&ipo=images`}
-          />
-        )
-      },
-      {
-        title: "Wood Frog",
-        LeftIcon: (
-          <Avatar
-            imgSrc={`https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fv2%2Fresize%3Afit%3A1200%2F1*EKAWH3tIOEed1vSrzzhDpg.jpeg&f=1&nofb=1&ipt=af41ba48a56ef7af73a0f953d337053dfd6f0e69963763ef2eafd55d489b4b72&ipo=images`}
-          />
-        )
-      }
-    ]
-  },
-  {
-    title: "FrogCraiglist",
-    children: [
-      {
-        title: "Digital Chair Listing"
-      }
-    ]
-  }
-];
 
 export const PodsCollectionBottomModal = (): JSX.Element => {
   const activeBottomModal = useBottomModal();
@@ -127,7 +76,7 @@ export const PodsCollectionBottomModal = (): JSX.Element => {
         </UserTitleContainer>
         <ListContainer>
           {activePod ? (
-            <CardBody isMainIdentity={false} pcd={activePod} />
+            <CardBody newUI={true} isMainIdentity={false} pcd={activePod} />
           ) : (
             <List list={podsCollectionList} />
           )}
