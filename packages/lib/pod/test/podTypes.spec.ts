246cea347b08bea9cd4b9e5377f6dbb47852d49e
Rob Knight <mail@robknight.org.uk>
2024-10-02 11:43:15 -0700

    Check that constant matches constant in @pcd/utils

--- packages/lib/pod/test/podTypes.spec.ts
+++ packages/lib/pod/test/podTypes.spec.ts
@@ -1,3 +1,4 @@
+import { BABY_JUB_PRIME } from "@pcd/util";
 import { r } from "@zk-kit/baby-jubjub";
 import { expect } from "chai";
 import "mocha";
@@ -6,5 +7,6 @@ import { BabyJubjubR } from "../src/podTypes";
 describe("POD type values should be correct", () => {
   it("should have the correct BabyJubjubR constant", () => {
     expect(BabyJubjubR).to.equal(r);
+    expect(BabyJubjubR).to.equal(BABY_JUB_PRIME);
   });
 });
