24d87bc210fd74fabeeb7a21256b614b846579cd
Foret Fang <saurfang@users.noreply.github.com>
2024-10-02 23:50:47 -0700

    automatically resize iframe for zapp screen

--- apps/passport-client/components/screens/ZappScreens/ZappScreen.tsx
+++ apps/passport-client/components/screens/ZappScreens/ZappScreen.tsx
@@ -1,3 +1,4 @@
+import IframeResizer from "iframe-resizer-react";
 import { ReactNode } from "react";
 import { useDispatch, useEmbeddedScreenState } from "../../../src/appHooks";
 import { ListenMode, useZappServer } from "../../../src/zapp/useZappServer";
@@ -8,8 +9,14 @@ export function ZappScreen({ url }: { url: string }): ReactNode {
   return (
     <>
       <ZappModal />
-      <iframe
-        style={{ width: "100%", height: "100%", borderRadius: "10px" }}
+      <IframeResizer
+        style={{
+          // NB: Using min-width to set the width of the iFrame, works around an issue in iOS that can prevent the iFrame from sizing correctly.
+          width: "1px",
+          minWidth: "calc(100% - 16px)",
+          borderRadius: "12px",
+          margin: "0 8px"
+        }}
         src={url}
         sandbox="allow-downloads allow-same-origin allow-scripts allow-popups allow-modals allow-forms allow-storage-access-by-user-activation allow-popups-to-escape-sandbox"
       />
