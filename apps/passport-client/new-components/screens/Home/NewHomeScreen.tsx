d0d9c815c9a681822a8a866119c619c91b57aa39
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-10-03 18:55:55 +0300

    Update NewHomeScreen.tsx

--- apps/passport-client/new-components/screens/Home/NewHomeScreen.tsx
+++ apps/passport-client/new-components/screens/Home/NewHomeScreen.tsx
@@ -35,7 +35,7 @@ const SCREEN_HORIZONTAL_PADDING = 20;
 const BUTTONS_CONTAINER_HEIGHT = 40;
 
 const isEventTicketPCD = (pcd: PCD<unknown, unknown>): pcd is TicketType => {
-  // TODO: fetch the pods type as well and prioritize it if theres a conflict.
+  // TODO: fetch the pods type as well and prioritize it if theres a conflict. test
   return isEdDSATicketPCD(pcd) || isPODTicketPCD(pcd);
 };
 
