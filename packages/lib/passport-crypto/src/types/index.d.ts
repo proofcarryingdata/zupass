4bacd3c258ea0dff4b0a2669f983d114c6d3babd
Rob Knight <mail@robknight.org.uk>
2024-10-11 09:24:47 -0700

    Use external packages instead of vendoring into the repo

--- packages/lib/passport-crypto/src/types/index.d.ts
+++ packages/lib/passport-crypto/src/types/index.d.ts
@@ -1,4 +1,4 @@
 declare module "@pcd/libsodium-wrappers-sumo" {
-  import * as libsodium from "@types/libsodium-wrappers-sumo";
+  import * as libsodium from "libsodium-wrappers-sumo";
   export = libsodium;
 }
