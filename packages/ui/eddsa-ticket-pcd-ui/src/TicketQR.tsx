44d5a80ac38ce7078dab0c17eb38f5b7d2b26c55
or orsatti <antonio@10play.dev>
2024-10-16 12:56:59 +0300

    fix(ticket qrs): remove specific events logic from qr generation

--- packages/ui/eddsa-ticket-pcd-ui/src/TicketQR.tsx
+++ packages/ui/eddsa-ticket-pcd-ui/src/TicketQR.tsx
@@ -14,14 +14,7 @@ export function TicketQR({
   pcd: EdDSATicketPCD;
 } & Omit<EdDSATicketPCDCardProps, "onClickAddons">): JSX.Element {
   const generate = useCallback(async () => {
-    if (
-      [
-        "53edb3e7-6733-41e0-a9be-488877c5c572", // eth berlin
-        "508313ea-f16b-4729-bdf0-281c64493ca9", //  eth prague
-        "5074edf5-f079-4099-b036-22223c0c6995" // devcon 7
-      ].includes(pcd.claim.ticket.eventId) &&
-      pcd.claim.ticket.ticketSecret
-    ) {
+    if (pcd.claim.ticket.ticketSecret) {
       return pcd.claim.ticket.ticketSecret;
     } else {
       return linkToTicket(
--- packages/ui/pod-ticket-pcd-ui/src/CardBody.tsx
+++ packages/ui/pod-ticket-pcd-ui/src/CardBody.tsx
@@ -99,14 +99,7 @@ export function TicketQR({
   idBasedVerifyURL: string;
 }): JSX.Element {
   const generate = useCallback(async () => {
-    if (
-      [
-        "53edb3e7-6733-41e0-a9be-488877c5c572", // eth berlin
-        "508313ea-f16b-4729-bdf0-281c64493ca9", //  eth prague
-        "5074edf5-f079-4099-b036-22223c0c6995" // devcon 7
-      ].includes(ticketData.eventId) &&
-      ticketData.ticketSecret
-    ) {
+    if (ticketData.ticketSecret) {
       return ticketData.ticketSecret;
     }
 
