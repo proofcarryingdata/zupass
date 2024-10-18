1ba076e455c170857d111d3250b0a92ba84a89b2
Ivan Chub <ivansergchub@gmail.com>
2024-10-17 18:54:59 -0700

    fixed tests

--- apps/passport-server/test/util/startTestingApplication.ts
+++ apps/passport-server/test/util/startTestingApplication.ts
@@ -1,9 +1,13 @@
 import { startApplication } from "../../src/application";
 import { APIs, ServerMode, Zupass } from "../../src/types";
+import { mockAPIs } from "./mockApis";
 
 export async function startTestingApp(
   apiOverrides?: Partial<APIs>
 ): Promise<Zupass> {
-  const application = await startApplication(ServerMode.UNIFIED, apiOverrides);
+  const application = await startApplication(
+    ServerMode.UNIFIED,
+    mockAPIs(apiOverrides)
+  );
   return application;
 }
