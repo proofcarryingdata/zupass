1fca483cf791377225839631aa7747e354119e7f
amir angel <amirangel101@hotmail.com>
2024-10-22 18:26:59 +0300

    Page Indicator Added & PageCircleButton Styling

--- apps/passport-client/new-components/screens/Home/NewHomeScreen.tsx
+++ apps/passport-client/new-components/screens/Home/NewHomeScreen.tsx
@@ -143,19 +143,17 @@ const disabledCSS = css`
 `;
 
 export const PageCircleButton = styled.button<{
-  diameter: number;
-  padding: number;
   disabled: boolean;
 }>`
-  ${(p): string => {
-    const size = p.diameter + 2 * p.padding + "px";
-    return `width: ${size};height: ${size};`;
-  }};
+  width: 40px;
+  height: 32px;
   cursor: pointer;
-  border-radius: 99px;
-  border: none;
+  border-radius: 200px;
+  border: 2px solid #ffffff;
   margin: 0;
-  padding: ${(p): number => p.padding}px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   box-shadow:
     0px 1px 3px 0px rgba(0, 0, 0, 0.1),
     0px 1px 2px 0px rgba(0, 0, 0, 0.06);
@@ -496,8 +494,6 @@ export const NewHomeScreen = (): ReactElement => {
               <ButtonsContainer>
                 <PageCircleButton
                   disabled={currentPos === 0}
-                  padding={6}
-                  diameter={28}
                   onClick={() => {
                     setCurrentPos((old) => {
                       if (old === 0) return old;
@@ -506,15 +502,22 @@ export const NewHomeScreen = (): ReactElement => {
                   }}
                 >
                   <ChevronLeftIcon
-                    width={20}
-                    height={20}
+                    width={24}
+                    height={24}
                     color="var(--text-tertiary)"
                   />
                 </PageCircleButton>
+                <Typography
+                  fontSize={14}
+                  color="#8B94AC"
+                  fontWeight={500}
+                  family="Rubik"
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  {currentPos + 1} OF {tickets.length}
+                </Typography>
                 <PageCircleButton
                   disabled={currentPos === tickets.length - 1}
-                  padding={6}
-                  diameter={28}
                   onClick={() => {
                     setCurrentPos((old) => {
                       if (old === tickets.length - 1) return old;
@@ -523,8 +526,8 @@ export const NewHomeScreen = (): ReactElement => {
                   }}
                 >
                   <ChevronRightIcon
-                    width={20}
-                    height={20}
+                    width={24}
+                    height={24}
                     color="var(--text-tertiary)"
                   />
                 </PageCircleButton>
