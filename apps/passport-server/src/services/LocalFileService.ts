ca1d02dffdcc42e469b6a334852fef4904e6ef8a
Ivan Chub <ivansergchub@gmail.com>
2024-10-24 15:23:03 -0700

    add a mutex to localfileservice

--- apps/passport-server/src/services/LocalFileService.ts
+++ apps/passport-server/src/services/LocalFileService.ts
@@ -1,4 +1,5 @@
 import { PipelineLoadSummary } from "@pcd/passport-interface";
+import { Mutex } from "async-mutex";
 import fs from "fs";
 import path from "path";
 import { PipelineAtom } from "../database/queries/pipelineAtomDB";
@@ -12,6 +13,7 @@ export interface SerializedPipelineLoad<T extends PipelineAtom> {
 }
 
 export class LocalFileService {
+  private readonly mutex = new Mutex();
   private readonly tempDirectory: string = path.join(process.cwd(), "temp");
   private readonly tempPipelineLoadDirectory: string = path.join(
     this.tempDirectory,
@@ -28,12 +30,14 @@ export class LocalFileService {
     pipelineId: string
   ): Promise<SerializedPipelineLoad<T> | undefined> {
     return traced("LocalFileService", "loadPipelineLoad", async () => {
-      const filePath = this.getPipelineLoadPath(pipelineId);
-      if (!fs.existsSync(filePath)) {
-        return undefined;
-      }
-      const serialized = await fs.promises.readFile(filePath, "utf8");
-      return JSON.parse(serialized) satisfies SerializedPipelineLoad<T>;
+      return await this.mutex.runExclusive(async () => {
+        const filePath = this.getPipelineLoadPath(pipelineId);
+        if (!fs.existsSync(filePath)) {
+          return undefined;
+        }
+        const serialized = await fs.promises.readFile(filePath, "utf8");
+        return JSON.parse(serialized) satisfies SerializedPipelineLoad<T>;
+      });
     });
   }
 
@@ -43,24 +47,26 @@ export class LocalFileService {
     atoms: T[]
   ): Promise<void> {
     return traced("LocalFileService", "savePipelineLoad", async () => {
-      const serialized = JSON.stringify({
-        pipelineId,
-        summary,
-        atoms
-      } satisfies SerializedPipelineLoad<T>);
+      return await this.mutex.runExclusive(async () => {
+        const serialized = JSON.stringify({
+          pipelineId,
+          summary,
+          atoms
+        } satisfies SerializedPipelineLoad<T>);
 
-      logger(
-        `Saving pipeline ('${pipelineId}') load with ${
-          atoms.length
-        } atoms to local file: ${JSON.stringify(serialized)} with ${
-          serialized.length
-        } bytes`
-      );
+        logger(
+          `Saving pipeline ('${pipelineId}') load with ${
+            atoms.length
+          } atoms to local file: ${JSON.stringify(serialized)} with ${
+            serialized.length
+          } bytes`
+        );
 
-      await fs.promises.writeFile(
-        this.getPipelineLoadPath(pipelineId),
-        JSON.stringify(serialized)
-      );
+        await fs.promises.writeFile(
+          this.getPipelineLoadPath(pipelineId),
+          JSON.stringify(serialized)
+        );
+      });
     });
   }
 }
