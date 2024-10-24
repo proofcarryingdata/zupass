703ae9ee75912ee7b63409d6b6f068f0f79a86ee
Rob Knight <mail@robknight.org.uk>
2024-10-24 02:52:45 +0200

    Add support for setting the circuit identifier in Z API calls (#2031)
    This adds support for setting a circuit identifier on GPC proofs when
    invoked via the Z API.
    
    This also bumps all of the Z API-related packages, allowing us to remove
    the @ts-expect-error comments from passport-client.

--- apps/passport-client/components/screens/EmbeddedScreens/EmbeddedGPCProofScreen.tsx
+++ apps/passport-client/components/screens/EmbeddedScreens/EmbeddedGPCProofScreen.tsx
@@ -1,7 +1,7 @@
 import { ProveResult } from "@parcnet-js/client-rpc";
 import * as p from "@parcnet-js/podspec";
 import { EntriesSchema, PodspecProofRequest } from "@parcnet-js/podspec";
-import { gpcProve } from "@pcd/gpc";
+import { GPCIdentifier, gpcProve } from "@pcd/gpc";
 import { Button, Spacer } from "@pcd/passport-ui";
 import { POD, POD_INT_MAX, POD_INT_MIN } from "@pcd/pod";
 import {
@@ -22,10 +22,12 @@ import { Spinner } from "../../shared/Spinner";
 export function EmbeddedGPCProofScreen({
   proofRequestSchema,
   collectionIds,
+  circuitIdentifier,
   callback
 }: {
   proofRequestSchema: PodspecProofRequest;
   collectionIds: string[];
+  circuitIdentifier?: GPCIdentifier;
   callback: (result: ProveResult) => void;
 }): ReactNode {
   useSyncE2EEStorage();
@@ -53,7 +55,6 @@ export function EmbeddedGPCProofScreen({
     return pods;
   }, [pcds, collectionIds]);
   const candidatePODs = useMemo(() => {
-    // @ts-expect-error not a meaningful type mismatch
     return prs.queryForInputs(allPods);
   }, [allPods, prs]);
   const proofRequest = useMemo(() => {
@@ -92,7 +93,6 @@ export function EmbeddedGPCProofScreen({
               key={name}
               name={name}
               schema={schema}
-              // @ts-expect-error not a meaningful type mismatch
               pods={candidatePODs[name]}
               selectedPOD={selectedPODs[name]}
               onChange={(pod) => {
@@ -112,7 +112,10 @@ export function EmbeddedGPCProofScreen({
               setProving(true);
 
               gpcProve(
-                proofRequest.proofConfig,
+                {
+                  ...proofRequest.proofConfig,
+                  ...(circuitIdentifier ? { circuitIdentifier } : {})
+                },
                 {
                   pods: selectedPODs as Record<string, POD>,
                   membershipLists: proofRequest.membershipLists,
