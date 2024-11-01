0337d95854347c6220a9b13e2e27c79f220a82b7
Richard Liu <richard.y.liu@berkeley.edu>
2024-11-01 00:20:32 -0700

    lint

--- apps/passport-client/components/shared/AppContainer.tsx
+++ apps/passport-client/components/shared/AppContainer.tsx
@@ -1,6 +1,6 @@
 import { ReactNode, useCallback } from "react";
 import { Toaster } from "react-hot-toast";
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 import { ErrorBottomModal } from "../../new-components/shared/Modals/ErrorBottomModal";
 import {
   useAppError,
