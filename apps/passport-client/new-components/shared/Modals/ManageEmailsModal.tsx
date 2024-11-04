72049603acad047ace5dbf171578bedcbc5e96ca
amir angel <amirangel101@hotmail.com>
2024-11-04 11:49:27 +0200

    Clear Confirmation & Email Field

--- apps/passport-client/new-components/shared/Modals/ManageEmailsModal.tsx
+++ apps/passport-client/new-components/shared/Modals/ManageEmailsModal.tsx
@@ -196,6 +196,8 @@ export const ManageEmailModal = (): JSX.Element => {
         stateContext.update({
           extraSubscriptionFetchRequested: true
         });
+        setConfirmationCode("");
+        setNewEmail("");
       } else {
         setError(
           `Couldn't add '${newEmail}', please wait and try again later.`
