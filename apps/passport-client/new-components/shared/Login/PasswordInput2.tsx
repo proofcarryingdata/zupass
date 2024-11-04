6c2b65ff46bf3d28e06676c1e5bd7dc5b674a3da
amir angel <amirangel101@hotmail.com>
2024-11-04 13:15:05 +0200

    make optional for case like NewAlreadyRegisteredScreen that has one input field

--- apps/passport-client/new-components/shared/Login/PasswordInput2.tsx
+++ apps/passport-client/new-components/shared/Login/PasswordInput2.tsx
@@ -1,11 +1,11 @@
 import EyeIcon from "@heroicons/react/16/solid/EyeIcon";
 import EyeSlashIcon from "@heroicons/react/16/solid/EyeSlashIcon";
-import { ForwardedRef, forwardRef } from "react";
+import { ForwardedRef, forwardRef, useState } from "react";
 import { Input2, NewInputProps } from "../Input";
 
 interface PasswordInputProps extends NewInputProps {
-  showPassword: boolean;
-  onTogglePassword: () => void;
+  showPassword?: boolean;
+  onTogglePassword?: () => void;
 }
 
 export const PasswordInput2 = forwardRef(
@@ -13,26 +13,28 @@ export const PasswordInput2 = forwardRef(
     props: PasswordInputProps,
     ref: ForwardedRef<HTMLInputElement>
   ): JSX.Element => {
+    const [localShowPassword, setLocalShowPassword] = useState(false);
     const { showPassword, onTogglePassword, ...inputProps } = props;
+
+    // Use local state if not provided
+    const isPasswordVisible = showPassword ?? localShowPassword;
+    const handleToggle =
+      onTogglePassword ?? (() => setLocalShowPassword((prev) => !prev));
 
     return (
       <Input2
         {...inputProps}
-        type={!showPassword ? "password" : "text"}
+        type={!isPasswordVisible ? "password" : "text"}
         ref={ref}
         endIcon={
-          !showPassword ? (
+          !isPasswordVisible ? (
             <EyeSlashIcon
-              onClick={onTogglePassword}
+              onClick={handleToggle}
               color="#8B94AC"
-              cursor={"pointer"}
+              cursor="pointer"
             />
           ) : (
-            <EyeIcon
-              onClick={onTogglePassword}
-              color="#8B94AC"
-              cursor={"pointer"}
-            />
+            <EyeIcon onClick={handleToggle} color="#8B94AC" cursor="pointer" />
           )
         }
       />
