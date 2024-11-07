c8803fec29041c89d6b9cfec15c3d0da71ae6b56
Richard Liu <richard.y.liu@berkeley.edu>
2024-11-08 02:41:38 +0700

    try green X

--- apps/passport-client/components/screens/ZappScreens/ZappFullScreen.tsx
+++ apps/passport-client/components/screens/ZappScreens/ZappFullScreen.tsx
@@ -1,34 +1,25 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ReactElement } from "react";
 import styled from "styled-components";
 import { SwipeViewContainer } from "../../../new-components/shared/SwipeViewContainer";
 import { AppContainer } from "../../shared/AppContainer";
 import { ZappScreen } from "./ZappScreen";
 
+const ContentWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+`;
+
 const FloatingReturnButton = styled.div`
-  position: fixed;
-  left: 50%;
-  bottom: 20px;
-  z-index: 2;
-  transform: translateX(-50%);
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  right: 22px;
+  top: 28px;
+  z-index: 9998;
   cursor: pointer;
-
-  display: inline-flex;
-  padding: 8px 20px;
-  align-items: flex-start;
-
-  border-radius: 200px;
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  background: rgba(0, 0, 0, 0.8);
-  backdrop-filter: blur(8px);
-
-  /* text */
-  color: #fff;
-  text-align: center;
-  font-family: Rubik;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 135%;
 `;
 
 interface ZappFullScreenProps {
@@ -43,10 +34,12 @@ export const ZappFullScreen = ({
   return (
     <AppContainer bg="gray" noPadding fullscreen>
       <SwipeViewContainer isZapp>
-        <FloatingReturnButton onClick={onReturn}>
-          <span>Back to Zupass</span>
-        </FloatingReturnButton>
-        <ZappScreen url={url} />
+        <ContentWrapper>
+          <FloatingReturnButton onClick={onReturn}>
+            <XMarkIcon fill="#12823B" />
+          </FloatingReturnButton>
+          <ZappScreen url={url} />
+        </ContentWrapper>
       </SwipeViewContainer>
     </AppContainer>
   );
