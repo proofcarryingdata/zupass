37c76d1bbfb71e54e0756fd1209197fb856ef023
Ivan Chub <ivansergchub@gmail.com>
2024-11-11 19:51:40 -0800

    add env var for changing the database pool size

--- apps/passport-server/src/database/postgresConfiguration.ts
+++ apps/passport-server/src/database/postgresConfiguration.ts
@@ -26,6 +26,15 @@ export function getDatabaseConfiguration(): PoolOptionsExplicit & SslSettings {
     throw new Error("Missing or incorrect env variable: DATABASE_SSL");
   }
 
+  let poolSize = 32;
+  if (process.env.DB_POOL_SIZE !== undefined) {
+    poolSize = parseInt(process.env.DB_POOL_SIZE, 10);
+    if (isNaN(poolSize)) {
+      poolSize = 32;
+    }
+    poolSize = Math.min(Math.max(poolSize, 32), 70);
+  }
+
   // defaults here: https://github.com/postgres-pool/postgres-pool/blob/9d623823dc365b5edea3303cab6ae519bfaa94f7/src/index.ts#L264C10-L290
   // docs here: https://github.com/postgres-pool/postgres-pool/blob/9d623823dc365b5edea3303cab6ae519bfaa94f7/src/index.ts#L29-L130
   return {
@@ -43,7 +52,7 @@ export function getDatabaseConfiguration(): PoolOptionsExplicit & SslSettings {
     // Pool configuration
     connectionTimeoutMillis: 16_000,
     idleTimeoutMillis: 8_000,
-    poolSize: 32, // max connection # on render is 97
+    poolSize: poolSize, // max connection # on render is 97
     waitForAvailableConnectionTimeoutMillis: 30_000,
     databaseStartupTimeoutMillis: 30_000,
     readOnlyTransactionReconnectTimeoutMillis: 30_000,
