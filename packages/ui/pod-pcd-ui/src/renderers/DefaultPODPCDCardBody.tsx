cb1274bb3a0f347a632815d7a18152ac2f3f20a5
Andrew Twyman <artwyman@users.noreply.github.com>
2024-11-14 20:27:26 -0800

    Copy POD text (#2178)
    Not pretty, but it works. Someone with more UI clue is welcome to do
    some styling.
    
    
    https://github.com/user-attachments/assets/51ae3fdc-4692-46fe-a225-68d6b4b3e529
    
    
    
    CC @saurfang @rrrliu

--- packages/ui/pod-pcd-ui/src/renderers/DefaultPODPCDCardBody.tsx
+++ packages/ui/pod-pcd-ui/src/renderers/DefaultPODPCDCardBody.tsx
@@ -1,4 +1,10 @@
-import { Card, HiddenText, styled, TextContainer } from "@pcd/passport-ui";
+import {
+  Card,
+  CardWithCopy,
+  HiddenText,
+  styled,
+  TextContainer
+} from "@pcd/passport-ui";
 import { podEntriesToJSON } from "@pcd/pod";
 import { PODPCD } from "@pcd/pod-pcd";
 
@@ -12,26 +18,38 @@ const StyledPre = styled.pre`
 export function DefaultPODPCDCardBody({ pcd }: { pcd: PODPCD }): JSX.Element {
   return (
     <div style={{ display: "flex", gap: 8, flexDirection: "column" }}>
-      <TextContainer
-        style={{
-          overflow: "auto",
-          maxHeight: "280px",
-          backgroundColor: "#F6F8FD"
+      <CardWithCopy
+        title=""
+        onCopy={() => {
+          return navigator.clipboard.writeText(
+            JSON.stringify(pcd.pod.toJSON(), null, 2)
+          );
         }}
       >
-        <StyledPre>
-          {JSON.stringify(podEntriesToJSON(pcd.claim.entries), null, 2)}
-        </StyledPre>
-      </TextContainer>
-      <Card title="EdDSA PUBLIC KEY">
-        <HiddenText
-          text={pcd.claim.signerPublicKey}
-          style={{ overflowX: "auto" }}
-        />
-      </Card>
-      <Card title="EdDSA SIGNATURE">
-        <HiddenText text={pcd.proof.signature} style={{ overflowX: "auto" }} />
-      </Card>
+        <TextContainer
+          style={{
+            overflow: "auto",
+            maxHeight: "280px",
+            backgroundColor: "#F6F8FD"
+          }}
+        >
+          <StyledPre>
+            {JSON.stringify(podEntriesToJSON(pcd.claim.entries), null, 2)}
+          </StyledPre>
+        </TextContainer>
+        <Card title="EdDSA PUBLIC KEY">
+          <HiddenText
+            text={pcd.claim.signerPublicKey}
+            style={{ overflowX: "auto" }}
+          />
+        </Card>
+        <Card title="EdDSA SIGNATURE">
+          <HiddenText
+            text={pcd.proof.signature}
+            style={{ overflowX: "auto" }}
+          />
+        </Card>
+      </CardWithCopy>
     </div>
   );
 }
