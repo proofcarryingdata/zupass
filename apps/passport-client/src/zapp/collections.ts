2481830f6e06d28a77482542739abd468bc6bc33
Rob Knight <mail@robknight.org.uk>
2025-01-21 15:25:54 +0100

    Basic implementation of Email PODs in Z API

--- apps/passport-client/src/zapp/collections.ts
+++ apps/passport-client/src/zapp/collections.ts
@@ -1,5 +1,6 @@
 import { PCDCollection } from "@pcd/pcd-collection";
-import type { POD } from "@pcd/pod";
+import { POD } from "@pcd/pod";
+import { PODEmailPCD, isPODEmailPCD } from "@pcd/pod-email-pcd";
 import { isPODPCD } from "@pcd/pod-pcd";
 import { PODTicketPCD, isPODTicketPCD, ticketToPOD } from "@pcd/pod-ticket-pcd";
 
@@ -13,6 +14,14 @@ export function getCollectionNames(pcds: PCDCollection): string[] {
   return pcds.getFoldersInFolder(COLLECTIONS_ROOT_FOLDER_NAME);
 }
 
+function emailPCDToPOD(pcd: PODEmailPCD): POD {
+  return POD.load(
+    pcd.claim.podEntries,
+    pcd.proof.signature,
+    pcd.claim.signerPublicKey
+  );
+}
+
 export function getPODsForCollections(
   pcds: PCDCollection,
   collectionIds: string[]
@@ -21,8 +30,16 @@ export function getPODsForCollections(
     .flatMap((collectionId) =>
       collectionId === "Devcon SEA"
         ? pcds.getAllPCDsInFolder("Devcon SEA")
+        : collectionId === "Email"
+        ? pcds.getAllPCDsInFolder("Email")
         : pcds.getAllPCDsInFolder(collectionIdToFolderName(collectionId))
     )
-    .filter((pcd) => isPODPCD(pcd) || isPODTicketPCD(pcd))
-    .map((pcd) => (isPODPCD(pcd) ? pcd.pod : ticketToPOD(pcd as PODTicketPCD)));
+    .filter((pcd) => isPODPCD(pcd) || isPODTicketPCD(pcd) || isPODEmailPCD(pcd))
+    .map((pcd) =>
+      isPODPCD(pcd)
+        ? pcd.pod
+        : isPODTicketPCD(pcd)
+        ? ticketToPOD(pcd as PODTicketPCD)
+        : emailPCDToPOD(pcd as PODEmailPCD)
+    );
 }
